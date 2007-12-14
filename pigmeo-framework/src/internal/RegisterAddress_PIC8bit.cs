62b5dc786e83ea533c0d161f5dfe2d69bd714a0d
urriellu <pigmeo@urriellu.net>
2007-12-14 19:25:54 +0000

    new (empty) constructor

--- pigmeo-framework/src/internal/RegisterAddress_PIC8bit.cs
+++ pigmeo-framework/src/internal/RegisterAddress_PIC8bit.cs
@@ -1,7 +1,12 @@
 ﻿namespace Pigmeo.Internal {
 	public class RegisterAddress_PIC8bit {
+		public bool undefined;
 		public byte bank;
 		public byte Address;
+
+		public RegisterAddress_PIC8bit() {
+			undefined = true;
+		}
 
 		public RegisterAddress_PIC8bit(byte bank, byte Address) {
 			this.bank = bank;
