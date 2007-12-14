0b7d811f04abbe7b28504f74752cf72ac36a3b09
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2007-12-14 19:23:46 +0000

    new compilation scripts

---
+++ trunk/compile.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# cd into the script's directory
+MyPath=`dirname $0`
+MyOldPath=$PWD
+cd $MyPath
+
+echo Compiling...
+
+./CountLinesOfCode.sh
+
+./pigmeo-framework/compile.sh
+./pigmeo-compiler/compile.sh
+
+
+# cd into the directory where you were before running this script
+cd $MyOldPath
