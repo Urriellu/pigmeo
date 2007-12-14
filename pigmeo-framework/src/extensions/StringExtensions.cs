60e55e9187d3e993eacd71e4bdc0e87636e9edf6
urriellu <pigmeo@urriellu.net>
2007-12-14 19:24:37 +0000

    fixing method names

--- pigmeo-framework/src/extensions/StringExtensions.cs
+++ pigmeo-framework/src/extensions/StringExtensions.cs
@@ -5,28 +5,28 @@ namespace Pigmeo.Extensions {
 		/// <summary>
 		/// Parses a string written in binary and converts it to a 8-bit unsigned integer
 		/// </summary>
-		public static byte Bin2Byte(this string BinaryString) {
+		public static byte BinToByte(this string BinaryString) {
 			return Convert.ToByte(BinaryString, 2);
 		}
 
 		/// <summary>
 		/// Parses a string written in binary and converts it to a 8-bit unsigned integer
 		/// </summary>
-		public static byte Bin2UInt8(this string BinaryString) {
-			return Bin2UInt8(BinaryString);
+		public static byte BinToUInt8(this string BinaryString) {
+			return BinToByte(BinaryString);
 		}
 
 		/// <summary>
 		/// Parses a string written in binary and converts it to a 16-bit unsigned integer
 		/// </summary>
-		public static UInt16 Bin2UInt16(this string BinaryString) {
+		public static UInt16 BinToUInt16(this string BinaryString) {
 			return Convert.ToUInt16(BinaryString, 2);
 		}
 
 		/// <summary>
 		/// Parses a string written in binary and converts it to a 32-bit unsigned integer
 		/// </summary>
-		public static UInt32 Bin2UInt32(this string BinaryString) {
+		public static UInt32 BinToUInt32(this string BinaryString) {
 			return Convert.ToUInt32(BinaryString, 2);
 		}
 	}
