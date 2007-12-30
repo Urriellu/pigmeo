5d7c3c3b15242b7f7a5fa52b7547262c694d1110
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2007-12-30 17:58:41 +0000

    testing PORTA and STATUS registers

--- trunk/pigmeo-framework/src/devices/PIC16F716.cs
+++ trunk/pigmeo-framework/src/devices/PIC16F716.cs
@@ -1,4 +1,5 @@
 ﻿using Pigmeo;
+using Pigmeo.Extensions;
 using Pigmeo.Internal;
 using Pigmeo.Internal.PIC14;
 using System;
@@ -6,6 +7,7 @@ using System.Reflection;
 
 [assembly: AssemblyVersion("0.1")]
 [assembly: DeviceLibrary(Architecture.PIC16, Branch.PIC16F716)]
+[assembly: AssemblyKeyFile("../../../pigmeo.key")]
 
 namespace Pigmeo.MCU {
 	/// <summary>
@@ -33,11 +35,57 @@ namespace Pigmeo.MCU {
 	}
 
 	public static class Registers {
-		[AsmName("PORTA"), Location(0,0x05)]
+		[AsmName("OriginalNameRemoveThisPORTA"), Location(0,0x05)]
 		public static byte PORTA = 0;
 
 
 		[AsmName("PORTB"), Location(0,0x06)]
 		public static byte PORTB = 0;
+
+		/// <summary>
+		/// Contains the arithmetic status of the ALU, the Reset status and the bank select bits for data memory
+		/// </summary>
+		public static class STATUS {
+			/// <summary>Register Bank Select bit (used for direct addressing). RP0=false for bank 0, RP0=true for bank 1</summary>
+			[Location(0, 3, 5)]
+			public static bool RP0 = false;
+
+			/// <summary>Time-out bit. INVERTED. true = After power-up, CLRWDT instruction or SLEEP instruction. false = A WDT time-out occurred</summary>
+			[Location(0, 3, 4)]
+			public static readonly bool _TO = true;
+
+			/// <summary>Power-down bit. INVERTED. true = After power-up or by the CLRWDT instruction. false = By execution of the SLEEP instruction</summary>
+			[Location(0, 3, 3)]
+			public static readonly bool _PD = true;
+
+			/// <summary>Zero bit. true = The result of an arithmetic or logic operation is zero. false = The result of an arithmetic or logic operation is not zero</summary>
+			[Location(0, 3, 2)]
+			public static bool Z;
+
+			/// <summary>Digit Carry/_Borrow bit (ADDWF, ADDLW,SUBLW,SUBWF instructions). For Borrow, the polarity is reversed. true = A carry-out from the 4th low-order bit of the result occurred. false = No carry-out from the 4th low-order bit of the result</summary>
+			[Location(0, 3, 1)]
+			public static bool DC;
+
+			/// <summary>Carry/_Borrow bit (ADDWF, ADDLW, SUBLW, SUBWF instructions). true = A carry-out from the Most Significant bit of the result occurred. false = No carry-out from the Most Significant bit of the result occurred</summary>
+			/// <remarks>For Borrow, the polarity is reversed. A subtraction is executed by adding the two's complement of the second operand. For rotate (RRF, RLF) instructions, this bit is loaded with either the high-order or low-order bit of the source register.</remarks>
+			[Location(0, 3, 0)]
+			public static bool C;
+
+/*			public static implicit operator STATUS(byte value){
+				RP0=value.GetBit(5);
+				//...
+			}*/
+	}
+
+	/*public static class PORTA {
+		public static bool this[byte bit] {
+			get {
+				return Registers.PORTA.GetBit(bit);
+			}
+		}
+
+		public static implicit operator PORTA(byte value) {
+			Registers.PORTA = value;
+		}*/
 	}
 }
