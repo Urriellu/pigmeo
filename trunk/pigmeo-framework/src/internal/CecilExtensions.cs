a47d1de89594c4ae854c61d0548b4fc29263fd80
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2007-12-30 19:07:47 +0000

    creating OpCode extensions

--- trunk/pigmeo-framework/src/internal/CecilExtensions.cs
+++ trunk/pigmeo-framework/src/internal/CecilExtensions.cs
@@ -1,10 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Pigmeo.Internal {
-	public static class CecilExtensions {
+	/// <summary>
+	/// Extensions to the Mono.Cecil.Opcode class
+	/// </summary>
+	public static class OpCodeExtension {
+		/// <summary>
+		/// Indicates if the opcode is a 'ldc'
+		/// </summary>
+		public static bool IsLdc(this OpCode opc) {
+			if(opc == OpCodes.Ldc_I4 ||
+				opc == OpCodes.Ldc_I4_0 ||
+				opc == OpCodes.Ldc_I4_1 ||
+				opc == OpCodes.Ldc_I4_2 ||
+				opc == OpCodes.Ldc_I4_3 ||
+				opc == OpCodes.Ldc_I4_4 ||
+				opc == OpCodes.Ldc_I4_5 ||
+				opc == OpCodes.Ldc_I4_6 ||
+				opc == OpCodes.Ldc_I4_7 ||
+				opc == OpCodes.Ldc_I4_8 ||
+				opc == OpCodes.Ldc_I4_M1 ||
+				opc == OpCodes.Ldc_I4_S ||
+				opc == OpCodes.Ldc_I8 ||
+				opc == OpCodes.Ldc_R4 ||
+				opc == OpCodes.Ldc_R8
+				) return true;
+			return false;
+		}
 
+		/// <summary>
+		/// Indicates if the opcode is an addition
+		/// </summary>
+		public static bool IsAdd(this OpCode opc) {
+			if(opc == OpCodes.Add ||
+				opc == OpCodes.Add_Ovf ||
+				opc == OpCodes.Add_Ovf_Un
+				) return true;
+			return false;
+		}
+
+		/// <summary>
+		/// Indicates if the opcode is a 'conv'
+		/// </summary>
+		public static bool IsConv(this OpCode opc) {
+			if(opc == OpCodes.Conv_I ||
+				opc == OpCodes.Conv_I1 ||
+				opc == OpCodes.Conv_I2 ||
+				opc == OpCodes.Conv_I4 ||
+				opc == OpCodes.Conv_I8 ||
+				opc == OpCodes.Conv_Ovf_I ||
+				opc == OpCodes.Conv_Ovf_I1 ||
+				opc == OpCodes.Conv_Ovf_I2 ||
+				opc == OpCodes.Conv_Ovf_I4 ||
+				opc == OpCodes.Conv_Ovf_I8 ||
+				opc == OpCodes.Conv_Ovf_I_Un ||
+				opc == OpCodes.Conv_Ovf_I1_Un ||
+				opc == OpCodes.Conv_Ovf_I2_Un ||
+				opc == OpCodes.Conv_Ovf_I4_Un ||
+				opc == OpCodes.Conv_Ovf_I8_Un ||
+				opc == OpCodes.Conv_Ovf_U ||
+				opc == OpCodes.Conv_Ovf_U1 ||
+				opc == OpCodes.Conv_Ovf_U2 ||
+				opc == OpCodes.Conv_Ovf_U4 ||
+				opc == OpCodes.Conv_Ovf_U8 ||
+				opc == OpCodes.Conv_Ovf_U_Un ||
+				opc == OpCodes.Conv_Ovf_U1_Un ||
+				opc == OpCodes.Conv_Ovf_U2_Un ||
+				opc == OpCodes.Conv_Ovf_U4_Un ||
+				opc == OpCodes.Conv_Ovf_U8_Un ||
+				opc == OpCodes.Conv_R_Un ||
+				opc == OpCodes.Conv_R4 ||
+				opc == OpCodes.Conv_R8 ||
+				opc == OpCodes.Conv_U ||
+				opc == OpCodes.Conv_U1 ||
+				opc == OpCodes.Conv_U2 ||
+				opc == OpCodes.Conv_U4 ||
+				opc == OpCodes.Conv_U8
+				) return true;
+			return false;
+		}
+
+		/// <summary>
+		/// Indicates if the frontend should modify the instruction or not
+		/// </summary>
+		public static bool IsFrontendDontTouch(this OpCode opc) {
+			if(opc.IsLdc() ||
+				opc == OpCodes.Stsfld ||
+				opc.IsAdd() ||
+				opc.IsConv() ||
+				opc == OpCodes.Ret
+				) return true;
+			return false;
+		}
 	}
 }
