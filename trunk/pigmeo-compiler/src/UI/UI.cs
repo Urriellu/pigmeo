c470b4125f2245f7384f62808a924fb2997001fc
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-09 19:26:15 +0000

    moving the winforms main window from S.W.Form to Pigmeo.Compiler.UI.WinForms.MainWindow

--- trunk/pigmeo-compiler/src/UI/UI.cs
+++ trunk/pigmeo-compiler/src/UI/UI.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Pigmeo.Compiler.UI {
+﻿namespace Pigmeo.Compiler.UI {
 	/// <summary>
 	/// All the available user interfaces
 	/// </summary>
 	public static class UIs {
-		public static System.Windows.Forms.Form WinFormsMainWindow;
+		public static UI.WinForms.MainWindow WinFormsMainWindow;
 	}
 }
