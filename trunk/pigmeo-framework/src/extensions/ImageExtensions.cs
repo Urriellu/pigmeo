5ad4bf64ed397b34c3afab62078e582d572582ae
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-09 19:17:53 +0000

    adding ImageExtensions

---
+++ trunk/pigmeo-framework/src/extensions/ImageExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+
+namespace Pigmeo.Extensions {
+	public static class ImageExtensions {
+		/// <summary>
+		/// Scales the image to fit the specified maximum width and height
+		/// </summary>
+		/// <param name="source"></param>
+		/// <param name="MaxWidth">Maximum width</param>
+		/// <param name="MaxHeight">Maximum height</param>
+		public static void Scale(this Image source, int MaxWidth, int MaxHeight) {
+			float ImgRatio = source.Width / (float)source.Height;
+
+			if(source.Width > MaxWidth) {
+				source = new Bitmap(source, new Size(MaxWidth, (int)Math.Round(MaxWidth / ImgRatio, 0)));
+			}
+
+			if(source.Height > MaxHeight) {
+				source = new Bitmap(source, new Size((int)Math.Round(MaxWidth * ImgRatio, 0), MaxHeight));
+			}
+		}
+	}
+}
