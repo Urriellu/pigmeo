fbbe956352c136308b60f19b07895bafe2af8abe
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-09 10:59:56 +0000

    adding WinForms GUI

---
+++ trunk/pigmeo-compiler/src/UI/WinForms/MainWindow.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Pigmeo.Compiler.UI.WinForms {
+	partial class MainWindow {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.MenuItem001 = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuItem002 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+			this.MenuItem003 = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuItem004 = new System.Windows.Forms.ToolStripMenuItem();
+			this.MenuItem005 = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem001,
+            this.MenuItem004});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(622, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// MenuItem001
+			// 
+			this.MenuItem001.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem002,
+            this.toolStripMenuItem1,
+            this.MenuItem003});
+			this.MenuItem001.Name = "MenuItem001";
+			this.MenuItem001.Size = new System.Drawing.Size(78, 20);
+			this.MenuItem001.Text = "not set (file)";
+			// 
+			// MenuItem002
+			// 
+			this.MenuItem002.Name = "MenuItem002";
+			this.MenuItem002.Size = new System.Drawing.Size(154, 22);
+			this.MenuItem002.Text = "not set (open)";
+			this.MenuItem002.Click += new System.EventHandler(this.MenuItem002_Click);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(151, 6);
+			// 
+			// MenuItem003
+			// 
+			this.MenuItem003.Name = "MenuItem003";
+			this.MenuItem003.Size = new System.Drawing.Size(154, 22);
+			this.MenuItem003.Text = "not set (exit)";
+			this.MenuItem003.Click += new System.EventHandler(this.MenuItem003_Click);
+			// 
+			// MenuItem004
+			// 
+			this.MenuItem004.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuItem005});
+			this.MenuItem004.Name = "MenuItem004";
+			this.MenuItem004.Size = new System.Drawing.Size(84, 20);
+			this.MenuItem004.Text = "not set (help)";
+			// 
+			// MenuItem005
+			// 
+			this.MenuItem005.Name = "MenuItem005";
+			this.MenuItem005.Size = new System.Drawing.Size(158, 22);
+			this.MenuItem005.Text = "not set (about)";
+			this.MenuItem005.Click += new System.EventHandler(this.MenuItem005_Click);
+			// 
+			// MainWindow
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(622, 448);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "MainWindow";
+			this.Text = "not set";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem MenuItem001;
+		private System.Windows.Forms.ToolStripMenuItem MenuItem002;
+		private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem MenuItem003;
+		private System.Windows.Forms.ToolStripMenuItem MenuItem004;
+		private System.Windows.Forms.ToolStripMenuItem MenuItem005;
+	}
+}
