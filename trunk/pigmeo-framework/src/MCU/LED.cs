fbeb717ce6b56c09dc6e5d5bc5c8a9c5d051d3ed
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-09 11:11:16 +0000

    adding LED class

---
+++ trunk/pigmeo-framework/src/MCU/LED.cs
@@ -0,0 +1,47 @@
+﻿/*namespace Pigmeo.MCU {
+	public enum LedStatus { OFF, ON }
+
+
+
+	public class LED {
+		private ReadPin_del ReadPinOfLed;
+		public readonly LogicType logic;
+
+		public LED(ReadPin_del del, LogicType logic) {
+			ReadPinOfLed = del;
+			this.logic = logic;
+		}
+
+		public LedStatus Status {
+			get {
+				LedStatus ret = LedStatus.OFF;
+				if(ReadPinOfLed.Invoke()) ret = LedStatus.ON;
+				return ret;
+			}
+			set {
+
+			}
+		}
+
+		public void TurnON() {
+			this.Status = LedStatus.ON;
+		}
+
+		public void TurnOFF() {
+			this.Status = LedStatus.OFF;
+		}
+
+		public bool IsON {
+			get {
+				if(this.Status == LedStatus.ON) return true;
+				return false;
+			}
+		}
+
+		public bool IsOFF {
+			get {
+				return !IsON;
+			}
+		}
+	}
+}*/
