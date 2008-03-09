12cbfbdd964f20162b9951aeadf0965f8408d1f8
urriellu <pigmeo@urriellu.net>
2008-03-09 11:02:32 +0000

    AssemblyToCompile moved here

---
+++ pigmeo-compiler/src/GlobalShares.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pigmeo.Compiler {
+	/// <summary>
+	/// Miscellaneous variables and objects available for the entire application
+	/// </summary>
+	public static class GlobalShares {
+		/// <summary>
+		/// Assembly which is going to be compiled. It is usually created from Frontend()
+		/// </summary>
+		/// <remarks>
+		/// We need to store the assembly here so we can compile it (in the backend) after running the frontend without saving it to disk
+		/// </remarks>
+		public static Mono.Cecil.AssemblyDefinition AssemblyToCompile;
+
+		/// <summary>
+		/// List of references of the user application
+		/// </summary>
+		public static List<string> UserAppReferenceFiles = new List<string>();
+	}
+}
