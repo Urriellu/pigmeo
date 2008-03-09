b56e0194e9925d6ff1a0369d50ee69b44c1d08c9
urriellu <pigmeo@urriellu.net>
2008-03-09 19:19:38 +0000

    more debugging information

--- pigmeo-compiler/src/BackendPIC14/Backend.cs
+++ pigmeo-compiler/src/BackendPIC14/Backend.cs
@@ -15,10 +15,21 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 		public static List<string> RunBrackend(AssemblyDefinition OriginalAssembly) {
 			ShowInfo.InfoVerbose("Running the 8-bit PIC backend");
 
+			ShowInfo.InfoDebug("Optimizing CIL for PIC14 architecture");
 			AssemblyDefinition OptimizedAssembly = OptimizeCIL(OriginalAssembly);
+			if(config.Internal.UI == UserInterface.WinForms) UI.UIs.WinFormsMainWindow.ProgBar.Value = 50;
+
+			ShowInfo.InfoDebug("Adding the kernel to the assembly");
 			AssemblyDefinition AssemblyWithKernel = AddKernel(OptimizedAssembly);
+			if(config.Internal.UI == UserInterface.WinForms) UI.UIs.WinFormsMainWindow.ProgBar.Value = 55;
+
+			ShowInfo.InfoDebug("Compiling to assembly language");
 			Asm AsmLangApp = CompileToAsm.Run(AssemblyWithKernel);
+			if(config.Internal.UI == UserInterface.WinForms) UI.UIs.WinFormsMainWindow.ProgBar.Value = 65;
+
+			ShowInfo.InfoDebug("Optimizing the assembly language for the target architecture");
 			Asm OptimizedAsmApp = OptimizeAsm(AsmLangApp);
+			if(config.Internal.UI == UserInterface.WinForms) UI.UIs.WinFormsMainWindow.ProgBar.Value = 75;
 
 			return OptimizedAsmApp.AsmCode;
 		}
