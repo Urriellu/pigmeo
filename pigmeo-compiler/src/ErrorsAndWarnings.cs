7996934f9c6549aaf369cb974d97f641c0e3aa0b
urriellu <pigmeo@urriellu.net>
2008-03-14 11:43:11 +0000

    Using PrintErrorMessage() and PrintMessage(). Moving some strings to i18n

--- pigmeo-compiler/src/ErrorsAndWarnings.cs
+++ pigmeo-compiler/src/ErrorsAndWarnings.cs
@@ -17,6 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
 using System.Collections.Generic;
+using Pigmeo.Internal;
 
 namespace Pigmeo.Compiler {
 
@@ -64,22 +65,28 @@ namespace Pigmeo.Compiler {
 		/// <param name="exit">Specifies if the execution must be stopped (set true for fatal errors)</param>
 		public static void Throw(errType type, string ID, bool exit, params string[] p) {
 			if(ErrWarns.ContainsKey(ID)) {
+				string message = "";
 				switch(type) {
 					case errType.Warning:
 						_TotalWarnings++;
-						Console.Write("Warning");
+						message += i18n.str(26);
 						break;
 					case errType.Error:
 						_TotalErrors++;
-						Console.Write("Error");
+						message += i18n.str(27);
 						break;
 				}
-				Console.Write(" {0}: {1}", ID, ErrWarns[ID]);
+				message += string.Format(" {0}: {1}", ID, ErrWarns[ID]);
 				if(p.Length > 0) {
-					Console.Write(". Extra info: {0}", p[0]);
+					message+=string.Format(i18n.str(28), p[0]);
 				}
-				Console.Write(Environment.NewLine);
-				Console.WriteLine("\tSee http://pigmeo.org/InfoError.php?id={0}", ID);
+				if(type == errType.Error) {
+					UI.UIs.PrintErrorMessage(message);
+					UI.UIs.PrintErrorMessage(i18n.str(29, ID));
+				} else {
+					UI.UIs.PrintMessage(message);
+					UI.UIs.PrintMessage(i18n.str(29, ID));
+				}
 
 				if(exit) Environment.Exit(1);
 			} else {
@@ -94,9 +101,10 @@ namespace Pigmeo.Compiler {
 		static ErrorsAndWarnings() {
 			//internals
 			ErrWarns.Add("INT0001", "Unknown exception");
-			ErrWarns.Add("INT0002", "Unknown error");
+			ErrWarns.Add("INT0002", "Unknown error or warning");
 			ErrWarns.Add("INT0003", "Unimplemented");
 			ErrWarns.Add("INT0004", "System.Windows.Forms libraries not available. Switching to console-based interface");
+			ErrWarns.Add("INT0005", "Invalid compilation progress value. Min=0, Max=100");
 
 			//warnings
 			ErrWarns.Add("W0001", "You are using an old config file version");
