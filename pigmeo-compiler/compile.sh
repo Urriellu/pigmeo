b13078024954b5177ac618528cc86c899ef0ae33
urriellu <pigmeo@urriellu.net>
2008-03-14 19:15:57 +0000

    Supporting Pigmeo.Extensions.dll

--- pigmeo-compiler/compile.sh
+++ pigmeo-compiler/compile.sh
@@ -7,7 +7,7 @@ cd $MyPath
 
 
 echo "Building the Pigmeo Compiler"
-	gmcs -t:winexe -langversion:linq -lib:../references,../output,../output/GAC -r:System.Core.dll,System.Data.dll,System.Drawing.dll,System.Windows.Forms.dll,Mono.Cecil.dll,Pigmeo.dll,Pigmeo.Internal.dll -doc:../output/GAC/pigmeo-compiler.xml -out:../output/pigmeo-compiler.exe src/*.cs src/BackendPIC14/*.cs src/BackendPIC14/instructions/*.cs src/UI/*.cs src/UI/WinForms/*.cs
+	gmcs -t:winexe -langversion:linq -lib:../references,../output,../output/GAC -r:System.Core.dll,System.Data.dll,System.Drawing.dll,System.Windows.Forms.dll,Mono.Cecil.dll,Pigmeo.dll,Pigmeo.Internal.dll,Pigmeo.Extensions.dll -doc:../output/GAC/pigmeo-compiler.xml -out:../output/pigmeo-compiler.exe src/*.cs src/BackendPIC14/*.cs src/BackendPIC14/instructions/*.cs src/UI/*.cs src/UI/WinForms/*.cs
 echo -e "\t[DONE]"
 
 
