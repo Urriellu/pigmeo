85ce519b140093860187ea7bdfff4feb1b92ccf5
urriellu <pigmeo@urriellu.net>
2008-03-17 14:08:10 +0000

    Internationalizating texts and removing obsolete comments

--- pigmeo-compiler/src/CmdLine.cs
+++ pigmeo-compiler/src/CmdLine.cs
@@ -21,82 +21,84 @@ using Pigmeo.Internal;
 
 namespace Pigmeo.Compiler {
 
-	/// <summary>Parses the parameters sent through the command line</summary>
+	/// <summary>
+	/// Parses the parameters sent through the command line
+	/// </summary>
 	public class CmdLine {
 
-		
+
 		public static void ParseParams(string[] args) {
-			/*if (args.Length == 0) Usage();
-			else {*/
-				Queue q = new Queue(args);
-				while (q.Count > 0) {
-					string token = (string) q.Dequeue ();
-	
-					if (token.Length < 1) Usage ();
+			Queue q = new Queue(args);
+			while(q.Count > 0) {
+				string token = (string)q.Dequeue();
 
-					if (token [0] == '-' && token [1] == '-') {
-						if (token.Length < 3) Usage ();
+				if(token.Length < 1) Usage();
 
-						token=token.Substring(2);
+				if(token[0] == '-' && token[1] == '-') {
+					if(token.Length < 3) Usage();
 
-						switch(token) {
-							case "about":
-								About();
-								break;
-							case "debug":
-								config.Internal.Verbosity = VerbosityLevel.Debug;
-								break;
-							case "help":
-								Usage();
-								break;
-							case "ui":
-								goto case "UI";
-							case "UI":
-								string ChoosenUI = (string)q.Dequeue();
-								try {
-									config.Internal.UI = (UserInterface)Enum.Parse(typeof(UserInterface), ChoosenUI, true);
-								} catch {
-									UnknownParam("--UI " + ChoosenUI);
-								}
-								break;
-							case "verbose":
-								config.Internal.Verbosity = VerbosityLevel.Verbose;
-								break;
-							case "version":
-								Version();
-								break;
-							default:
-								UnknownParam(token);
-								break;
-						}
-					}
-	
-					if (token [0] == '-' || token [0] == '/') {
-						token = token.Substring(1);
+					token = token.Substring(2);
 
-						switch(token) {
-							case "v":
-								config.Internal.Verbosity = VerbosityLevel.Verbose;
-								break;
-							case "h":
-								Usage();
-								break;
-							default:
-								UnknownParam(token);
-								break;
-						}
-					} else {
-						config.Internal.UserApp = config.Internal.WorkingDirectory + "/" + token;
+					switch(token) {
+						case "about":
+							About();
+							break;
+						case "debug":
+							config.Internal.Verbosity = VerbosityLevel.Debug;
+							break;
+						case "help":
+							Usage();
+							break;
+						case "quiet":
+							config.Internal.Verbosity = VerbosityLevel.Quiet;
+							break;
+						case "ui":
+							goto case "UI";
+						case "UI":
+							string ChoosenUI = (string)q.Dequeue();
+							try {
+								config.Internal.UI = (UserInterface)Enum.Parse(typeof(UserInterface), ChoosenUI, true);
+							} catch {
+								UnknownParam("--UI " + ChoosenUI);
+							}
+							break;
+						case "verbose":
+							config.Internal.Verbosity = VerbosityLevel.Verbose;
+							break;
+						case "version":
+							Version();
+							break;
+						default:
+							UnknownParam(token);
+							break;
 					}
 				}
-			//}
+
+				if(token[0] == '-' || token[0] == '/') {
+					token = token.Substring(1);
+
+					switch(token) {
+						case "v":
+							config.Internal.Verbosity = VerbosityLevel.Verbose;
+							break;
+						case "h":
+							Usage();
+							break;
+						default:
+							UnknownParam(token);
+							break;
+					}
+				} else {
+					config.Internal.UserApp = config.Internal.WorkingDirectory + "/" + token;
+				}
+			}
 		}
 
 		/// <summary>
 		/// Prints a message saying that an unknown parameter was found
 		/// </summary>
 		static void UnknownParam(string str){
-			Console.WriteLine ("Unknown parameter: {0}", str);
+			Console.WriteLine (i18n.str(101, str));
 			Console.WriteLine();
 			Usage();
 			Environment.Exit(1);
@@ -105,37 +107,42 @@ namespace Pigmeo.Compiler {
 		/// <summary>
 		/// Explains how the executable must be called
 		/// </summary>
-		static void Usage() {
-			Console.WriteLine(config.Internal.AppName + " version " + config.Internal.AppVersion);
-			Console.WriteLine("pigmeo [options] UserApp.exe");
+		public static void Usage() {
+			Console.WriteLine(config.Internal.AppName + " " + config.Internal.AppVersion);
+			Console.WriteLine(i18n.str(102));
 
-			Console.WriteLine("\t--about\t\tShow information about the {0} and exit", config.Internal.AppName);
-			Console.WriteLine("\t--debug\t\tGet much more info. Useful only for developers");
-			Console.WriteLine("\t-h, --help\tDisplay this help and exit");
-			Console.WriteLine("\t--ui VALUE, --UI VALUE\tChoose the user interface. Value can be Console or WinForms. Case insensitive");
-			Console.WriteLine("\t-v, --verbose\tGet more info when running the compiler");
-			Console.WriteLine("\t--version\tPrint the version number of the {0}", config.Internal.AppName);
-			Console.WriteLine("");
-			Console.WriteLine("\tSample: pigmeo MyApp.exe");
+			Console.WriteLine(i18n.str(103, config.Internal.AppName));
+			Console.WriteLine(i18n.str(104));
+			Console.WriteLine(i18n.str(105));
+			Console.WriteLine(i18n.str(106));
+			Console.WriteLine(i18n.str(107));
+			Console.WriteLine(i18n.str(108));
+			Console.WriteLine(i18n.str(109, config.Internal.AppName));
+			Console.WriteLine();
+			Console.WriteLine(i18n.str(110));
 
 			Environment.Exit(1);
 		}
 
-		/// <summary>Prints the version of the application</summary>
+		/// <summary>
+		/// Prints the version of the application
+		/// </summary>
 		static void Version () {
 			Console.WriteLine ("{0} {1}", config.Internal.AppName, config.Internal.AppVersion);
 			Environment.Exit (1);
 		}
 
-		/// <summary>Prints the some information about the application</summary>
+		/// <summary>
+		/// Prints the some information about the application
+		/// </summary>
 		static void About () {
-			Console.WriteLine("{0}, the CIL compiler from {1} project", config.Internal.AppName, config.Internal.PrjName);
+			Console.WriteLine(i18n.str(111, config.Internal.AppName, config.Internal.PrjName)); //title
 			Console.WriteLine(i18n.str(7)); //description
 			Console.WriteLine(i18n.str(8)); //developers
 			foreach(string developer in config.Internal.Developers.Split('\n')) {
 				Console.WriteLine("\t{0}", developer);
 			}
-			Console.WriteLine(i18n.str(9));
+			Console.WriteLine(i18n.str(9)); //more info
 			Console.WriteLine ("\t{0}", config.Internal.PrjWebsite);
 			Environment.Exit (1);
 		}
