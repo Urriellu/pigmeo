70cb2d313932df253ef08528eb8767dfc65b5bec
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-18 10:50:13 +0000

    Architecture.PIC16 is now Architecture.PIC14

--- trunk/pigmeo-framework/src/devices/PIC16F59.cs
+++ trunk/pigmeo-framework/src/devices/PIC16F59.cs
@@ -2,7 +2,7 @@
 
 namespace Pigmeo.MCU {
 	public static class Target {
-		public const Architecture TargetArch = Architecture.PIC16;
+		public const Architecture TargetArch = Architecture.PIC14;
 		public const Branch TargetBranch = Branch.PIC16F59;
 	}
 }
