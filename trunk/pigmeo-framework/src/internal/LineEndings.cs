15968a85a2a1acb8158251f49f986f707e5b4348
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-20 12:53:02 +0000

    adding LineEndingsArray

--- trunk/pigmeo-framework/src/internal/LineEndings.cs
+++ trunk/pigmeo-framework/src/internal/LineEndings.cs
@@ -3,5 +3,7 @@
 		public const string Unix = "\n"; //New Line/Line Feed
 		public const string Windows = "\r\n"; //Carriage Return + New Line/Line Feed
 		public const string MacOS9 = "\r"; //Carriage Return
+
+		public static string[] LineEndingsArray = { Windows, Unix, MacOS9 };
 	}
 }
