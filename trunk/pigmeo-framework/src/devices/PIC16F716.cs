7b602dccc45ddd77a4c534f4d298bdd57ac96325
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-28 14:55:59 +0000

    Supporting more registers

--- trunk/pigmeo-framework/src/devices/PIC16F716.cs
+++ trunk/pigmeo-framework/src/devices/PIC16F716.cs
@@ -35,12 +35,23 @@ namespace Pigmeo.MCU {
 	}
 
 	public static class Registers {
-		[AsmName("OriginalNameRemoveThisPORTA"), Location(0,0x05)]
-		public static byte PORTA = 0;
+		/// <summary>
+		/// Addressing this location uses contents of FSR to address data memory (not a physical register)
+		/// </summary>
+		[AsmName("INDF"), Location(0, 0x00)]
+		public static byte INDF = 0;
 
+		/// <summary>
+		/// Timer0 module's register
+		/// </summary>
+		[AsmName("TMR0"), Location(0, 0x01)]
+		public static byte TMR0 = 0;
 
-		[AsmName("PORTB"), Location(0, 0x06)]
-		public static byte PORTB = 0;
+		/// <summary>
+		/// Program Counter's (PC) Least Significant Byte
+		/// </summary>
+		[AsmName("PCL"), Location(0, 0x02)]
+		public static byte PCL = 0;
 
 		/// <summary>
 		/// Contains the arithmetic status of the ALU, the Reset status and the bank select bits for data memory
@@ -70,22 +81,84 @@ namespace Pigmeo.MCU {
 			/// <remarks>For Borrow, the polarity is reversed. A subtraction is executed by adding the two's complement of the second operand. For rotate (RRF, RLF) instructions, this bit is loaded with either the high-order or low-order bit of the source register.</remarks>
 			[Location(0, 3, 0)]
 			public static bool C;
-
-/*			public static implicit operator STATUS(byte value){
-				RP0=value.GetBit(5);
-				//...
-			}*/
-	}
-
-	/*public static class PORTA {
-		public static bool this[byte bit] {
-			get {
-				return Registers.PORTA.GetBit(bit);
-			}
 		}
 
-		public static implicit operator PORTA(byte value) {
-			Registers.PORTA = value;
-		}*/
+		/// <summary>
+		/// Indirect Data Memory Address Pointer
+		/// </summary>
+		[AsmName("FSR"), Location(0, 0x04)]
+		public static byte FSR = 0;
+
+		[AsmName("OriginalNameRemoveThisPORTA"), Location(0,0x05)]
+		public static byte PORTA = 0;
+
+
+		[AsmName("PORTB"), Location(0, 0x06)]
+		public static byte PORTB = 0;
+
+		/// <summary>
+		/// Write Buffer for the upper 5 bits of the Program Counter
+		/// </summary>
+		[AsmName("PCLATH"), Location(0, 0x0A)]
+		public static byte PCLATH = 0;
+
+		/// <summary>
+		/// Contains various enable and flag bits for the TMR0 register overflow, RB Port change and external RB0/INT pin interrupts
+		/// </summary>
+		public static class INTCON {
+			/// <summary>
+			/// Global Interrupt Enable bit. true = Enables all unmasked interrupts. false = Disables all interrupts
+			/// </summary>
+			[Location(0, 0x0B, 7)]
+			public static bool GIE = false;
+
+			/// <summary>
+			/// Peripheral Interrupt Enable bit. true = Enables all unmasked peripheral interrupts. false = Disables all peripheral interrupts
+			/// </summary>
+			[Location(0, 0x0B, 6)]
+			public static bool PEIE = false;
+
+			/// <summary>
+			/// Timer0 Overflow Interrupt Enable bit. true = Enables the Timer0 interrupt. false = Disables the Timer0 interrupt
+			/// </summary>
+			[Location(0, 0x0B, 5)]
+			public static bool T0IE = false;
+
+			/// <summary>
+			/// RB0/INT External Interrupt Enable bit. true = Enables the RB0/INT external interrupt. false = Disables the Timer0 interrupt
+			/// </summary>
+			[Location(0, 0x0B, 4)]
+			public static bool INTE = false;
+
+			/// <summary>
+			/// PORTB Change Interrupt Enable bit. true = Enables the PORTB change interrupt. false = Disables the PORTB change interrupt
+			/// </summary>
+			/// <remarks>
+			/// IOCB register must also be enabled
+			/// </remarks>
+			[Location(0, 0x0B, 3)]
+			public static bool RBIE = false;
+
+			/// <summary>
+			/// Timer0 Overflow Interrupt Flag bit. true = TMR0 register has overflowed. false = TMR0 register did not overflow
+			/// </summary>
+			/// <remarks>
+			/// T0IF bit is set when Timer0 rolls over. Timer0 is unchanged on Reset and should be initialized before clearing T0IF bit
+			/// </remarks>
+			[Location(0, 0x0B, 2)]
+			public static bool T0IF = false;
+
+			/// <summary>
+			/// RB0/INT External Interrupt Flag bit. true = The RB0/INT external interrupt occurred. false = The RB0/INT external interrupt did not occur
+			/// </summary>
+			[Location(0, 0x0B, 1)]
+			public static bool INTF = false;
+
+			/// <summary>
+			///  PORTB Change Interrupt Flag bit. true = When at least one of the PORTB general purpose I/O pins changed state. false = None of the PORTB general purpose I/O pins have changed state
+			/// </summary>
+			[Location(0, 0x0B, 1)]
+			public static bool RBIF = false;
+		}
 	}
 }
