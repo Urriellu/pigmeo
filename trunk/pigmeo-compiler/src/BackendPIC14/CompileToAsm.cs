429541599b946ad157735d273a086dd0f0e97abd
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-03-29 16:40:12 +0000

    a bunch of updates

--- trunk/pigmeo-compiler/src/BackendPIC14/CompileToAsm.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/CompileToAsm.cs
@@ -4,14 +4,16 @@ using Pigmeo.Internal;
 using Pigmeo.Internal.PIC14;
 using System;
 using System.Collections.Generic;
+using Pigmeo.Compiler;
 
 namespace Pigmeo.Compiler.BackendPIC14 {
 	public static partial class CompileToAsm {
 		private static Asm AsmLangApp;
 		private static Asm AsmHeader;
 		private static Asm AsmDirectives;
-		private static List<CompiledStaticFunction> StaticFunctions = new List<CompiledStaticFunction>();
-		public static Dictionary<RegisterAddress, string> StaticVariables = new Dictionary<RegisterAddress, string>();
+		private static List<CompiledStaticFunction> StaticFunctions;
+		public static Dictionary<RegisterAddress, string> StaticVariables;
+		public static InfoPIC8bit TargetDeviceInfo;
 
 		/// <summary>
 		/// Compiles the whole .NET assembly to assembly language
@@ -21,6 +23,9 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 		public static Asm Run(AssemblyDefinition assembly) {
 			ShowInfo.InfoDebug("Compiling to 8-bit PIC assembly language");
 			AsmLangApp = new Asm();
+			StaticFunctions = new List<CompiledStaticFunction>();
+			StaticVariables = new Dictionary<RegisterAddress, string>();
+			TargetDeviceInfo = config.Compilation.TargetDeviceInfo as InfoPIC8bit;
 
 			#region compile all the parts
 			ShowInfo.InfoDebug("Building header");
@@ -45,30 +50,14 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 
 			ShowInfo.InfoDebug("Adding global variables");
 			foreach(KeyValuePair<RegisterAddress,string> kv in StaticVariables) {
-				switch(config.Internal.NumeralSystem) {
-					case NumeralSystems.Binary:
-						AsmLangApp.Instructions.Add(new EQU(kv.Value, "B'" + Convert.ToString(kv.Key.Address, 2) + "'", ""));
-						break;
-					case NumeralSystems.Decimal:
-						AsmLangApp.Instructions.Add(new EQU(kv.Value, "D'" + Convert.ToString(kv.Key.Address, 10) + "'", ""));
-						break;
-					case NumeralSystems.Hexadecimal:
-						AsmLangApp.Instructions.Add(new EQU(kv.Value, "0x" + kv.Key.Address.ToString("X2"), ""));
-						break;
-					case NumeralSystems.Octal:
-						AsmLangApp.Instructions.Add(new EQU(kv.Value, "O'" + Convert.ToString(kv.Key.Address, 8) + "'", ""));
-						break;
-					default:
-						ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "BE0002", false, config.Internal.NumeralSystem.ToString());
-						break;
-				}
+				AsmLangApp.Instructions.Add(new EQU(kv.Value, kv.Key.Address.ToAsmString(), ""));
 			}
 			AddAsmSeparator(AsmLangApp);
 
 			ShowInfo.InfoDebug("Adding EntryPoint and interrupts");
-			AsmLangApp.Instructions.Add(new ORG("0x00", ""));
-			AsmLangApp.Instructions.Add(new GOTO("", "EntryPoint", ""));
-			AsmLangApp.Instructions.Add(new ORG("0x04", "interruption vector"));
+			AsmLangApp.Instructions.Add(new ORG(((byte)0).ToAsmString(), ""));
+			AsmLangApp.Instructions.Add(new GOTO("", assembly.EntryPoint.Name, ""));
+			AsmLangApp.Instructions.Add(new ORG(((byte)4).ToAsmString(), "interruption vector"));
 			AsmLangApp.Instructions.Add(new GOTO("", "EndOfApp", "Interruptions not implemented"));
 			AddAsmSeparator(AsmLangApp);
 
@@ -100,7 +89,8 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 			AsmHeader.Instructions.Add(new Label("", i18n.str(132, config.Internal.AppName, config.Internal.AppVersion)));
 			AsmHeader.Instructions.Add(new Label("", i18n.str(133, config.Internal.PrjWebsite)));
 			AsmHeader.Instructions.Add(new Label("", " "));
-			AsmHeader.Instructions.Add(new Label("", " " + config.Internal.UserApp + " => " + config.Internal.FileAsm));
+			AsmHeader.Instructions.Add(new Label("", i18n.str(139, config.Internal.UserApp)));
+			AsmHeader.Instructions.Add(new Label("", i18n.str(140, config.Internal.FileAsm)));
 			AsmHeader.Instructions.Add(new Label("", " " + String.Format("{0:F}", DateTime.Now)));
 			AsmHeader.Instructions.Add(new Label("", " ========================================================================================"));
 		}
@@ -110,7 +100,8 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 		/// </summary>
 		private static void BuildAsmDirectives(AssemblyDefinition assembly) {
 			AsmDirectives = new Asm();
-			AsmDirectives.Instructions.Add(new INCLUDE("MODEL.INC", ""));
+			AsmDirectives.Instructions.Add(new INCLUDE(TargetDeviceInfo.IncludeFile, ""));
+			AsmDirectives.Instructions.Add(new PROCESSOR(TargetDeviceInfo.branch.ToString(), ""));
 		}
 
 		/// <summary>
