51f90febf0e8ff1fd6f84c93338ae30f2a02b4fe
urriellu <pigmeo@urriellu.net>
2008-03-31 15:38:44 +0000

    removing -langversion:linq parameter from compilation scripts

--- pigmeo-framework/src/internal/compile.sh
+++ pigmeo-framework/src/internal/compile.sh
@@ -7,7 +7,7 @@ cd $MyPath
 
 
 echo -e "\tBuilding the (internal) global things of the Pigmeo Framework"
-	gmcs -t:library -langversion:linq -lib:../../../references,../../../output/GAC -r:Mono.Cecil.dll,System.Core.dll,Pigmeo.dll,Pigmeo.Extensions.dll  -doc:../../../output/GAC/Pigmeo.Internal.xml -out:../../../output/GAC/Pigmeo.Internal.dll *.cs PIC14/*.cs
+	gmcs -t:library -lib:../../../references,../../../output/GAC -r:Mono.Cecil.dll,System.Core.dll,Pigmeo.dll,Pigmeo.Extensions.dll  -doc:../../../output/GAC/Pigmeo.Internal.xml -out:../../../output/GAC/Pigmeo.Internal.dll *.cs PIC14/*.cs
 echo -e "\t\t[DONE]"
 
 
