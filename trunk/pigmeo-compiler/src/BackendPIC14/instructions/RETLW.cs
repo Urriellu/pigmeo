77ffb5425525e2cd559edb74d5de6d678ff170f5
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-03 11:12:04 +0000

    More documentation

--- trunk/pigmeo-compiler/src/BackendPIC14/instructions/RETLW.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/instructions/RETLW.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// The W register is loaded with the eight bit literal "k". The program counter is loaded from the top of the stack (the return address). This is a two-cycle instruction
+	/// </summary>
 	public class RETLW:AsmInstruction {
 		/// <summary>
 		/// The W register is loaded with the eight bit literal "k". The program counter is loaded from the top of the stack (the return address). This is a two-cycle instruction
