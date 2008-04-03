bba29443a756c5f8cfff2d3b74953bb41b36730d
urriellu <pigmeo@urriellu.net>
2008-04-03 11:12:25 +0000

    More documentation

--- pigmeo-compiler/src/BackendPIC14/instructions/WHILE.cs
+++ pigmeo-compiler/src/BackendPIC14/instructions/WHILE.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// Program lines between WHILE and ENDW would be executed as long as condition was met. If a condition stopped being valid, program would continue executing instructions following ENDW line. Number of instructions between WHILE and ENDW can be 100 at the most, and number of executions 256
+	/// </summary>
 	public class WHILE:AsmInstruction {
 		/// <summary>
 		/// Program lines between WHILE and ENDW would be executed as long as condition was met. If a condition stopped being valid, program would continue executing instructions following ENDW line. Number of instructions between WHILE and ENDW can be 100 at the most, and number of executions 256
