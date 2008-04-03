0c3f012ee905239e6495e87ea7cdef383b3a589c
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-03 11:09:54 +0000

    More documentation

--- trunk/pigmeo-compiler/src/BackendPIC14/instructions/MOVF.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/instructions/MOVF.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// The contents of register f is moved to a destination dependent upon the status of d. d = F is useful to test a file register since status flag Z is affected
+	/// </summary>
 	public class MOVF:AsmInstruction {
 		/// <summary>
 		/// The contents of register f is moved to a destination dependent upon the status of d. d = F is useful to test a file register since status flag Z is affected
