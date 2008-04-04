d88e7a8dc566bf2bdc4b70a3b3c5e3afb766358c
urriellu <pigmeo@urriellu.net>
2008-04-04 19:25:22 +0000

    More documentation

--- pigmeo-compiler/src/BackendPIC14/instructions/CBLOCK.cs
+++ pigmeo-compiler/src/BackendPIC14/instructions/CBLOCK.cs
@@ -1,4 +1,8 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// It is used to give values to named constants
+	/// </summary>
+	/// <param name="FirstValue">Starting value</param>
 	public class CBLOCK:AsmInstruction {
 		/// <summary>
 		/// It is used to give values to named constants
