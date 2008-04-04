f18262aa9bb43ded696e420a2f4730625a7996d0
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-04 14:47:03 +0000

    More documentation

--- trunk/pigmeo-compiler/src/BackendPIC14/instructions/INCFSZ.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/instructions/INCFSZ.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// The contents of register "f" are incremented. If the result is "1", the next instruction is executed. If the result is "0", a NOP is executed instead, making it a two-cycle instruction
+	/// </summary>
 	public class INCFSZ:AsmInstruction {
 		/// <summary>
 		/// The contents of register "f" are incremented. If the result is "1", the next instruction is executed. If the result is "0", a NOP is executed instead, making it a two-cycle instruction
