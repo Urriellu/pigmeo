51f093a516203215f2391c4ab3bdae948fb56191
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-04 14:50:50 +0000

    More documentation

--- trunk/pigmeo-compiler/src/BackendPIC14/instructions/IFNDEF.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/instructions/IFNDEF.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// If designation FirstValue was not previously defined, or if its definition was erased with directive UNDEFINE, instructions which follow would be executed until ELSE or ENDIF directives would be reached
+	/// </summary>
 	public class IFNDEF:AsmInstruction {
 		/// <summary>
 		/// If designation FirstValue was not previously defined, or if its definition was erased with directive UNDEFINE, instructions which follow would be executed until ELSE or ENDIF directives would be reached
