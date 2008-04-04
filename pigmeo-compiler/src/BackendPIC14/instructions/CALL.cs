b8babb5acb6ec7ebb0b672cbed0fa5c07dca19f7
urriellu <pigmeo@urriellu.net>
2008-04-04 19:24:53 +0000

    More documentation

--- pigmeo-compiler/src/BackendPIC14/instructions/CALL.cs
+++ pigmeo-compiler/src/BackendPIC14/instructions/CALL.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// Call Subroutine. First, return address (PC + 1) is pushed onto the stack. The eleven-bit immediate address is loaded into PC bits 10:0. The upper bits ofthe PC are loaded from PCLATH. CALL is a two-cycle instruction
+	/// </summary>
 	public class CALL:AsmInstruction {
 		/// <summary>
 		/// Call Subroutine. First, return address (PC + 1) is pushed onto the stack. The eleven-bit immediate address is loaded into PC bits 10:0. The upper bits ofthe PC are loaded from PCLATH. CALL is a two-cycle instruction
