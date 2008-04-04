09b03f87dcc0843507d539dbd27cfb44b488f23d
urriellu <pigmeo@urriellu.net>
2008-04-04 14:46:37 +0000

    More documentation

--- pigmeo-compiler/src/BackendPIC14/instructions/BTFSS.cs
+++ pigmeo-compiler/src/BackendPIC14/instructions/BTFSS.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// If bit "b" in register "f" is "0", the next instruction is executed. If bit "b" is "1", then the next instruction is discarded and a NOP is executed instead, making this a two-cycle instruction
+	/// </summary>
 	public class BTFSS:AsmInstruction {
 		/// <summary>
 		/// If bit "b" in register "f" is "0", the next instruction is executed. If bit "b" is "1", then the next instruction is discarded and a NOP is executed instead, making this a two-cycle instruction
