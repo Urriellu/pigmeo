4b8617a3f926af41e15918e957a99f849768416b
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-04 19:25:01 +0000

    More documentation

--- trunk/pigmeo-compiler/src/BackendPIC14/instructions/CLRF.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/instructions/CLRF.cs
@@ -1,4 +1,7 @@
 ﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// The contents of register "f" are cleared and the Z bit is set.
+	/// </summary>
 	public class CLRF:AsmInstruction {
 		/// <summary>
 		/// The contents of register "f" are cleared and the Z bit is set.
