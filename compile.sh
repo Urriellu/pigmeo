08723e2542f3b4f389d61f3004b3417b8a2f8f30
urriellu <pigmeo@urriellu.net>
2008-04-11 16:49:05 +0000

    Mono.Cecil.dll is now copied to the output directory from trunk/compile.sh

--- compile.sh
+++ compile.sh
@@ -11,9 +11,11 @@ echo Compiling...
 
 mkdir -p output/GAC
 
+cp references/Mono.Cecil.dll output/GAC		#we need Cecil in the GAC because it is required by other libraries that must be in the GAC
 ./pigmeo-framework/compile.sh
 ./pigmeo-compiler/compile.sh
 cp -a images output
+cp -a i18n output
 
 
 # cd into the directory where you were before running this script
