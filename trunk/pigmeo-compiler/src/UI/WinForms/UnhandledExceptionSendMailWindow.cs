ed1639d1ff7f8ed43d7e2dca5e2b868c16f270e4
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-12 09:38:45 +0000

    Adding unhandled exception window. WinForms

---
+++ trunk/pigmeo-compiler/src/UI/WinForms/UnhandledExceptionSendMailWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Pigmeo.Internal;
+
+namespace Pigmeo.Compiler.UI.WinForms {
+	public partial class UnhandledExceptionSendMailWindow:Form {
+		protected Exception ThrownException;
+
+		public UnhandledExceptionSendMailWindow(Exception e) {
+			InitializeComponent();
+			ThrownException = e;
+		}
+
+		private void UnhandledExceptionSendMailWindow_Load(object sender, EventArgs e) {
+			ShowInfo.InfoDebug("Instantiating a new Pigmeo.Compiler.UI.WinForms.UnhandledExceptionSendMailWindow");
+
+			#region global settings
+			LoadLanguageStrings();
+			txtMailContents.Text = UnknownError.GenerateErrorReport(ThrownException);
+			#endregion
+
+			btnSend.Focus();
+		}
+
+		protected void LoadLanguageStrings() {
+			ShowInfo.InfoDebug("Loading language strings (WinForms unhandled exception mail sender interface)");
+
+			this.Text = i18n.str("unknown_error");
+			lblDescription.Text = i18n.str("lbl_description");
+			btnSend.Text = i18n.str("Send");
+			btnIgnore.Text = i18n.str("Ignore");
+		}
+
+		private void btnIgnore_Click(object sender, EventArgs e) {
+			this.Close();
+		}
+	}
+}
