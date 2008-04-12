e1efea543e08bf5812e75f25c78fbead64673b84
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-12 11:28:49 +0000

    Internationalizating. Resizing controls on-the-fly. Sending mail using SMTP

--- trunk/pigmeo-compiler/src/UI/WinForms/UnhandledExceptionSendMailWindow.cs
+++ trunk/pigmeo-compiler/src/UI/WinForms/UnhandledExceptionSendMailWindow.cs
@@ -33,12 +33,46 @@ namespace Pigmeo.Compiler.UI.WinForms {
 
 			this.Text = i18n.str("unknown_error");
 			lblDescription.Text = i18n.str("lbl_description");
+			lblSendReport.Text = i18n.str("lbl_send_report");
+			lblPrivateInfo.Text = i18n.str("lbl_PrivateInfo");
 			btnSend.Text = i18n.str("Send");
 			btnIgnore.Text = i18n.str("Ignore");
+
+			ResizeControls();
+		}
+
+		protected void ResizeControls() {
+			ShowInfo.InfoDebug("Resizing controls (WinForms unhandled exception mail sender interface)");
+
+			Label WidestLabel = lblDescription;
+			if(lblPrivateInfo.Size.Width > WidestLabel.Size.Width) WidestLabel = lblPrivateInfo;
+			if(lblSendReport.Size.Width > WidestLabel.Size.Width) WidestLabel = lblSendReport;
+			this.MinimumSize = new Size(WidestLabel.Location.X + WidestLabel.Size.Width + 5, 300);
 		}
 
 		private void btnIgnore_Click(object sender, EventArgs e) {
+			ShowInfo.InfoDebug("Ignoring unknown error");
 			this.Close();
+		}
+
+		[PigmeoToDo("find a public SMTP server, build our own, or send bug reports by other way")]
+		private void btnSend_Click(object sender, EventArgs e) {
+			ShowInfo.InfoDebug("Sending e-mail to developers about an unknown error");
+
+			try {
+				System.Net.Mail.MailMessage message = new System.Net.Mail.MailMessage();
+				message.To.Add("bug-reports@pigmeo.org");
+				message.Subject = "Unknown error";
+				message.From = new System.Net.Mail.MailAddress("PigmeoCompiler@pigmeo.org");
+				message.Body = txtMailContents.Text;
+
+				System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient("localhost");
+				smtp.Send(message);
+			} catch {
+				MessageBox.Show(i18n.str("UnableSendBug"), i18n.str(12), MessageBoxButtons.OK, MessageBoxIcon.Error);
+			} finally {
+				this.Close();
+			}
 		}
 	}
 }
