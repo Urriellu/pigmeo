63953bed5a417f2dc79d143b43e0fb0a97854e92
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-19 11:48:44 +0000

    adding enums TimeUnits and TimeUnitsSI

---
+++ trunk/pigmeo-framework/src/Physics/TimeUnits.cs
@@ -0,0 +1,28 @@
+﻿namespace Pigmeo.Physics {
+	/// <summary>
+	/// Units in which time is measured
+	/// </summary>
+	public enum TimeUnits {
+		second,
+		minute,
+		hour,
+	}
+
+	/// <summary>
+	/// Units in which time is measured. International System of Units
+	/// </summary>
+	public enum TimeUnitsSI {
+		/// <summary>
+		/// Microsecond. 10^-6 seconds
+		/// </summary>
+		µs,
+		/// <summary>
+		/// Millisecond. 10^-3 seconds
+		/// </summary>
+		ms,
+		/// <summary>
+		/// Second
+		/// </summary>
+		s
+	}
+}
