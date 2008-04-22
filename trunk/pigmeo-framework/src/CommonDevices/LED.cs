54012e5bf41a6810bd36b8b9748306649889e068
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-04-22 19:16:20 +0000

    More documentation. Status checker was fixed

--- trunk/pigmeo-framework/src/CommonDevices/LED.cs
+++ trunk/pigmeo-framework/src/CommonDevices/LED.cs
@@ -1,11 +1,22 @@
 ﻿namespace Pigmeo.CommonDevices {
-	public enum LedStatus { OFF, ON }
-
+	/// <summary>
+	/// Light-Emitting Diode
+	/// </summary>
 	public class LED {
 		private Delegates.ReadBool ReadLed;
 		private Delegates.SetBool WriteLed;
+
+		/// <summary>
+		/// Digital logic in which the LED works
+		/// </summary>
 		public readonly LogicType logic;
 
+		/// <summary>
+		/// Instantiates a new LED object
+		/// </summary>
+		/// <param name="LedReader">Delegate that reads the LED status</param>
+		/// <param name="LedWriter">Delegate that writes the LED status</param>
+		/// <param name="logic">Digital logic in which the LED works</param>
 		public LED(Delegates.ReadBool LedReader, Delegates.SetBool LedWriter, LogicType logic) {
 			ReadLed = LedReader;
 			WriteLed = LedWriter;
@@ -15,32 +26,45 @@
 		/// <summary>
 		/// Gets or sets the LED status
 		/// </summary>
-		public LedStatus Status {
+		public OnOffStatus Status {
 			get {
-				if(ReadLed.Invoke()) return LedStatus.ON;
-				else return LedStatus.OFF;
+				bool BitValue = ReadLed.Invoke();
+				if((BitValue && logic == LogicType.Positive) || (!BitValue && logic == LogicType.Negative)) return OnOffStatus.ON;
+				else return OnOffStatus.OFF;
 			}
 			set {
-				if((logic == LogicType.Positive && value == LedStatus.ON) || logic == LogicType.Negative && value == LedStatus.OFF) WriteLed.Invoke(true);
+				if((logic == LogicType.Positive && value == OnOffStatus.ON) || logic == LogicType.Negative && value == OnOffStatus.OFF) WriteLed.Invoke(true);
 				else WriteLed.Invoke(false);
 			}
 		}
 
+		/// <summary>
+		/// Turns ON the LED
+		/// </summary>
 		public void TurnON() {
-			this.Status = LedStatus.ON;
+			this.Status = OnOffStatus.ON;
 		}
 
+		/// <summary>
+		/// Turns OFF the LED
+		/// </summary>
 		public void TurnOFF() {
-			this.Status = LedStatus.OFF;
+			this.Status = OnOffStatus.OFF;
 		}
 
+		/// <summary>
+		/// Indicates if the LED is lighting
+		/// </summary>
 		public bool IsON {
 			get {
-				if(this.Status == LedStatus.ON) return true;
-				return false;
+				if(this.Status == OnOffStatus.ON) return true;
+				else return false;
 			}
 		}
 
+		/// <summary>
+		/// Indicates if the LED is OFF (not lighting)
+		/// </summary>
 		public bool IsOFF {
 			get {
 				return !IsON;
