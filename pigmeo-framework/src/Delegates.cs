0ae0dfba526e625758ad0fabdccff4e31dd453ed
urriellu <pigmeo@urriellu.net>
2008-04-22 18:00:37 +0000

    adding Delegates.cs

---
+++ pigmeo-framework/src/Delegates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo {
+	public static class Delegates {
+		/// <summary>
+		/// Reads a variable and returns a boolean value
+		/// </summary>
+		public delegate bool ReadBool();
+
+		/// <summary>
+		/// Sets a bool variable to a given value
+		/// </summary>
+		/// <returns></returns>
+		public delegate void SetBool(bool value);
+	}
+}
