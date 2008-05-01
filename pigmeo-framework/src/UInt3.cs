1b5b5cdc4d45ef808c4528bf3de1e4e29ac7067a
urriellu <pigmeo@urriellu.net>
2008-05-01 18:56:54 +0000

    Supporting additions and conversions to and from Byte

--- pigmeo-framework/src/UInt3.cs
+++ pigmeo-framework/src/UInt3.cs
@@ -1,4 +1,7 @@
-﻿namespace Pigmeo {
+﻿using System;
+using Pigmeo.Extensions;
+
+namespace Pigmeo {
 	public struct UInt3 {
 		public bool Bit0;
 		public bool Bit1;
@@ -10,17 +13,36 @@
 			this.Bit2 = Bit2;
 		}
 
+		public UInt3(byte b) {
+			this = (UInt3)b;
+		}
+
 		public string ToBinaryString() {
 			return string.Format("{0}:X", Bit2) + string.Format("{0}:X", Bit1) + string.Format("{0}:X", Bit0);
 		}
 
-		#region operators
+		#region operator overloading
+		public static UInt3 operator +(UInt3 a, UInt3 b) {
+			return (UInt3)((byte)a + (byte)b);
+		}
+
+		public static UInt3 operator ++(UInt3 a) {
+			return (UInt3)(((byte)a)+1);
+		}
+		#endregion
+
+		#region conversions
 		public static implicit operator byte(UInt3 n) {
 			byte v = 0;
-			if(n.Bit0) v+=1;
+			if(n.Bit0) v += 1;
 			if(n.Bit1) v += 2;
 			if(n.Bit2) v += 4;
 			return v;
+		}
+
+		public static explicit operator UInt3(byte n) {
+			if(n > 7) throw new OverflowException("Value too high");
+			return new UInt3(n.GetBit(2), n.GetBit(1), n.GetBit(0));
 		}
 		#endregion
 	}
