3e69a2475179b8b9a907f6a1afb24c105270708c
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-05-01 18:57:54 +0000

    CommonDevices/LED.cs is now write-only

--- trunk/pigmeo-framework/src/CommonDevices/LED.cs
+++ trunk/pigmeo-framework/src/CommonDevices/LED.cs
@@ -1,9 +1,8 @@
 ﻿namespace Pigmeo.CommonDevices {
 	/// <summary>
-	/// Light-Emitting Diode
+	/// Light-Emitting Diode (write-only)
 	/// </summary>
 	public class LED {
-		private Delegates.ReadBool ReadLed;
 		private Delegates.SetBool WriteLed;
 
 		/// <summary>
@@ -14,24 +13,17 @@
 		/// <summary>
 		/// Instantiates a new LED object
 		/// </summary>
-		/// <param name="LedReader">Delegate that reads the LED status</param>
 		/// <param name="LedWriter">Delegate that writes the LED status</param>
 		/// <param name="logic">Digital logic in which the LED works</param>
-		public LED(Delegates.ReadBool LedReader, Delegates.SetBool LedWriter, LogicType logic) {
-			ReadLed = LedReader;
+		public LED(Delegates.SetBool LedWriter, LogicType logic) {
 			WriteLed = LedWriter;
 			this.logic = logic;
 		}
 
 		/// <summary>
-		/// Gets or sets the LED status
+		/// Sets the LED status
 		/// </summary>
 		public OnOffStatus Status {
-			get {
-				bool BitValue = ReadLed.Invoke();
-				if((BitValue && logic == LogicType.Positive) || (!BitValue && logic == LogicType.Negative)) return OnOffStatus.ON;
-				else return OnOffStatus.OFF;
-			}
 			set {
 				if((logic == LogicType.Positive && value == OnOffStatus.ON) || logic == LogicType.Negative && value == OnOffStatus.OFF) WriteLed.Invoke(true);
 				else WriteLed.Invoke(false);
@@ -50,25 +42,6 @@
 		/// </summary>
 		public void TurnOFF() {
 			this.Status = OnOffStatus.OFF;
-		}
-
-		/// <summary>
-		/// Indicates if the LED is lighting
-		/// </summary>
-		public bool IsON {
-			get {
-				if(this.Status == OnOffStatus.ON) return true;
-				else return false;
-			}
-		}
-
-		/// <summary>
-		/// Indicates if the LED is OFF (not lighting)
-		/// </summary>
-		public bool IsOFF {
-			get {
-				return !IsON;
-			}
 		}
 	}
 }
