b9672c15bf6360bb51eecff58868caa41781f745
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-05-04 17:51:40 +0000

    New ITimerInterrupts.cs

---
+++ trunk/pigmeo-framework/src/ITimerInterrups.cs
@@ -0,0 +1,10 @@
+﻿using Pigmeo.Physics;
+
+namespace Pigmeo {
+	/// <summary>
+	/// Timer that executes an event periodically
+	/// </summary>
+	public interface ITimerInterrupts:ITimer {
+		void Configure(float frequency, SIPrefixes FreqPrefix, FrequencyUnits FreqUnit, float Period, SIPrefixes PerPrefix, TimeUnits PeriodUnit);
+	}
+}
