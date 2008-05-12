a99033cb6d5dab2c00e10becd94896fd65255eeb
urriellu <pigmeo@urriellu.net>
2008-05-12 16:41:56 +0000

    Supporting more TimeUnitsSI

--- pigmeo-framework/src/Physics/TimeUnits.cs
+++ pigmeo-framework/src/Physics/TimeUnits.cs
@@ -13,6 +13,33 @@
 	/// </summary>
 	public enum TimeUnitsSI {
 		/// <summary>
+		/// Yoctosecond. 10^-23 seconds
+		/// </summary>
+		ys,
+		/// <summary>
+		/// Zeptosecond. 10^-21 seconds
+		/// </summary>
+		zs,
+		/// <summary>
+		/// Attoosecond. 10^-18 seconds
+		/// </summary>
+		/// <remarks>
+		/// "as" is a special keyword
+		/// </remarks>
+		attos,
+		/// <summary>
+		/// Femtosecond. 10^-15 seconds
+		/// </summary>
+		fs,
+		/// <summary>
+		/// Picosecond. 10^-12 seconds
+		/// </summary>
+		ps,
+		/// <summary>
+		/// Nanosecond. 10^-9 seconds
+		/// </summary>
+		ns,
+		/// <summary>
 		/// Microsecond. 10^-6 seconds
 		/// </summary>
 		µs,
@@ -20,6 +47,14 @@
 		/// Millisecond. 10^-3 seconds
 		/// </summary>
 		ms,
+		/// <summary>
+		/// Centisecond. 10^-2 seconds
+		/// </summary>
+		cs,
+		/// <summary>
+		/// Decisecond. 10^-1 seconds
+		/// </summary>
+		ds,
 		/// <summary>
 		/// Second
 		/// </summary>
