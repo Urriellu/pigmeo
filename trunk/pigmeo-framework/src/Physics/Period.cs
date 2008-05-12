0bc8cac01f11378b4277991d0f87871a7b2b290a
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-05-12 16:40:36 +0000

    Converting Periods to and from Frequencies. Supporting more arithmetic and comparison operators

--- trunk/pigmeo-framework/src/Physics/Period.cs
+++ trunk/pigmeo-framework/src/Physics/Period.cs
@@ -11,7 +11,15 @@ namespace Pigmeo.Physics {
 		/// <summary>
 		/// Units in which the value will be stored in memory
 		/// </summary>
-		protected static TimeUnits StorageUnit = TimeUnits.second;
+		private static TimeUnits _StorageUnit = TimeUnits.second;
+		public static TimeUnits StorageUnit {
+			get {
+				return _StorageUnit;
+			}
+			protected set {
+				_StorageUnit = value;
+			}
+		}
 
 		public Period(float value, SIPrefixes prefix, TimeUnits Punit) {
 			this.value = Convert(ConvertPrefix(value, prefix, StoragePrefix), Punit, StorageUnit);
@@ -21,17 +29,29 @@ namespace Pigmeo.Physics {
 			SIPrefixes prefix = SIPrefixes.Unit;
 
 			switch(Punits) {
+				case TimeUnitsSI.µs:
+					prefix = SIPrefixes.µ;
+					break;
 				case TimeUnitsSI.ms:
 					prefix = SIPrefixes.m;
 					break;
+
 				case TimeUnitsSI.s:
 					prefix = SIPrefixes.Unit;
 					break;
 				default:
-					throw new Exception("SI time unit not supported yet");
+					throw new Exception("SI time unit \"" + Punits.ToString() + "\" not supported yet");
 			}
 
 			this.value = ConvertPrefix(value, prefix, StoragePrefix);
+		}
+
+		public Period(Frequency freq) {
+			value = Convert(1 / freq.GetValue(SIPrefixes.Unit, FrequencyUnits.Hz), TimeUnits.second, Period.StorageUnit);
+		}
+
+		public float GetValue(SIPrefixes prefix, TimeUnits Tunit) {
+			return Convert(ConvertPrefix(this.value, StoragePrefix, prefix), StorageUnit, Tunit);
 		}
 
 		protected static float GetTimeMultiplier(TimeUnits unit) {
@@ -52,16 +72,48 @@ namespace Pigmeo.Physics {
 			return multip;
 		}
 
-		/*public Period(Frequency freq) {
-			value = 1 / freq;
-		}*/
-
 		public static float Convert(float value, TimeUnits CurrentUnit, TimeUnits NewUnit) {
 			return value * GetTimeMultiplier(CurrentUnit) / GetTimeMultiplier(NewUnit);
 		}
 
-		/*public Frequency ToFrequency() {
+		public Frequency ToFrequency() {
 			return new Frequency(this);
-		}*/
+		}
+
+		public static Period operator +(Period T1, Period T2) {
+			return new Period(T1.value + T2.value, Period.StoragePrefix, Period.StorageUnit);
+		}
+
+		public static Period operator -(Period T1, Period T2) {
+			return new Period(T1.value - T2.value, Period.StoragePrefix, Period.StorageUnit);
+		}
+
+		public static Period operator *(Period T, float n) {
+			return new Period(T.GetValue(Period.StoragePrefix, Period.StorageUnit) * n, Period.StoragePrefix, Period.StorageUnit);
+		}
+
+		public static Period operator /(Period T, float n) {
+			return new Period(T.GetValue(Period.StoragePrefix, Period.StorageUnit) / n, Period.StoragePrefix, Period.StorageUnit);
+		}
+
+		public static float operator /(Period T1, Period T2) {
+			return T1.value / T2.value;
+		}
+
+		public static bool operator <(Period T1, Period T2) {
+			return T1.value < T2.value;
+		}
+
+		public static bool operator >(Period T1, Period T2) {
+			return T1.value > T2.value;
+		}
+
+		public static bool operator ==(Period T1, Period T2) {
+			return T1.value == T2.value;
+		}
+
+		public static bool operator !=(Period T1, Period T2) {
+			return T1.value != T2.value;
+		}
 	}
 }
