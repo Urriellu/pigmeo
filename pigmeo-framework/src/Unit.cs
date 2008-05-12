fa7e97dc517ecf0b54aaa596f7c6ddba2d669699
urriellu <pigmeo@urriellu.net>
2008-05-12 16:50:10 +0000

    Replacing MathExtended.Pow() by Math.Pow()

--- pigmeo-framework/src/Unit.cs
+++ pigmeo-framework/src/Unit.cs
@@ -6,26 +6,35 @@ namespace Pigmeo {
 		protected float value;
 
 		public float ConvertPrefix(float value, SIPrefixes CurrentPrefix, SIPrefixes NewPrefix) {
-			return value*GetMultiplier(CurrentPrefix)/GetMultiplier(NewPrefix);
+			return value * GetMultiplier(CurrentPrefix) / GetMultiplier(NewPrefix);
 		}
 
 		protected float GetMultiplier(SIPrefixes prefix) {
 			float multip = 0;
 			switch(prefix) {
+				case SIPrefixes.p:
+					multip = (float)Math.Pow(10, -12);
+					break;
+				case SIPrefixes.n:
+					multip = (float)Math.Pow(10, -9);
+					break;
 				case SIPrefixes.µ:
-					multip = MathExtended.Pow(10, -6);
+					multip = (float)Math.Pow(10, -6);
+					break;
+				case SIPrefixes.m:
+					multip = (float)Math.Pow(10, -3);
 					break;
 				case SIPrefixes.Unit:
 					multip = 1;
 					break;
 				case SIPrefixes.k:
-					multip = MathExtended.Pow(10, 3);
+					multip = (float)Math.Pow(10, 3);
 					break;
 				case SIPrefixes.M:
-					multip = MathExtended.Pow(10, 6);
+					multip = (float)Math.Pow(10, 6);
 					break;
 				default:
-					throw new Exception("Prefix not supported yet");
+					throw new Exception("Prefix \"" + prefix.ToString() + "\" not supported yet");
 			}
 			return multip;
 		}
