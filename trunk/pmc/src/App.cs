f37c92b22ed53d487c35719e388543d61c60df66
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-05-26 15:20:27 +0000

    New subproject: PMC

---
+++ trunk/pmc/src/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.PMC {
+	/// <summary>
+	/// Represents a software application that can be called from within PMC
+	/// </summary>
+	public class App {
+		public readonly string RealName;
+		public readonly string UnixName;
+		public readonly string Command;
+
+		public App(string RealName, string UnixName, string Command) {
+			this.RealName = RealName;
+			this.UnixName = UnixName;
+			this.Command = Command;
+		}
+	}
+}
---
+++ trunk/pmc/src/Apps.cs
@@ -0,0 +1,30 @@
+﻿namespace Pigmeo.PMC {
+	/// <summary>
+	/// List of available applications which can be called from within PMC
+	/// </summary>
+	public static class Apps {
+		/// <summary>
+		/// High level language compilers
+		/// </summary>
+		public static class HL {
+			public static App gmcs = new App("Mono C# Compiler", "mcs", "gmcs");
+			public static App CustomHLCompiler;
+		}
+
+		/// <summary>
+		/// Pigmeo-related applications
+		/// </summary>
+		public static class Pigmeo {
+			public static App PMC = new App("PMC", "pmc", "pmc");
+			public static App PigmeoCompiler = new App("Pigmeo Compiler", "pigmeo-compiler", "pigmeo");
+		}
+
+		/// <summary>
+		/// Assembler applications
+		/// </summary>
+		public static class Assemblers {
+			public static App gpasm = new App("GNU PIC Assembler", "gpasm", "gpasm");
+			public static App CustomAssembler;
+		}
+	}
+}
