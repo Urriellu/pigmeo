c66244588ac9d894829b8050dc39de50c919d1b4
urriellu <pigmeo@urriellu.net>
2008-05-26 15:21:04 +0000

    Supporting PMC

--- CountLinesOfCode.sh
+++ CountLinesOfCode.sh
@@ -8,10 +8,13 @@ LinesFramework=`find pigmeo-framework -type f -name "*.cs" 2>/dev/null | xargs c
 FilesFramework=`ls -R pigmeo-framework | grep .cs$ | wc -l`
 LinesCompiler=`find pigmeo-compiler -type f -name "*.cs" 2>/dev/null | xargs cat | wc -l`
 FilesCompiler=`ls -R pigmeo-compiler | grep .cs$ | wc -l`
-let TotalLines=$LinesFramework+$LinesCompiler
-let TotalFiles=$FilesFramework+$FilesCompiler
+LinesPMC=`find pmc -type f -name "*.cs" 2>/dev/null | xargs cat | wc -l`
+FilesPMC=`ls -R pmc | grep .cs$ | wc -l`
+let TotalLines=$LinesFramework+$LinesCompiler+$LinesPMC
+let TotalFiles=$FilesFramework+$FilesCompiler+$FilesPMC
 echo Framework: $LinesFramework lines of code in $FilesFramework \".cs\" files
 echo Compiler: $LinesCompiler lines of code in $FilesCompiler \".cs\" files
+echo PMC: $LinesPMC lines of code in $FilesPMC \".cs\" files
 echo Total: $TotalLines lines of code in $TotalFiles \".cs\" files
 
 cd "$MyOldPath"
