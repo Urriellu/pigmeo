d13e82df30ce31f0f734747bed96a156bac75cc9
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-05-27 17:27:35 +0000

    New class PmcException

---
+++ trunk/pmc/src/PmcException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pigmeo.PMC {
+	/// <summary>
+	/// Represents an exception thrown by PMC
+	/// </summary>
+	public class PmcException:Exception {
+		public readonly string msg;
+
+		public PmcException(string message) {
+			this.msg = message;
+		}
+	}
+}
