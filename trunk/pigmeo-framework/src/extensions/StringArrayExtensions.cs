62830c743e85c181cb86f2bad3bd1c81d2f18792
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-06-04 08:21:36 +0000

    new StringArrayExtensions.cs

---
+++ trunk/pigmeo-framework/src/extensions/StringArrayExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Extensions {
+	public static class StringArrayExtensions {
+		/// <summary>
+		/// Gets the list of strings separated by a comma
+		/// </summary>
+		public static string CommaSeparatedList(this string[] strs) {
+			string list = "";
+			for(int i = 0 ; i < strs.Length ; i++) {
+				if(i == 0) list += strs[i];
+				else list += ", " + strs[i];
+			}
+			return list;
+		}
+	}
+}
