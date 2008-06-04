7730ea541b86333ca55190c8394d276e174aff37
urriellu <pigmeo@urriellu.net>
2008-06-04 08:31:07 +0000

    char config.PathSplitter

--- pmc/src/config.cs
+++ pmc/src/config.cs
@@ -19,6 +19,8 @@ namespace Pigmeo.PMC {
 		/// </summary>
 		public static VerbosityLevel Verbosity = VerbosityLevel.Quiet;
 
+		public static char PathSplitter = '/';
+
 		/// <summary>
 		/// Full path to the directory that contains the PMC executable file. The last character is NOT a slash ('/')
 		/// </summary>
