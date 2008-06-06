e5c2b7639e32d0441b53abbcf414ff465a6f2630
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-06-06 17:57:27 +0000

    new class PigmeoCompiler.cs

---
+++ trunk/pmc/src/Apps/PigmeoCompiler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pigmeo.PMC {
+	/// <summary>
+	/// The Pigmeo Compiler application
+	/// </summary>
+	public class PigmeoCompiler:App {
+		public PigmeoCompiler(string RealName, string UnixName, string Command)
+			: base(RealName, UnixName, Command) {
+		}
+
+		/// <summary>
+		/// Generates the list of parameters which will be passed to the command and then base.Run() executes the application using the given "Command" and "Parameters"
+		/// </summary>
+		/// <returns></returns>
+		public override int Run() {
+			PrintMsg.InfoDebug("Running PigmeoCompiler.Run()");
+
+			Parameters.Clear();
+			Parameters.Add("--ui console");
+			if(config.Verbosity == VerbosityLevel.Verbose) Parameters.Add("--verbose");
+			if(config.Verbosity == VerbosityLevel.Debug) Parameters.Add("--debug");
+			Parameters.Add(config.CompiledExeFullName);
+
+			return base.Run();
+		}
+	}
+}
