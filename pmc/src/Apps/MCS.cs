d6c193d71f35a2b8317eeb83c50665b03b081d26
urriellu <pigmeo@urriellu.net>
2008-06-06 18:00:43 +0000

    More documentation. Supporting a few more parameters

--- pmc/src/Apps/MCS.cs
+++ pmc/src/Apps/MCS.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Pigmeo.Extensions;
 
 namespace Pigmeo.PMC {
 	/// <summary>
 	/// The Mono C# Compiler
 	/// </summary>
 	public class MCS:App {
+		/// <summary>
+		/// List of source files being compiled
+		/// </summary>
 		public List<string> SourceFiles = new List<string>();
+
+		/// <summary>
+		/// List of paths where libraries will be looked for
+		/// </summary>
+		public List<string> LibPaths = new List<string>();
+
+		/// <summary>
+		/// List of referenced libraries/assemblies
+		/// </summary>
+		public List<string> RefLibs = new List<string>();
+
 		public List<string> Modules = new List<string>();
 
 		/// <summary>
@@ -27,10 +42,15 @@ namespace Pigmeo.PMC {
 			PrintMsg.InfoDebug("Running MCS.Run()");
 
 			Parameters.Clear();
+			Parameters.Add("-t:exe");
 			if(Checked) Parameters.Add("-checked");
+			if(LibPaths.Count > 0) Parameters.Add("-lib:" + LibPaths.ToArray().CommaSeparatedList(false));
+			if(RefLibs.Count > 0) Parameters.Add("-r:" + RefLibs.ToArray().CommaSeparatedList(false));
+			Parameters.Add("-out:" + config.CompiledExeFullName);
 			foreach(string SrcFile in SourceFiles) {
 				Parameters.Add(SrcFile);
 			}
+
 			return base.Run();
 		}
 	}
