5046e65080cbb72e19ca9b847774ba6f50b3bdbf
urriellu <pigmeo@urriellu.net>
2008-06-06 17:56:48 +0000

    Now CommaSeparatedList() can return a list separated by a comma or by a comma followed by a space

--- pigmeo-framework/src/extensions/StringArrayExtensions.cs
+++ pigmeo-framework/src/extensions/StringArrayExtensions.cs
@@ -9,10 +9,21 @@ namespace Pigmeo.Extensions {
 		/// Gets the list of strings separated by a comma
 		/// </summary>
 		public static string CommaSeparatedList(this string[] strs) {
+			return strs.CommaSeparatedList(true);
+		}
+
+		/// <summary>
+		/// Gets the list of strings separated by a comma
+		/// </summary>
+		/// <param name="spaced">True if the list is separated by a comma followed by a space. False if the separator is just a comma</param>
+		public static string CommaSeparatedList(this string[] strs, bool spaced) {
+			string separator = ",";
+			if(spaced) separator = ", ";
+
 			string list = "";
 			for(int i = 0 ; i < strs.Length ; i++) {
 				if(i == 0) list += strs[i];
-				else list += ", " + strs[i];
+				else list += separator + strs[i];
 			}
 			return list;
 		}
