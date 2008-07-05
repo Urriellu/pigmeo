43a915192138376b3c0d7c73d73f77979069d82e
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-07-05 10:15:48 +0000

    New temperature-related classes

---
+++ trunk/pigmeo-framework/src/Physics/TempCelsius.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Pigmeo.Physics {
+	/// <summary>
+	/// Temperature value represented in the Celsius scale
+	/// </summary>
+	public class TempCelsius:Temperature {
+		const string Suffix = "ºC";
+
+		#region constructors
+		public TempCelsius(float value) {
+			this.value = value;
+		}
+		#endregion
+
+		#region overriders
+		public override TempCelsius ToCelsius() {
+			return this;
+		}
+
+		public override TempFahrenheit ToFahrenheit() {
+			return new TempFahrenheit(value * 9 / 5 + 32);
+		}
+
+		public override TempKelvin ToKelvin() {
+			return new TempKelvin(value + 273.15f);
+		}
+
+		public override string ToString() {
+			return value.ToString() + Suffix;
+		}
+		#endregion
+
+		#region implicit conversions
+		public static implicit operator TempCelsius(TempFahrenheit TF) {
+			return TF.ToCelsius();
+		}
+
+		public static implicit operator TempCelsius(TempKelvin TK) {
+			return TK.ToCelsius();
+		}
+		#endregion
+	}
+}
