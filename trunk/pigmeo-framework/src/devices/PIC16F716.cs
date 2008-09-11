8bb6a44bb10e317131b016743ed34d15b7ff65e7
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-11 17:49:27 +0000

    Setting volatile keyword to registers

--- trunk/pigmeo-framework/src/devices/PIC16F716.cs
+++ trunk/pigmeo-framework/src/devices/PIC16F716.cs
@@ -47,13 +47,13 @@ namespace Pigmeo.MCU {
 		/// Timer0 module's register
 		/// </summary>
 		[AsmName("TMR0"), Location(0, 0x01)]
-		public static byte TMR0 = 0;
+		public volatile static byte TMR0 = 0;
 
 		/// <summary>
 		/// Program Counter's (PC) Least Significant Byte
 		/// </summary>
 		[AsmName("PCL"), Location(0, 0x02)]
-		public static byte PCL = 0;
+		public volatile static byte PCL = 0;
 
 		/// <summary>
 		/// Contains the arithmetic status of the ALU, the Reset status and the bank select bits for data memory
@@ -61,41 +61,41 @@ namespace Pigmeo.MCU {
 		public static class STATUS {
 			/// <summary>Register Bank Select bit (used for direct addressing). RP0=false for bank 0, RP0=true for bank 1</summary>
 			[Location(0, 3, 5)]
-			public static bool RP0 = false;
+			public volatile static bool RP0 = false;
 
 			/// <summary>Time-out bit. INVERTED. true = After power-up, CLRWDT instruction or SLEEP instruction. false = A WDT time-out occurred</summary>
 			[Location(0, 3, 4)]
-			public static readonly bool _TO = true;
+			public volatile static readonly bool _TO = true;
 
 			/// <summary>Power-down bit. INVERTED. true = After power-up or by the CLRWDT instruction. false = By execution of the SLEEP instruction</summary>
 			[Location(0, 3, 3)]
-			public static readonly bool _PD = true;
+			public volatile static readonly bool _PD = true;
 
 			/// <summary>Zero bit. true = The result of an arithmetic or logic operation is zero. false = The result of an arithmetic or logic operation is not zero</summary>
 			[Location(0, 3, 2)]
-			public static bool Z;
+			public volatile static bool Z;
 
 			/// <summary>Digit Carry/_Borrow bit (ADDWF, ADDLW,SUBLW,SUBWF instructions). For Borrow, the polarity is reversed. true = A carry-out from the 4th low-order bit of the result occurred. false = No carry-out from the 4th low-order bit of the result</summary>
 			[Location(0, 3, 1)]
-			public static bool DC;
+			public volatile static bool DC;
 
 			/// <summary>Carry/_Borrow bit (ADDWF, ADDLW, SUBLW, SUBWF instructions). true = A carry-out from the Most Significant bit of the result occurred. false = No carry-out from the Most Significant bit of the result occurred</summary>
 			/// <remarks>For Borrow, the polarity is reversed. A subtraction is executed by adding the two's complement of the second operand. For rotate (RRF, RLF) instructions, this bit is loaded with either the high-order or low-order bit of the source register.</remarks>
 			[Location(0, 3, 0)]
-			public static bool C;
+			public volatile static bool C;
 		}
 
 		/// <summary>
 		/// Indirect Data Memory Address Pointer
 		/// </summary>
 		[AsmName("FSR"), Location(0, 0x04)]
-		public static byte FSR = 0;
+		public volatile static byte FSR = 0;
 
 		[AsmName("PORTA"), Location(0, 0x05)]
-		public static byte PORTA = 0;
+		public volatile static byte PORTA = 0;
 
 		[AsmName("PORTB"), Location(0, 0x06)]
-		public static byte PORTB = 0;
+		public volatile static byte PORTB = 0;
 
 		/// <summary>
 		/// Write Buffer for the upper 5 bits of the Program Counter
@@ -104,35 +104,35 @@ namespace Pigmeo.MCU {
 		/// The upper byte of the program counter is not directly accessible. PCLATH is a holding register for PC[12:8] whose contents are transferred to the upper byte of the program counter
 		/// </remarks>
 		[AsmName("PCLATH"), Location(0, 0x0A)]
-		public static byte PCLATH = 0;
+		public volatile static byte PCLATH = 0;
 
 		/// <summary>
 		/// Contains various enable and flag bits for the TMR0 register overflow, RB Port change and external RB0/INT pin interrupts
 		/// </summary>
-		public static class INTCON {
+		public volatile static class INTCON {
 			/// <summary>
 			/// Global Interrupt Enable bit. true = Enables all unmasked interrupts. false = Disables all interrupts
 			/// </summary>
 			[Location(0, 0x0B, 7)]
-			public static bool GIE = false;
+			public volatile static bool GIE = false;
 
 			/// <summary>
 			/// Peripheral Interrupt Enable bit. true = Enables all unmasked peripheral interrupts. false = Disables all peripheral interrupts
 			/// </summary>
 			[Location(0, 0x0B, 6)]
-			public static bool PEIE = false;
+			public volatile static bool PEIE = false;
 
 			/// <summary>
 			/// Timer0 Overflow Interrupt Enable bit. true = Enables the Timer0 interrupt. false = Disables the Timer0 interrupt
 			/// </summary>
 			[Location(0, 0x0B, 5)]
-			public static bool T0IE = false;
+			public volatile static bool T0IE = false;
 
 			/// <summary>
 			/// RB0/INT External Interrupt Enable bit. true = Enables the RB0/INT external interrupt. false = Disables the Timer0 interrupt
 			/// </summary>
 			[Location(0, 0x0B, 4)]
-			public static bool INTE = false;
+			public volatile static bool INTE = false;
 
 			/// <summary>
 			/// PORTB Change Interrupt Enable bit. true = Enables the PORTB change interrupt. false = Disables the PORTB change interrupt
@@ -141,7 +141,7 @@ namespace Pigmeo.MCU {
 			/// IOCB register must also be enabled
 			/// </remarks>
 			[Location(0, 0x0B, 3)]
-			public static bool RBIE = false;
+			public volatile static bool RBIE = false;
 
 			/// <summary>
 			/// Timer0 Overflow Interrupt Flag bit. true = TMR0 register has overflowed. false = TMR0 register did not overflow
@@ -150,19 +150,19 @@ namespace Pigmeo.MCU {
 			/// T0IF bit is set when Timer0 rolls over. Timer0 is unchanged on Reset and should be initialized before clearing T0IF bit
 			/// </remarks>
 			[Location(0, 0x0B, 2)]
-			public static bool T0IF = false;
+			public volatile static bool T0IF = false;
 
 			/// <summary>
 			/// RB0/INT External Interrupt Flag bit. true = The RB0/INT external interrupt occurred. false = The RB0/INT external interrupt did not occur
 			/// </summary>
 			[Location(0, 0x0B, 1)]
-			public static bool INTF = false;
+			public volatile static bool INTF = false;
 
 			/// <summary>
 			/// PORTB Change Interrupt Flag bit. true = When at least one of the PORTB general purpose I/O pins changed state. false = None of the PORTB general purpose I/O pins have changed state
 			/// </summary>
 			[Location(0, 0x0B, 0)]
-			public static bool RBIF = false;
+			public volatile static bool RBIF = false;
 		}
 
 		/// <summary>
@@ -173,38 +173,38 @@ namespace Pigmeo.MCU {
 			/// A/D Interrupt Flag bit. true = A/D conversion complete. false = A/D conversion has not completed or has not been started
 			/// </summary>
 			[Location(0, 0x0C, 6)]
-			public static bool ADIF = false;
+			public volatile static bool ADIF = false;
 
 			/// <summary>
 			/// CCP1 Interrupt Flag bit. Capture Mode {true = A TMR1 register capture occurred (must be cleared in software), false = No TMR1 register capture occurred}. Compare Mode {true = A TMR1 register compare match occurred (must be cleared in software), false = No TMR1 register compare match occurred}. PWM Mode {Unused in this mode}
 			/// </summary>
 			[Location(0, 0x0C, 2)]
-			public static bool CCP1IF = false;
+			public volatile static bool CCP1IF = false;
 
 			/// <summary>
 			/// Timer2 to PR2 Match Interrupt Flag bit. true = Timer2 to PR2 match occurred (must be cleared in software). false = Timer2 to PR2 match occurred (must be cleared in software)
 			/// </summary>
 			[Location(0, 0x0C, 1)]
-			public static bool TMR2IF = false;
+			public volatile static bool TMR2IF = false;
 
 			/// <summary>
 			/// Timer1 Overflow Interrupt Flag bit. true = Timer1 register overflowed (must be cleared in software). false = Timer1 register overflowed (must be cleared in software)
 			/// </summary>
 			[Location(0, 0x0C, 0)]
-			public static bool TMR1IF = false;
+			public volatile static bool TMR1IF = false;
 		}
 
 		/// <summary>
 		/// Holding Register for the Least Significant Byte of the 16-bit TMR1 Register
 		/// </summary>
 		[AsmName("TMR1L"), Location(0, 0x0E)]
-		public static byte TMR1L = 0;
+		public volatile static byte TMR1L = 0;
 
 		/// <summary>
 		/// Holding Register for the Most Significant Byte of the 16-bit TMR1 Register
 		/// </summary>
 		[AsmName("TMR1H"), Location(0, 0x0F)]
-		public static byte TMR1H = 0;
+		public volatile static byte TMR1H = 0;
 
 		/// <summary>
 		/// It's used to control Timer1 and select the various features of the Timer1 module
@@ -214,44 +214,44 @@ namespace Pigmeo.MCU {
 			/// Timer1 Input Clock Prescale Select bit 1
 			/// </summary>
 			[Location(0, 0x10, 5)]
-			public static bool T1CKPS1 = false;
+			public volatile static bool T1CKPS1 = false;
 
 			/// <summary>
 			///  Timer1 Input Clock Prescale Select bit 0
 			/// </summary>
 			[Location(0, 0x10, 4)]
-			public static bool T1CKPS0 = false;
+			public volatile static bool T1CKPS0 = false;
 
 			/// <summary>
 			/// Timer1 Oscillator Enable Control bit. true = Timer1 oscillator is enabled, false = Timer1 oscillator is disabled
 			/// </summary>
 			[Location(0, 0x10, 3)]
-			public static bool T1OSCEN = false;
+			public volatile static bool T1OSCEN = false;
 
 			/// <summary>
 			/// Timer1 External Clock Input Synchronization Control bit. INVERTED. If TMR1CS==false {This bit is ignored. Timer1 uses the internal clock}. If TMR1CS==true {true = Do not synchronize external clock input, false = Synchronize external clock input}
 			/// </summary>
 			[Location(0, 0x10, 2)]
-			public static bool _T1SYNC = false;
+			public volatile static bool _T1SYNC = false;
 
 			/// <summary>
 			/// Timer1 Clock Source Select bit. true = External clock from T1CKI pin (on the rising edge). false = Internal clock (Fosc/4)
 			/// </summary>
 			[Location(0, 0x10, 1)]
-			public static bool TMR1CS = false;
+			public volatile static bool TMR1CS = false;
 
 			/// <summary>
 			/// Timer1 On bit. true = Enables Timer1. false = Stops Timer1
 			/// </summary>
 			[Location(0, 0x10, 0)]
-			public static bool TMR1ON = false;
+			public volatile static bool TMR1ON = false;
 		}
 
 		/// <summary>
 		/// Timer2 Module's Register
 		/// </summary>
 		[AsmName("TMR2"), Location(0, 0x11)]
-		public static byte TMR2 = 0;
+		public volatile static byte TMR2 = 0;
 
 
 
@@ -282,62 +282,62 @@ namespace Pigmeo.MCU {
 			/// PORTB Pull-up Enable bit. INVERTED. true = PORTB pull-ups are disabled. false = PORTB pull-ups are enabled by individual PORT latch values
 			/// </summary>
 			[Location(1, 0x01, 7)]
-			public static bool _RBPU = true;
+			public volatile static bool _RBPU = true;
 
 			/// <summary>
 			/// Interrupt Edge Select bit. true = Interrupt on rising edge of RB0/INT pin. false = Interrupt on falling edge of RB0/INT pin
 			/// </summary>
 			[Location(1, 0x01, 6)]
-			public static bool INTEDG = true;
+			public volatile static bool INTEDG = true;
 
 			/// <summary>
 			/// Timer0 Clock Source Select bit. true = Transition on T0CKI pin. false = Internal instruction cycle clock (Fosc/4)
 			/// </summary>
 			[Location(1, 0x01, 5)]
-			public static bool T0CS = true;
+			public volatile static bool T0CS = true;
 
 			/// <summary>
 			/// Timer0 Source Edge Select bit. true = Increment on high-to-low transition on RA4/T0CKI pin. false = Increment on low-to-high transition on RA4/T0CKI pin
 			/// </summary>
 			[Location(1, 0x01, 4)]
-			public static bool T0SE = true;
+			public volatile static bool T0SE = true;
 
 			/// <summary>
 			/// Prescaler Assignment bit. true = Prescaler is assigned to the WDT. false = Prescaler is assigned to the Timer0 module
 			/// </summary>
 			[Location(1, 0x01, 3)]
-			public static bool PSA = true;
+			public volatile static bool PSA = true;
 
 			/// <summary>
 			/// Prescaler Rate Select bit 2
 			/// </summary>
 			[Location(1, 0x01, 2)]
-			public static bool PS2 = true;
+			public volatile static bool PS2 = true;
 
 			/// <summary>
 			/// Prescaler Rate Select bit 1
 			/// </summary>
 			[Location(1, 0x01, 1)]
-			public static bool PS1 = true;
+			public volatile static bool PS1 = true;
 
 			/// <summary>
 			/// Prescaler Rate Select bit 0
 			/// </summary>
 			[Location(1, 0x01, 0)]
-			public static bool PS0 = true;
+			public volatile static bool PS0 = true;
 		}
 
 		/// <summary>
 		/// PORTA data direction register. true = Input. false = Output
 		/// </summary>
 		[AsmName("TRISA"), Location(1, 0x05)]
-		public static byte TRISA = 0;
+		public volatile static byte TRISA = 0;
 
 		/// <summary>
 		/// PORTB data direction register. true = Input. false = Output
 		/// </summary>
 		[AsmName("TRISB"), Location(1, 0x06)]
-		public static byte TRISB = 0;
+		public volatile static byte TRISB = 0;
 
 		/// <summary>
 		/// Contains the individual enable bits for the peripheral interrupts.
@@ -347,25 +347,25 @@ namespace Pigmeo.MCU {
 			/// A/D Converter (ADC) Interrupt Enable bit. true = Enables the ADC interrupt. false = Disables the ADC interrupt
 			/// </summary>
 			[Location(1, 0x0C, 6)]
-			public static bool ADIE = false;
+			public volatile static bool ADIE = false;
 
 			/// <summary>
 			/// CCP1 Interrupt Enable bit. true = Enables the CCP1 interrupt. false = Disables the CCP1 interrupt
 			/// </summary>
 			[Location(1, 0x0C, 2)]
-			public static bool CCP1IE = false;
+			public volatile static bool CCP1IE = false;
 
 			/// <summary>
 			/// Timer2 to PR2 Match Interrupt Enable bit. true = Enables the Timer2 to PR2 match interrupt. false = Disables the Timer2 to PR2 match interrupt
 			/// </summary>
 			[Location(1, 0x0C, 1)]
-			public static bool TMR2IE = false;
+			public volatile static bool TMR2IE = false;
 
 			/// <summary>
 			/// Timer1 Overflow Interrupt Enable bit. true = Enables the Timer1 overflow interrupt. false = Disables the Timer1 overflow interrupt
 			/// </summary>
 			[Location(1, 0x0C, 0)]
-			public static bool TMR1IE = false;
+			public volatile static bool TMR1IE = false;
 		}
 
 		/// <summary>
@@ -376,20 +376,20 @@ namespace Pigmeo.MCU {
 			/// Power-on Reset Status bit. INVERTED. true = No Power-on Reset occurred. false = A Power-on Reset occurred (must be set in software after a Power-on Reset occurs)
 			/// </summary>
 			[Location(1, 0x0E, 1)]
-			public static bool _POR = false;
+			public volatile static bool _POR = false;
 
 			/// <summary>
 			/// Brown-out Reset Status bit. INVERTED. true = No Brown-out Reset occurred. false = A Brown-out Reset occurred (must be set in software after a Brown-out Reset occurs)
 			/// </summary>
 			[Location(1, 0x0E, 0)]
-			public static bool _BOR = false;
+			public volatile static bool _BOR = false;
 		}
 
 		/// <summary>
 		/// Timer2 Period Register
 		/// </summary>
 		[AsmName("PR2"), Location(1, 0x12)]
-		public static byte PR2 = 0xFF;
+		public volatile static byte PR2 = 0xFF;
 
 		/// <summary>
 		/// A/D Control Register 1
@@ -399,19 +399,19 @@ namespace Pigmeo.MCU {
 			/// A/D Port Configuration Control bit 2
 			/// </summary>
 			[Location(1, 0x1F, 2)]
-			public static bool PCFG2 = false;
+			public volatile static bool PCFG2 = false;
 
 			/// <summary>
 			/// A/D Port Configuration Control bit 1
 			/// </summary>
 			[Location(1, 0x1F, 1)]
-			public static bool PCFG1 = false;
+			public volatile static bool PCFG1 = false;
 
 			/// <summary>
 			/// A/D Port Configuration Control bit 0
 			/// </summary>
 			[Location(1, 0x1F, 0)]
-			public static bool PCFG0 = false;
+			public volatile static bool PCFG0 = false;
 		}
 	}
 }
