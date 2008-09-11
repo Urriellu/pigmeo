13cbe247c591236c5be795242144a371e9720dd3
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-11 18:47:21 +0000

    Updating device libraries

---
+++ trunk/pigmeo-framework/src/devices/Shared/Globals.cs
@@ -0,0 +1,10 @@
+﻿// NOTE: Everything in this file will be compiled into ALL the device libraries
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyKeyFile("pigmeo.key")]
