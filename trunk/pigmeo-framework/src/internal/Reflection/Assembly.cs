31d306df70240bac2dab855971e0b3a2f263f87f
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-12 18:17:18 +0000

    New Pigmeo.Internal.Reflection stuff

---
+++ trunk/pigmeo-framework/src/internal/Reflection/Assembly.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using Mono.Cecil.Binary;
+using Mono.Cecil.Cil;
+using Mono.Cecil.Metadata;
+using System.IO;
+
+namespace Pigmeo.Internal.Reflection {
+	/// <summary>
+	/// Represents a reflected .NET Assembly (.exe or .dll)
+	/// </summary>
+	public partial class Assembly {
+		protected readonly AssemblyDefinition CclAssembly;
+		protected readonly string ReflectedFile;
+		public ReferencesCollection References;
+
+		public Assembly(string File) {
+			ReflectedFile = File;
+			CclAssembly = AssemblyFactory.GetAssembly(File);
+			References = new ReferencesCollection(CclAssembly);
+		}
+
+		#region static methods
+		/// <summary>
+		/// Indicates if the given file is a valid .NET assembly or not
+		/// </summary>
+		/// <param name="File"></param>
+		/// <exception cref="T:System.IO.FileLoadException">A file that was found could not be loaded.</exception>
+		/// <exception cref="T:System.IO.FileNotFoundException">The path parameter is an empty string ("") or does not exist.</exception>
+		public static bool IsAssembly(string File) {
+			try {
+				if(System.Reflection.Assembly.LoadFile(File).FullName != null) return true;
+			} catch(BadImageFormatException e) {
+				return false;
+			}
+			return false;
+		}
+		#endregion
+
+		#region properties
+		public string ReflectedFileName {
+			get {
+				return Path.GetFileName(ReflectedFile);
+			}
+		}
+		#endregion
+	}
+}
---
+++ trunk/pigmeo-framework/src/internal/Reflection/ReflectionException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Internal.Reflection {
+	public class ReflectionException:Exception {
+		public ReflectionException(string Message)
+			: base(Message) {
+		}
+	}
+}
