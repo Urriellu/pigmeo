f03db811a0f098aad5a11e8a3115a591bcf9ac81
urriellu <pigmeo@urriellu.net>
2008-09-12 18:11:32 +0000

    Suppporting the new Pigmeo Intermediate Representation

---
+++ pigmeo-compiler/src/PIR/Method.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Method:TypeMember {
+		public string name;
+	}
+}
---
+++ pigmeo-compiler/src/PIR/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Represents a software program/application
+	/// </summary>
+	public class Program {
+		public Method EntryPoint;
+		public TypeCollection Types = new TypeCollection();
+	}
+}
