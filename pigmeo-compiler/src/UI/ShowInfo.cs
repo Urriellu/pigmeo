e2fb4c9d4cd41ac19a021335cf2a3e886218af02
urriellu <pigmeo@urriellu.net>
2008-09-18 11:19:05 +0000

    new method InfoDebugDecompile()

--- pigmeo-compiler/src/UI/ShowInfo.cs
+++ pigmeo-compiler/src/UI/ShowInfo.cs
@@ -40,7 +40,7 @@ namespace Pigmeo.Compiler.UI {
 		}
 
 		/// <summary>
-		/// Prinst a message if --debug was specified
+		/// Prints a message if --debug was specified
 		/// </summary>
 		/// <remarks>
 		/// The message should be written in english. Avoid translations
@@ -49,6 +49,20 @@ namespace Pigmeo.Compiler.UI {
 		/// <param name="args">List of strings to format, merge all together</param>
 		public static void InfoDebug(string message, params object[] args) {
 			InfoDebug(string.Format(message, args));
+		}
+
+		/// <summary>
+		/// Prints the decompilation of an Assembly, Program or any member as debug information
+		/// </summary>
+		/// <param name="Title">Title attached to the debug information</param>
+		/// <param name="obj">Object to decompile</param>
+		public static void InfoDebugDecompile(string Title, object obj) {
+			string Delimiter = "==================================================================";
+			InfoDebug(Delimiter);
+			InfoDebug("        >>>>>" + Title + "<<<<<");
+			InfoDebug("");
+			foreach(string str in obj.ToString().Replace("\t", "    ").Split('\n')) InfoDebug(str);
+			InfoDebug(Delimiter);
 		}
 	}
 }
