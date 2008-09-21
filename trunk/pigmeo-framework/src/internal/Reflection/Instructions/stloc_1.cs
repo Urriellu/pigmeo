2f181d9dd893ddac1bd1b4a64efc55b69d601199
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-21 18:43:55 +0000

    New CIL instructions

---
+++ trunk/pigmeo-framework/src/internal/Reflection/Instructions/stloc_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Pop a value from stack into local variable 1
+		/// </summary>
+		public class stloc_1:stloc {
+			public stloc_1(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.stloc_1;
+				Index = 1;
+			}
+		}
+	}
+}
