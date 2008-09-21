f6d96c42969ff2d943cf71cf3b551c7d8571aa4a
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-21 18:38:23 +0000

    Supporting locations defined in header files

--- trunk/pigmeo-framework/src/internal/PIC14/CustomAttributes.cs
+++ trunk/pigmeo-framework/src/internal/PIC14/CustomAttributes.cs
@@ -6,14 +6,24 @@ namespace Pigmeo.Internal.PIC14 {
 
 	[AttributeUsage(AttributeTargets.Field)]
 	public class Location:Attribute {
-		public readonly RegisterAddress Address;
+		public readonly RegisterAddress Address = new RegisterAddress();
+		public readonly bool DefinedInHeader;
 
 		public Location(byte Bank, byte Address) {
-			this.Address = new RegisterAddress(Bank, Address);
+			this.Address.Bank = Bank;
+			this.Address.Address = Address;
+			DefinedInHeader = false;
 		}
 
 		public Location(byte Bank, byte Address, byte Bit) {
-			this.Address = new RegisterAddress(Bank, Address, Bit);
+			this.Address.Bank = Bank;
+			this.Address.Address = Address;
+			this.Address.Bit = Bit;
+			DefinedInHeader = false;
+		}
+
+		public Location(bool DefinedInHeader) {
+			this.DefinedInHeader = DefinedInHeader;
 		}
 	}
 }
