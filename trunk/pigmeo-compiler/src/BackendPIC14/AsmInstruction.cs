53aac3c1e23aa8f390114835c2ffa2807016c8a2
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-21 17:25:36 +0000

    Enums moved to Enum.cs

--- trunk/pigmeo-compiler/src/BackendPIC14/AsmInstruction.cs
+++ trunk/pigmeo-compiler/src/BackendPIC14/AsmInstruction.cs
@@ -4,94 +4,6 @@ using Pigmeo.Extensions;
 
 namespace Pigmeo.Compiler.BackendPIC14 {
 	/// <summary>
-	/// Specifies if the result of the operation of some byte oriented instructions will be stored in W or in a file/register
-	/// </summary>
-	public enum Destination:byte { W=0, F=1 } //this should be one bit
-
-	/// <summary>
-	/// Available instruction types, such as Directive, Byte Orientes Instruction, just a label, a Control Instruction...
-	/// </summary>
-	/// <remarks>
-	/// It also specifies the parameters it can handle
-	/// </remarks>
-	public enum InstructionType:byte {
-		Custom, Control, Label,
-		BitOriented_fb, ByteOriented_none, ByteOriented_f, ByteOriented_fd,
-		Literal_address, Literal_number,
-		Directive_str_str, Directive_str, Directive_str_sep_str, Directive_str_dir_str, Directive_lbl_dir_str, Directive_none
-	}
-
-	/// <summary>
-	/// Available directive names, such as DEFINE, INCLUDE, EQU, ORG...
-	/// </summary>
-	public enum Directive:byte {
-		DEFINE,
-		INCLUDE,
-		IFDEF,
-		IFNDEF,
-		CBLOCK,
-		PROCESSOR,
-		__CONFIG,
-		SET,
-		EQU,
-		DE,
-		ORG,
-		DB,
-		DT,
-		END,
-		ELSE,
-		ENDIF,
-		ENDW,
-		ENDC,
-		CONSTANT,
-		VARIABLE,
-		IF,
-		WHILE,
-		UNDEFINE
-	}
-
-	/// <summary>
-	/// Available instruction names, such as ADDWF, ANDWF, NOP...
-	/// </summary>
-	public enum OpCode:byte {
-		ADDWF,
-		ANDWF,
-		CLRF,
-		CLRW,
-		COMF,
-		DECF,
-		DECFSZ,
-		INCF,
-		INCFSZ,
-		IORWF,
-		MOVF,
-		MOVWF,
-		NOP,
-		RLF,
-		RRF,
-		SUBWF,
-		SWAPF,
-		XORWF,
-		BCF,
-		BSF,
-		BTFSC,
-		BTFSS,
-		ADDLW,
-		ANDLW,
-		CALL,
-		CLRWDT,
-		GOTO,
-		IORLW,
-		MOVLW,
-		RETFIE,
-		RETLW,
-		RETURN,
-		SLEEP,
-		SUBLW,
-		XORLW
-	}
-
-	/// <summary>
 	/// Represents ONE instruction written in assembly language
 	/// </summary>
 	public class AsmInstruction {
@@ -111,8 +23,6 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 		/// Prefix added to some directives
 		/// </summary>
 		public string Prefix = "";
-
-
 
 		/// <summary>
 		/// Name of the instruction
@@ -187,7 +97,7 @@ namespace Pigmeo.Compiler.BackendPIC14 {
 					returned += OP.ToString() + " " + file;
 					break;
 				case InstructionType.ByteOriented_fd:
-					returned += OP.ToString() + " " + file + "," + (byte)DestinationWF;
+					returned += OP.ToString() + " " + file + "," + DestinationWF.ToString();
 					break;
 				case InstructionType.Literal_address:
 					returned += OP.ToString() + " " + LiteralAddr;
---
+++ trunk/pigmeo-compiler/src/BackendPIC14/Enums.cs
@@ -0,0 +1,89 @@
+﻿namespace Pigmeo.Compiler.BackendPIC14 {
+	/// <summary>
+	/// Specifies if the result of the operation of some byte oriented instructions will be stored in W or in a file/register
+	/// </summary>
+	public enum Destination:byte { W = 0, F = 1 } //this should be one bit
+
+	/// <summary>
+	/// Available instruction types, such as Directive, Byte Orientes Instruction, just a label, a Control Instruction...
+	/// </summary>
+	/// <remarks>
+	/// It also specifies the parameters it can handle
+	/// </remarks>
+	public enum InstructionType:byte {
+		Custom, Control, Label,
+		BitOriented_fb, ByteOriented_none, ByteOriented_f, ByteOriented_fd,
+		Literal_address, Literal_number,
+		Directive_str_str, Directive_str, Directive_str_sep_str, Directive_str_dir_str, Directive_lbl_dir_str, Directive_none
+	}
+
+	/// <summary>
+	/// Available directive names, such as DEFINE, INCLUDE, EQU, ORG...
+	/// </summary>
+	public enum Directive:byte {
+		DEFINE,
+		INCLUDE,
+		IFDEF,
+		IFNDEF,
+		CBLOCK,
+		PROCESSOR,
+		__CONFIG,
+		SET,
+		EQU,
+		DE,
+		ORG,
+		DB,
+		DT,
+		END,
+		ELSE,
+		ENDIF,
+		ENDW,
+		ENDC,
+		CONSTANT,
+		VARIABLE,
+		IF,
+		WHILE,
+		UNDEFINE
+	}
+
+	/// <summary>
+	/// Available instruction names, such as ADDWF, ANDWF, NOP...
+	/// </summary>
+	public enum OpCode:byte {
+		ADDWF,
+		ANDWF,
+		CLRF,
+		CLRW,
+		COMF,
+		DECF,
+		DECFSZ,
+		INCF,
+		INCFSZ,
+		IORWF,
+		MOVF,
+		MOVWF,
+		NOP,
+		RLF,
+		RRF,
+		SUBWF,
+		SWAPF,
+		XORWF,
+		BCF,
+		BSF,
+		BTFSC,
+		BTFSS,
+		ADDLW,
+		ANDLW,
+		CALL,
+		CLRWDT,
+		GOTO,
+		IORLW,
+		MOVLW,
+		RETFIE,
+		RETLW,
+		RETURN,
+		SLEEP,
+		SUBLW,
+		XORLW
+	}
+}
