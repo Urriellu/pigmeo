667a157ffd8364754e89e5e8b6573b01a71e7795
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-09-25 13:59:53 +0000

    Supporting processor families. More documentation

--- trunk/pigmeo-framework/src/internal/InfoDevice.cs
+++ trunk/pigmeo-framework/src/internal/InfoDevice.cs
@@ -9,9 +9,18 @@ namespace Pigmeo.Internal {
 	/// This information is retrieved from the library of the specified branch. For example if you want information about the PIC16F716 a InfoDevice object is instantiated as 'new InfoDevice("PIC16F716")', the library PIC16F716.dll is loaded using reflection and the information is retrieved from that library.</remarks>
 	public abstract class InfoDevice {
 		/// <summary>
-		/// The target architecture (PIC16, PIC18, dsPIC...)
+		/// The target architecture
 		/// </summary>
 		public Architecture arch;
+
+		/// <summary>
+		/// The target family
+		/// </summary>
+		public Family family;
+
+		/// <summary>
+		/// The target branch/device
+		/// </summary>
 		public Branch branch;
 	}
 
