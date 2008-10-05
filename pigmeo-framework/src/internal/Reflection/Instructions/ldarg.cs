305e7a2cde91155ad6e013fd7cee771ec063bba0
urriellu <pigmeo@urriellu.net>
2008-10-05 18:52:51 +0000

    Supporting more CIL instructions

---
+++ pigmeo-framework/src/internal/Reflection/Instructions/ldarg.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Load argument onto the stack
+		/// </summary>
+		public class ldarg:Instruction {
+			/// <summary>
+			/// Argument index
+			/// </summary>
+			public UInt16 Index {
+				get;
+				protected set;
+			}
+
+			/// <summary>
+			/// Local variable being copied to the stack
+			/// </summary>
+			public Parameter Argument {
+				get {
+					return ParentMethod.Parameters[Index];
+				}
+			}
+
+			public ldarg(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				if(OriginalInstruction.Operand is UInt16) Index = (UInt16)OriginalInstruction.Operand;
+				this.OpCode = OpCodes.ldarg;
+			}
+
+			public override string ToString() {
+				return OpCodeName + " " + Argument.Name;
+			}
+		}
+	}
+}
---
+++ pigmeo-framework/src/internal/Reflection/Instructions/ldarg_1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Load argument 1 onto the stack
+		/// </summary>
+		public class ldarg_1:ldarg {
+			public ldarg_1(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.ldarg_0;
+				Index = 1;
+			}
+		}
+	}
+}
