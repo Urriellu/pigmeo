ab1f83f6da00b8043f109d415e21259fd31bdea7
urriellu <pigmeo@urriellu.net>
2008-10-08 17:15:45 +0000

    Moving shared things to LocalVariableOperand.cs

--- pigmeo-framework/src/internal/Reflection/Instructions/stloc.cs
+++ pigmeo-framework/src/internal/Reflection/Instructions/stloc.cs
@@ -8,32 +8,11 @@ namespace Pigmeo.Internal.Reflection {
 		/// <summary>
 		/// Pop a value from stack into a local variable
 		/// </summary>
-		public class stloc:Instruction {
-			/// <summary>
-			/// Local variable index
-			/// </summary>
-			public UInt16 Index {
-				get;
-				protected set;
-			}
-
-			/// <summary>
-			/// Local variable where the Top Of Stack is being moved to
-			/// </summary>
-			public LocalVariable Variable {
-				get {
-					return ParentMethod.LocalVariables[Index];
-				}
-			}
-
+		public class stloc:LocalVariableOperand {
 			public stloc(Method ParentMethod, MCCil.Instruction OriginalInstruction)
 				: base(ParentMethod, OriginalInstruction) {
 				if(OriginalInstruction.Operand is UInt16) Index = (UInt16)OriginalInstruction.Operand; //stloc operand is UInt16
 				this.OpCode = OpCodes.stloc;
-			}
-
-			public override string ToString() {
-				return OpCodeName + " " + Variable.Name;
 			}
 		}
 	}
