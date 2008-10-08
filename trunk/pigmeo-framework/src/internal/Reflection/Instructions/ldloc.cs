825c3a01c80a7d7f00c9a11c6c47934aa8faafb9
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-10-08 16:48:10 +0000

    Moving shared things to LocalVariableOperand

--- trunk/pigmeo-framework/src/internal/Reflection/Instructions/ldloc.cs
+++ trunk/pigmeo-framework/src/internal/Reflection/Instructions/ldloc.cs
@@ -8,32 +8,10 @@ namespace Pigmeo.Internal.Reflection {
 		/// <summary>
 		/// Load local variable into stack
 		/// </summary>
-		public class ldloc:Instruction {
-			/// <summary>
-			/// Local variable index
-			/// </summary>
-			public UInt16 Index {
-				get;
-				protected set;
-			}
-
-			/// <summary>
-			/// Local variable being copied to the stack
-			/// </summary>
-			public LocalVariable Variable {
-				get {
-					return ParentMethod.LocalVariables[Index];
-				}
-			}
-
+		public class ldloc:LocalVariableOperand {
 			public ldloc(Method ParentMethod, MCCil.Instruction OriginalInstruction)
 				: base(ParentMethod, OriginalInstruction) {
-				if(OriginalInstruction.Operand is UInt16) Index = (UInt16)OriginalInstruction.Operand;
 				this.OpCode = OpCodes.ldloc;
-			}
-
-			public override string ToString() {
-				return OpCodeName + " " + Variable.Name;
 			}
 		}
 	}
