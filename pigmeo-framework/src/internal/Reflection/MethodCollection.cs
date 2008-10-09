31063732fa251d4c7659b31b42e6cf9c60a7c582
urriellu <pigmeo@urriellu.net>
2008-10-09 10:19:42 +0000

    New property "string[] Names"

--- pigmeo-framework/src/internal/Reflection/MethodCollection.cs
+++ pigmeo-framework/src/internal/Reflection/MethodCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Pigmeo.Extensions;
 
 namespace Pigmeo.Internal.Reflection {
 	/// <summary>
@@ -24,13 +25,24 @@ namespace Pigmeo.Internal.Reflection {
 			return false;
 		}
 
+		public string[] Names {
+			get {
+				if(_Names == null) {
+					_Names = new string[this.Count];
+					for(int i = 0 ; i < this.Count ; i++) _Names[i] = this[i].Name;
+				}
+				return _Names;
+			}
+		}
+		protected string[] _Names;
+
 		/// <summary>
 		/// Retrieves a Method from this collection, by its given name
 		/// </summary>
 		/// <param name="MethodName">Name of the method being retrieved</param>
 		public Method this[string MethodName] {
 			get {
-				ShowExternalInfo.InfoDebug("Trying to retrieve the method {0} from this MethodCollection", MethodName);
+				ShowExternalInfo.InfoDebug("Trying to retrieve the method {0} from this MethodCollection. Known methods: {1}", MethodName, Names.CommaSeparatedList());
 				for(int i = 0 ; i < this.Count ; i++) {
 					if(this[i].Name == MethodName) return this[i];
 				}
