b80e766caf54a74246520a521c4c6fa7701a893c
urriellu <pigmeo@urriellu.net>
2008-10-10 15:00:37 +0000

    Supporting new CIL instructions in Pigmeo.Internal.Reflection

---
+++ pigmeo-framework/src/internal/Reflection/Instructions/br.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Unconditional branch (typical jump or goto)
+		/// </summary>
+		public class br:InstructionOperand {
+			public br(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.br;
+			}
+		}
+	}
+}
