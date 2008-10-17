785b8722b7c2559ed5ed1b036081bae9306cb1bb
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-10-17 15:23:55 +0000

    Supporting PIR Operations and their Operands

---
+++ trunk/pigmeo-compiler/src/PIR/Operations/Copy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PRefl=Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Copy:Operation {
+		protected Copy(Method ParentMethod):base(ParentMethod) {
+			Operator = "Copy";
+			Arguments = new Operand[1];
+		}
+
+		public Copy(Method ParentMethod, PRefl.Instructions.ldc_i4 OrigCilInstr):this(ParentMethod) {
+			Arguments[0] = new ConstantInt32Operand(OrigCilInstr);
+			Result = GlobalOperands.TOSS;
+		}
+
+		public override string ToString() {
+			return base.ToString1st() + Arguments[0].ToString();
+		}
+	}
+}
