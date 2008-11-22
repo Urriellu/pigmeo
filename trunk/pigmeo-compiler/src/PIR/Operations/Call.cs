cd152b022cc5b462ff5ed36406c197534e9dffcb
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-11-22 19:16:37 +0000

    New PIR Operations: Add, Call and Return

---
+++ trunk/pigmeo-compiler/src/PIR/Operations/Call.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PRefl=Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Call:Operation {
+		protected Call(Method ParentMethod):base(ParentMethod) {
+			Operator = "Call";
+		}
+
+		public Call(Method ParentMethod, PRefl.Instructions.call OrigCilInstr):this(ParentMethod) {
+			Arguments = new Operand[OrigCilInstr.ReferencedMethod.Parameters.Count + 1];
+			Arguments[0] = new MethodOperand(ParentProgram.Types[OrigCilInstr.ReferencedMethod.ParentType.FullName].Methods[OrigCilInstr.ReferencedMethod.Name]);
+			for(int i = 0 ; i < OrigCilInstr.ReferencedMethod.Parameters.Count ; i++) Arguments[i + 1] = GlobalOperands.TOSS;
+			if(OrigCilInstr.ReferencedMethod.ReturnType.FullName != "System.Void") Result = GlobalOperands.TOSS;
+		}
+	}
+}
