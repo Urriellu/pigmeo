3a30ba05b9d5d3cc641ca0317ecc56904cd9b003
urriellu <pigmeo@urriellu.net>
2008-11-22 19:55:07 +0000

    Adding NewByArch()

--- pigmeo-compiler/src/PIR/Struct.cs
+++ pigmeo-compiler/src/PIR/Struct.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
-	public class Struct:ReferenceType {
-		public Struct(PRefl.Type ReflectedType, bool IncludeMembers)
-			: base(ReflectedType, IncludeMembers) {
+	public abstract class Struct:ValueType {
+		protected Struct(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers):base(ParentProgram, ReflectedType, IncludeMembers) {
+		}
+
+		public static Struct NewByArch(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers) {
+			switch(ParentProgram.TargetArch) {
+				case Architecture.PIC:
+					return new PIC.Struct(ParentProgram, ReflectedType, IncludeMembers);
+				default:
+					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
+					return null;
+			}
 		}
 
 		public override Type Clone() {
