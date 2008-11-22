ca7e9b241dfe1ba02b07e7e697479bbc3227fa9c
urriellu <pigmeo@urriellu.net>
2008-11-22 19:42:52 +0000

    Adding NewByArch()

--- pigmeo-compiler/src/PIR/LocalVariable.cs
+++ pigmeo-compiler/src/PIR/LocalVariable.cs
@@ -5,7 +5,7 @@ using PRefl = Pigmeo.Internal.Reflection;
 using Pigmeo.Compiler.UI;
 
 namespace Pigmeo.Compiler.PIR {
-	public class LocalVariable {
+	public abstract class LocalVariable {
 		public Program ParentProgram {
 			get {
 				return ParentMethod.ParentProgram;
@@ -21,11 +21,28 @@ namespace Pigmeo.Compiler.PIR {
 			}
 		}
 
-		public LocalVariable(Method ParentMethod, PRefl.LocalVariable RefldLocalVar) {
+		/*public LocalVariable(Method ParentMethod, PRefl.LocalVariable RefldLocalVar) {
 			ShowInfo.InfoDebug("Converting reflected LocalVariable {0} (in method {1}) to PIR", RefldLocalVar.ToString(), RefldLocalVar.ParentMethod.FullNameWithAssembly);
 			this.ParentMethod = ParentMethod;
 			this.LocalVarType = ParentProgram.Types[RefldLocalVar.VariableType.FullName];
 			Name = RefldLocalVar.Name;
+		}*/
+
+		public static LocalVariable NewByArch(Method ParentMethod, PRefl.LocalVariable RefldLocalVar) {
+			ShowInfo.InfoDebug("Converting reflected LocalVariable {0} (in method {1}) to PIR", RefldLocalVar.ToString(), RefldLocalVar.ParentMethod.FullNameWithAssembly);
+			LocalVariable NewLV = null;
+			switch(ParentMethod.ParentProgram.TargetArch) {
+				case Architecture.PIC:
+					NewLV = new PIC.LocalVariable();
+					break;
+				default:
+					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
+					break; ;
+			}
+			NewLV.ParentMethod = ParentMethod;
+			NewLV.LocalVarType = ParentMethod.ParentProgram.Types[RefldLocalVar.VariableType.FullName];
+			NewLV.Name = RefldLocalVar.Name;
+			return NewLV;
 		}
 
 		public override string ToString() {
