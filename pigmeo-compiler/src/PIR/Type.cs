a3761eaffe2c2ca36218a5c2ee85b6e81359a4a8
urriellu <pigmeo@urriellu.net>
2008-11-22 20:04:00 +0000

    Support for empty instantiations

--- pigmeo-compiler/src/PIR/Type.cs
+++ pigmeo-compiler/src/PIR/Type.cs
@@ -4,6 +4,8 @@ using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
 	public abstract class Type {
+		public Program ParentProgram;
+
 		/// <summary>
 		/// The Type's name. Note that in PIR there is no distinction between Name and FullName, because the Namespace is now part of its name
 		/// </summary>
@@ -20,10 +22,15 @@ namespace Pigmeo.Compiler.PIR {
 		/// </summary>
 		public abstract Type Clone();
 
+		protected Type(Program ParentProgram) {
+			this.ParentProgram = ParentProgram;
+		}
+
 		/// <summary>
 		/// Generates a PIR of a Type from a reflected type
 		/// </summary>
-		public Type(PRefl.Type ReflectedType, bool IncludeMembers) {
+		protected Type(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers) {
+			this.ParentProgram = ParentProgram;
 			Name = ReflectedType.FullName;
 			IsPublic = ReflectedType.IsPublic;
 		}
