4c9cc16bd949e09662ece85fa7dca2445a398feb
urriellu <pigmeo@urriellu.net>
2008-11-22 19:31:27 +0000

    Adding NewByArch() and allowing empty instances.

--- pigmeo-compiler/src/PIR/Class.cs
+++ pigmeo-compiler/src/PIR/Class.cs
@@ -3,11 +3,33 @@ using System.Collections.Generic;
 using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
-	public class Class:ReferenceType {
-		public Class(PRefl.Type ReflectedType, bool IncludeMembers)
-			: base(ReflectedType, IncludeMembers) {
+	public abstract class Class:ReferenceType {
+		protected Class(Program ParentProgram):base(ParentProgram) {
+		}
+
+		protected Class(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers):base(ParentProgram, ReflectedType, IncludeMembers) {
 			IsAbstract = ReflectedType.IsAbstract;
 			IsSealed = ReflectedType.IsSealed;
+		}
+
+		public static Class NewByArch(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers) {
+			switch(ParentProgram.TargetArch) {
+				case Architecture.PIC:
+					return new PIC.Class(ParentProgram, ReflectedType, IncludeMembers);
+				default:
+					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
+					return null;
+			}
+		}
+
+		public static Class NewByArch(Program ParentProgram) {
+			switch(ParentProgram.TargetArch) {
+				case Architecture.PIC:
+					return new PIC.Class(ParentProgram);
+				default:
+					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
+					return null;
+			}
 		}
 
 		public bool IsAbstract;
