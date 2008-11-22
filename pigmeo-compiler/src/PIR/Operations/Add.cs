a8b860b7bb9d083a7d82d6d930b66bcbf74edac9
urriellu <pigmeo@urriellu.net>
2008-11-22 19:16:37 +0000

    New PIR Operations: Add, Call and Return

---
+++ pigmeo-compiler/src/PIR/Operations/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PRefl=Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Add:Operation {
+		public Add(Method ParentMethod):base(ParentMethod) {
+			Operator = "Add";
+			Arguments = new Operand[2];
+			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
+		}
+
+		public override string ToString() {
+			return Label + ": " + Result + " " + AssignmentSign + " " + Arguments[0] + " + " + Arguments[1];
+		}
+	}
+}
