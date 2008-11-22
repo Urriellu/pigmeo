2b11f7b225dbd5191651e5ef01f14243b75d3de6
urriellu <pigmeo@urriellu.net>
2008-11-22 19:45:17 +0000

    Adding NewByArch(), ToStringRetTypeNameArgs() and AvoidTOSS()

--- pigmeo-compiler/src/PIR/Method.cs
+++ pigmeo-compiler/src/PIR/Method.cs
@@ -5,12 +5,12 @@ using PRefl = Pigmeo.Internal.Reflection;
 using Pigmeo.Compiler.UI;
 
 namespace Pigmeo.Compiler.PIR {
-	public class Method:TypeMember {
+	public abstract class Method:TypeMember {
 		public Type ReturnType;
 		public LocalVariableCollection LocalVariables = new LocalVariableCollection();
 		public OperationCollection Operations = new OperationCollection();
 
-		public Method(Program ParentProgram, PRefl.Method ReflectedMethod) {
+		/*protected Method(Program ParentProgram, PRefl.Method ReflectedMethod) {
 			ShowInfo.InfoDebug("Converting reflected method {0} to PIR", ReflectedMethod.FullNameWithAssembly);
 			this.ParentProgram = ParentProgram;
 			ParentType = ParentProgram.Types[ReflectedMethod.ParentType.FullName];
@@ -20,6 +20,28 @@ namespace Pigmeo.Compiler.PIR {
 			IsStatic = ReflectedMethod.IsStatic;
 			IsPublic = ReflectedMethod.IsPublic;
 			IsAbstract = ReflectedMethod.IsAbstract;
+		}*/
+
+		public static Method NewByArch(Program ParentProgram, PRefl.Method ReflectedMethod) {
+			ShowInfo.InfoDebug("Converting reflected method {0} to PIR", ReflectedMethod.FullNameWithAssembly);
+			Method NewMethod = null;
+			switch(ParentProgram.TargetArch) {
+				case Architecture.PIC:
+					NewMethod = new PIC.Method();
+					break;
+				default:
+					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
+					break;
+			}
+			NewMethod.ParentProgram = ParentProgram;
+			NewMethod.ParentType = ParentProgram.Types[ReflectedMethod.ParentType.FullName];
+			NewMethod.ReturnType = ParentProgram.Types[ReflectedMethod.ReturnType.FullName];
+			NewMethod.Name = ReflectedMethod.Name;
+			NewMethod.IsEntryPoint = ReflectedMethod.IsEntryPoint;
+			NewMethod.IsStatic = ReflectedMethod.IsStatic;
+			NewMethod.IsPublic = ReflectedMethod.IsPublic;
+			NewMethod.IsAbstract = ReflectedMethod.IsAbstract;
+			return NewMethod;
 		}
 
 		/// <summary>
@@ -72,6 +94,71 @@ namespace Pigmeo.Compiler.PIR {
 			foreach(Operation Op in Operations) Output += "\t" + Op.ToString() + "\n";
 			Output += "}\n";
 			return Output;
+		}
+
+		/// <summary>
+		/// Returns the strings that represents this Method, including its return type, name and arguments
+		/// </summary>
+		public string ToStringRetTypeNameArgs() {
+			return ReturnType.Name + " " + Name + "()";
+		}
+
+		/// <summary>
+		/// Avoid copying values back and forth to the TOSS, as .NET does. Make operation work with operands directly instead of working always with the TOSS
+		/// </summary>
+		/// <returns>True if something was modified</returns>
+		[PigmeoToDo("Some Copy operations will be removed. If they have incoming jumps, those jumps will point to nowhere and the program will break")]
+		public bool AvoidTOSS() {
+			bool MethodModified = false;
+			bool CurrOpModified;
+
+			do {
+				CurrOpModified = false;
+				foreach(Operation CurrOp in Operations) {
+					if(CurrOp is Copy) {
+						Copy CurrCopyOp = CurrOp as Copy;
+
+						#region [Register]TOSS := something
+						if(CurrOp.Arguments[0] != GlobalOperands.TOSS && CurrOp.Result == GlobalOperands.TOSS) {
+							ShowInfo.InfoDebugDecompile(string.Format("Method {0} BEFORE avoiding the TOSS at Operation {1}", this.FullName, CurrCopyOp), this);
+
+							for(int i = CurrOp.Index + 1 ; i < Operations.Count && !CurrOpModified ; i++) { //find the operation that uses this operand
+								Operation OperUsingTossAsArg = Operations[i];
+								//foreach(Operand CurrArg in Operations[i].Arguments) {
+								for(int j = 0 ; j < OperUsingTossAsArg.Arguments.Length ; j++) {
+									Operand CurrArg = OperUsingTossAsArg.Arguments[j];
+									if(CurrArg == GlobalOperands.TOSS) {
+										ShowInfo.InfoDebug("Moving argument {0} from Operation {1} to argument #{2} and in Operation {3}, and removing the old Cupy Operation", CurrCopyOp.Arguments[0], CurrCopyOp, j, Operations[i]);
+										OperUsingTossAsArg.Arguments[j] = CurrCopyOp.Arguments[0];
+										Operations.Remove(CurrOp);
+										CurrOpModified = MethodModified = true;
+										break;
+									}
+								}
+							}
+
+							ShowInfo.InfoDebugDecompile(string.Format("Method {0} AFTER avoiding the TOSS", this.FullName), this);
+						}
+						#endregion
+						if(CurrOpModified) break; //make the foreach loop to start again, because this.Operations was modified
+					}
+
+					#region The result of an operation is placed on the TOSS and the following Operation copies the TOSS to some operand
+					if(Operations.IndexOf(CurrOp) == Operations.Count - 1) break; //this is the last Operation. Ignore
+					Operation NextOp = Operations[Operations.IndexOf(CurrOp) + 1];
+					if(CurrOp.Result == GlobalOperands.TOSS && NextOp != null && NextOp is Copy && NextOp.Arguments[0] == GlobalOperands.TOSS && NextOp.Result != GlobalOperands.TOSS) {
+						ShowInfo.InfoDebugDecompile(string.Format("Method {0} BEFORE avoiding the TOSS at Operations {1} and {2}", this.FullName, CurrOp, NextOp), this);
+						CurrOp.Result = NextOp.Result;
+						Operations.Remove(NextOp);
+						CurrOpModified = MethodModified = true;
+						ShowInfo.InfoDebugDecompile(string.Format("Method {0} AFTER avoiding the TOSS", this.FullName), this);
+						break;
+					}
+					#endregion
+				}
+			} while(CurrOpModified);
+
+			return MethodModified;
 		}
 	}
 }
