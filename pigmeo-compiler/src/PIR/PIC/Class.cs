4491a6c95d934ae88c7189ec8dc97409df49dd95
urriellu <pigmeo@urriellu.net>
2008-11-22 19:20:51 +0000

    New PIR.PIC classes

---
+++ pigmeo-compiler/src/PIR/PIC/Class.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR.PIC {
+	public class Class:PIR.Class {
+		public Class(PIR.Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers):base(ParentProgram, ReflectedType, IncludeMembers) {
+		}
+
+		public Class(PIR.Program ParentProgram):base(ParentProgram) {
+		}
+	}
+}
---
+++ pigmeo-compiler/src/PIR/PIC/Field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Pigmeo.Internal;
+using Pigmeo.Internal.PIC;
+using PRefl = Pigmeo.Internal.Reflection;
+using Pigmeo.Compiler.UI;
+
+namespace Pigmeo.Compiler.PIR.PIC {
+	public class Field:PIR.Field {
+		public Location Location;
+
+		[PigmeoToDo("Find CustomAttribute Location()")]
+		public Field(PRefl.Field ReflectedField) {
+		}
+	}
+}
