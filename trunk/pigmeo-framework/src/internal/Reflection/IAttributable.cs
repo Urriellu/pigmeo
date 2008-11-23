3139df1fc05d502a1a4c44d7224c430db86a3e34
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-11-23 17:44:45 +0000

    Supporting Custom Attributes in P.I.Reflection

---
+++ trunk/pigmeo-framework/src/internal/Reflection/IAttributable.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Pigmeo.Internal.Reflection {
+	public interface IAttributable {
+		CustomAttrCollection CustomAttributes { get; }
+	}
+}
