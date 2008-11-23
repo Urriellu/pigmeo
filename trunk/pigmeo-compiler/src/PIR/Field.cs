a45784c46637417042f1b698249d7fc29663a855
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-11-23 18:14:42 +0000

    Supporting Custom Attributes and parsing the AsmName() CAttr

--- trunk/pigmeo-compiler/src/PIR/Field.cs
+++ trunk/pigmeo-compiler/src/PIR/Field.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Pigmeo.Internal;
+using Pigmeo.Extensions;
 using PRefl = Pigmeo.Internal.Reflection;
 using Pigmeo.Compiler.UI;
 
 namespace Pigmeo.Compiler.PIR {
 	public abstract class Field:TypeMember {
-		/*public Field(Program ParentProgram, PRefl.Field ReflectedField) {
+		protected Field(Program ParentProgram, PRefl.Field ReflectedField) {
 			ShowInfo.InfoDebug("Converting reflected Field {0} to PIR", ReflectedField.FullNameWithAssembly);
 			this.ParentProgram = ParentProgram;
 			this.ParentType = ParentProgram.Types[ReflectedField.ParentType.FullName];
@@ -14,26 +15,30 @@ namespace Pigmeo.Compiler.PIR {
 			Name = ReflectedField.Name;
 			IsPublic = ReflectedField.IsPublic;
 			IsStatic = ReflectedField.IsStatic;
-		}*/
+
+			foreach(PRefl.CustomAttr cattr in ReflectedField.CustomAttributes) {
+				#region find AsmName
+				if(cattr.CAttrType.FullName == "Pigmeo.AsmName") {
+					try {
+						if(cattr.Parameters.Count == 1) {
+							_AsmName = ((string)cattr.Parameters[0].Value);
+						} else ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", true, "Parsing CustomAttribute Pigmeo.AsmName but constructor is unknown");
+					} catch(InvalidCastException) {
+						ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", true, "Parsing CustomAttribute Pigmeo.AsmName but constructor is unknown (invalid cast)");
+					}
+				}
+				#endregion
+			}
+		}
 
 		public static Field NewByArch(Program ParentProgram, PRefl.Field ReflectedField) {
-			ShowInfo.InfoDebug("Converting reflected Field {0} to PIR", ReflectedField.FullNameWithAssembly);
-			Field NewField = null;
 			switch(ParentProgram.TargetArch) {
 				case Architecture.PIC:
-					NewField = new PIC.Field(ReflectedField);
-					break;
+					return new PIC.Field(ParentProgram, ReflectedField);
 				default:
 					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0001", true);
-					break;
+					return null;
 			}
-			NewField.ParentProgram = ParentProgram;
-			NewField.ParentType = ParentProgram.Types[ReflectedField.ParentType.FullName];
-			NewField.FieldType = ParentProgram.Types[ReflectedField.FieldType.FullName];
-			NewField.Name = ReflectedField.Name;
-			NewField.IsPublic = ReflectedField.IsPublic;
-			NewField.IsStatic = ReflectedField.IsStatic;
-			return NewField;
 		}
 
 		public Type FieldType;
@@ -42,7 +47,9 @@ namespace Pigmeo.Compiler.PIR {
 		public bool IsStatic;
 
 		public override string ToString() {
+			string[] attrs = ToStringAttributes();
 			string Output = "";
+			if(attrs.Length > 0) Output += "[" + attrs.CommaSeparatedList() + "]" + "\n";
 			if(IsPublic) Output += "public ";
 			if(IsStatic) Output += "static ";
 			Output += FieldType.Name + " " + Name;
@@ -51,6 +58,12 @@ namespace Pigmeo.Compiler.PIR {
 
 		public string ToStringTypeAndName() {
 			return FieldType.Name + " " + Name;
+		}
+
+		public virtual string[] ToStringAttributes() {
+			List<string> attrs = new List<string>();
+			attrs.Add("AsmName(\"" + AsmName + "\")");
+			return attrs.ToArray();
 		}
 	}
 }
