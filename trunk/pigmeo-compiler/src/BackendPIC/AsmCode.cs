eb8e763ebabbc55132251348bc245b0ba1d7e128
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-11-27 18:30:40 +0000

    New methods: Add() and AddRange()

--- trunk/pigmeo-compiler/src/BackendPIC/AsmCode.cs
+++ trunk/pigmeo-compiler/src/BackendPIC/AsmCode.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Pigmeo.Compiler.UI;
+using Pigmeo.Compiler;
 
 namespace Pigmeo.Compiler.BackendPIC {
 	/// <summary>
@@ -11,11 +14,12 @@ namespace Pigmeo.Compiler.BackendPIC {
 		/// </summary>
 		public string[] Code {
 			get {
-				string[] code = new string[Instructions.Count];
-				for(int i=0;i<Instructions.Count;i++){
-					Code[i]=Instructions[i].ToString();
+				ShowInfo.InfoDebug("Converting the objects representing assembly code to actual source code. There are {0} instructions", Instructions.Count);
+				string[] NewCode = new string[Instructions.Count];
+				for(int i = 0 ; i < Instructions.Count ; i++) {
+					NewCode[i] = Instructions[i].ToString();
 				}
-				return code;
+				return NewCode;
 			}
 		}
 
@@ -24,6 +28,34 @@ namespace Pigmeo.Compiler.BackendPIC {
 
 
 		#region methods
+		/// <summary>
+		/// Adds an assembly-language instruction to the end of this code
+		/// </summary>
+		/// <param name="Instruction">Instruction to add</param>
+		public void Add(AsmInstruction Instruction) {
+			Instructions.Add(Instruction);
+		}
+
+		/// <summary>
+		/// Adds the given code to the end of this
+		/// </summary>
+		/// <param name="Code">Code to add</param>
+		public void Add(AsmCode Code) {
+			Instructions.AddRange(Code.Instructions);
+		}
+
+		/// <summary>
+		/// Adds all the given codes to the end of this
+		/// </summary>
+		/// <param name="Codes">Codes to add</param>
+		/// <param name="EmptyLines">Amount of empty lines to put between codes</param>
+		public void AddRange(AsmCode[] Codes, UInt16 EmptyLines) {
+			for(int i = 0 ; i < Codes.Length ; i++) {
+				Instructions.AddRange(Codes[i].Instructions);
+				if(i < Codes.Length - 1) Instructions.Add(new Label("", ""));
+			}
+		}
+
 		public override string ToString() {
 			return ToString(config.Internal.EndOfLine);
 		}
