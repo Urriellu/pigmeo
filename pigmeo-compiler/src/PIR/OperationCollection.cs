2d03f2ab1135076ef01a478f9e0d5c0c0749f3d2
urriellu <pigmeo@urriellu.net>
2008-12-06 17:47:20 +0000

    Insert() and Remove() methods overriden, now they update other operations that reference another operation which has just being moved

--- pigmeo-compiler/src/PIR/OperationCollection.cs
+++ pigmeo-compiler/src/PIR/OperationCollection.cs
@@ -12,5 +12,34 @@ namespace Pigmeo.Compiler.PIR {
 			if(!Contains(PreviousOperation)) throw new ArgumentException("PreviousOperation couldn't be found in this collection");
 			Insert(IndexOf(PreviousOperation) + 1, Item);
 		}
+
+		public new void Insert(int Index, Operation Item) {
+			base.Insert(Index, Item);
+
+			//update arguments that points to an operation that has just changed its index
+			foreach(Operation O in this) {
+				foreach(Operand Op in O.Arguments) {
+					if(Op is OperationOperand) {
+						OperationOperand OpOp = Op as OperationOperand;
+						if(OpOp.OperationIndex >= Index) OpOp.OperationIndex++;
+					}
+				}
+			}
+		}
+
+		public new void Remove(Operation Item) {
+			int RemovedItemIndex = IndexOf(Item);
+			base.Remove(Item);
+			
+			//update arguments that points to an operation that has just changed its index
+			foreach(Operation O in this) {
+				foreach(Operand Op in O.Arguments) {
+					if(Op is OperationOperand) {
+						OperationOperand OpOp = Op as OperationOperand;
+						if(OpOp.OperationIndex > RemovedItemIndex) OpOp.OperationIndex--;
+					}
+				}
+			}
+		}
 	}
 }
