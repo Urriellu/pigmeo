c710ae2f14251bea6b3640ad3a69e08e6c634a94
urriellu <pigmeo@urriellu.net>
2008-12-06 12:13:54 +0000

    Removing unuseful Delay() methods

--- pigmeo-framework/src/MCU/Processor.cs
+++ pigmeo-framework/src/MCU/Processor.cs
@@ -13,11 +13,22 @@ namespace Pigmeo.MCU {
 		/// No meaningful operation is performed although a processing cycle will be consumed
 		/// </summary>
 		public static void Nop() {
+			Nop(1);
+		}
+
+		/// <summary>
+		/// No meaningful operation is performed although the given amount of cycles will be wasted
+		/// </summary>
+		public static void Nop(int Instructions) {
+			throw new NotImplementedException();
 		}
 
 		/// <summary>
 		/// Keeps the CPU doing nothing useful for a given amount of time (in milliseconds)
 		/// </summary>
+		/// <remarks>
+		/// Note: in most microcontroller architectures, this is done by keeping the CPU on an infinite loop, wasting power. Avoid it on battery-powered devices
+		/// </remarks>
 		public static void Delay(int milliseconds) {
 			Thread.Sleep(milliseconds);
 		}
@@ -27,20 +38,6 @@ namespace Pigmeo.MCU {
 		/// </summary>
 		public static void Delay(float Time, TimeUnitsSI Unit) {
 			Thread.Sleep(((int)(new Period(Time, Unit)).GetValue(SIPrefixes.m, TimeUnits.second)));
-		}
-
-		/// <summary>
-		/// Keeps the CPU doing nothing useful for a given amount of time
-		/// </summary>
-		public static void Delay(TimeSpan Time) {
-			Thread.Sleep(Time);
-		}
-
-		/// <summary>
-		/// Keeps the CPU doing nothing useful for a given amount of time
-		/// </summary>
-		public static void Delay(Period T) {
-			Thread.Sleep(((int)T.GetValue(SIPrefixes.m, TimeUnits.second)));
 		}
 	}
 }
