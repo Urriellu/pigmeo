3918841d0c6e4f593c4a510b78722bc448678941
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-06 12:04:52 +0000

    New methods InsertBefore() and InsertAfter()

--- trunk/pigmeo-compiler/src/PIR/OperationCollection.cs
+++ trunk/pigmeo-compiler/src/PIR/OperationCollection.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 
 namespace Pigmeo.Compiler.PIR {
 	public class OperationCollection:List<Operation> {
+		public void InsertBefore(Operation NextOperation, Operation Item) {
+			if(!Contains(NextOperation)) throw new ArgumentException("NextOperation couldn't be found in this collection");
+			Insert(IndexOf(NextOperation), Item);
+		}
+
+		public void InsertAfter(Operation PreviousOperation, Operation Item) {
+			if(!Contains(PreviousOperation)) throw new ArgumentException("PreviousOperation couldn't be found in this collection");
+			Insert(IndexOf(PreviousOperation) + 1, Item);
+		}
 	}
 }
