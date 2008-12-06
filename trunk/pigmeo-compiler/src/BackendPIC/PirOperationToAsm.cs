e89ebb676e0b07aae16a200de721f2751abe90bd
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-06 17:36:31 +0000

    Adding labels to asm instructions

--- trunk/pigmeo-compiler/src/BackendPIC/PirOperationToAsm.cs
+++ trunk/pigmeo-compiler/src/BackendPIC/PirOperationToAsm.cs
@@ -12,27 +12,29 @@ namespace Pigmeo.Compiler.BackendPIC {
 
 			if(O is PIR.Copy) {
 				// W:=constant
-				if(O.Arguments[0] is PIR.ConstantInt32Operand && O.Result == GlobalOperands.W) Code.Add(new MOVLW("", (byte)((O.Arguments[0] as PIR.ConstantInt32Operand).Value), ""));
+				if(O.Arguments[0] is PIR.ConstantInt32Operand && O.Result == GlobalOperands.W) Code.Add(new MOVLW(O.AsmLabel, (byte)((O.Arguments[0] as PIR.ConstantInt32Operand).Value), ""));
 
 				// W:=8bitField
-				if(O.Arguments[0] is PIR.FieldOperand && (O.Arguments[0] as PIR.FieldOperand).TheField.Size == 1 && O.Result == GlobalOperands.W) Code.Add(new MOVF("", (O.Arguments[0] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
+				if(O.Arguments[0] is PIR.FieldOperand && (O.Arguments[0] as PIR.FieldOperand).TheField.Size == 1 && O.Result == GlobalOperands.W) Code.Add(new MOVF(O.AsmLabel, (O.Arguments[0] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
 
 				// 8bitField:=W
-				if(O.Arguments[0] == GlobalOperands.W && O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.Size == 1) Code.Add(new MOVWF("", (O.Result as PIR.FieldOperand).TheField.AsmName, ""));
+				if(O.Arguments[0] == GlobalOperands.W && O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.Size == 1) Code.Add(new MOVWF(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField.AsmName, ""));
 			} else if(O is PIR.Add) {
 				if(O.Arity != 2) ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", false, "Invalid addition arity: " + O.Arity);
 
 				// W:=W+8bitField
-				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.FieldOperand && (O.Arguments[1] as PIR.FieldOperand).TheField.Size == 1) Code.Add(new ADDWF("", (O.Arguments[1] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
+				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.FieldOperand && (O.Arguments[1] as PIR.FieldOperand).TheField.Size == 1) Code.Add(new ADDWF(O.AsmLabel, (O.Arguments[1] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
 
 				// W:=W+constant
-				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.ConstantInt32Operand) Code.Add(new ADDLW("", (byte)((O.Arguments[1] as PIR.ConstantInt32Operand).Value), ""));
+				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.ConstantInt32Operand) Code.Add(new ADDLW(O.AsmLabel, (byte)((O.Arguments[1] as PIR.ConstantInt32Operand).Value), ""));
+			}else if(O is PIR.Jump){
+				Code.Add(new GOTO(O.AsmLabel, (O.Arguments[0] as PIR.OperationOperand).TheOperation.AsmLabel, ""));
 			} else if(O is PIR.Return) {
-				if(O.ParentMethod.IsEntryPoint) Code.Add(new GOTO("", "EndOfApp", ""));
+				if(O.ParentMethod.IsEntryPoint) Code.Add(new GOTO(O.AsmLabel, "EndOfApp", ""));
 			}
 
 			if(Code.Instructions.Count == 0) {
-				Code.Add(new Label("", O.ToString() + "    NOT IMPLEMENTED!!!"));
+				Code.Add(new Label(O.AsmLabel, O.ToString() + "    NOT IMPLEMENTED!!!"));
 				ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", false, "Convert \"" + O.ToString() + "\" to PIC assembly language");
 			}
 
