bd1defde3e20466f99dcf24a8e563d2542096ccd
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-19 15:17:46 +0000

    Old compilation process moved from trunk to old-code

--- trunk/pigmeo-compiler/src/Backend.cs
+++ trunk/pigmeo-compiler/src/Backend.cs
@@ -12,35 +12,6 @@ namespace Pigmeo.Compiler {
 	/// </summary>
 	public static class Backend {
 		/// <summary>
-		/// Parses a bundled assembly generated from Frontend() and converts it to assembly code
-		/// </summary>
-		/// <remarks>
-		/// Before calling this method take care of the required variables placed in the config class (AssemblyToCompile...)
-		/// </remarks>
-		[Obsolete("Use the new compilation process by calling Backend.Run()")]
-		public static List<string> RunBackend(AssemblyDefinition AssemblyToCompile) {
-			ShowInfo.InfoDebug("Running the backend");
-			List<string> AsmCode = new List<string>();
-			DeviceTarget target = DeviceTarget.GetDeviceTarget(AssemblyToCompile);
-			config.Compilation.TargetDeviceInfo = target.GetDeviceInfo();
-			UIs.UpdateProgressBar(45);
-			ShowInfo.InfoVerbose(i18n.str(113, AssemblyToCompile.Name.Name, target.branch.ToString(), target.arch.ToString()));
-			switch(target.arch) {
-				case Architecture.PIC:
-					AsmCode = BackendPIC.Backend.RunBrackend(AssemblyToCompile);
-					break;
-				default:
-					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "BE0001", true, target.arch.ToString());
-					break;
-			}
-			if(config.Internal.GenerateAsmFile) {
-				SaveAsmToFile(AsmCode.ToArray(), config.Internal.FileAsm);
-			}
-			GlobalShares.CompilationProgress = 77;
-			return AsmCode;
-		}
-
-		/// <summary>
 		/// Parses a PIR Program generated in the Frontend and converts it to assembly code
 		/// </summary>
 		/// <remarks>
