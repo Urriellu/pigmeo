b079e7a84991ad21c0a95b1617d6b9f8f71d0016
urriellu <pigmeo@urriellu.net>
2008-12-19 17:40:51 +0000

    Overriding operators == and !=

--- pigmeo-compiler/src/PIR/Operand.cs
+++ pigmeo-compiler/src/PIR/Operand.cs
@@ -8,5 +8,15 @@ namespace Pigmeo.Compiler.PIR {
 		public override string ToString() {
 			return "[UnknownOperand]";
 		}
+
+		public static bool operator ==(Operand First, Operand Second) {
+			if(First is ConstantInt32Operand && Second is ConstantInt32Operand) return (First as ConstantInt32Operand).Value == (Second as ConstantInt32Operand).Value;
+			if(First is FieldOperand && Second is FieldOperand) return (First as FieldOperand).TheField == (Second as FieldOperand).TheField;
+			return Object.ReferenceEquals(First, Second);
+		}
+
+		public static bool operator !=(Operand First, Operand Second) {
+			return !(First == Second);
+		}
 	}
 }
