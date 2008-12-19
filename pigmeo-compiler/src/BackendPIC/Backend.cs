2299fb262be038dd979da727d7afb4e7dcbe0633
urriellu <pigmeo@urriellu.net>
2008-12-19 15:26:25 +0000

    Old compilation process moved from trunk to old-code

--- pigmeo-compiler/src/BackendPIC/Backend.cs
+++ pigmeo-compiler/src/BackendPIC/Backend.cs
@@ -12,55 +12,6 @@ namespace Pigmeo.Compiler.BackendPIC {
 	/// Converts a .NET assembly to assembly language for a 8-bit Microchip PIC microcontroller
 	/// </summary>
 	public class Backend {
-		/// <summary>
-		/// Runs the backend/compiler
-		/// </summary>
-		/// <returns>
-		/// Compiled source code in assembly language. Each value of the collection represents a line
-		/// </returns>
-		[Obsolete("Use the new compilation process. Call BackendPIC.Backend.Run() instead")]
-		public static List<string> RunBrackend(AssemblyDefinition OriginalAssembly) {
-			ShowInfo.InfoDebug("Running the PIC backend");
-
-			AssemblyDefinition OptimizedAssembly = OptimizeCIL(OriginalAssembly);
-			GlobalShares.CompilationProgress = 50;
-
-			AssemblyDefinition AssemblyWithKernel = AddKernel(OptimizedAssembly);
-			GlobalShares.CompilationProgress = 55;
-
-			Asm AsmLangApp = CompileToAsm.Run(AssemblyWithKernel);
-			GlobalShares.CompilationProgress = 65;
-
-			Asm OptimizedAsmApp = OptimizeAsm(AsmLangApp);
-			GlobalShares.CompilationProgress = 75;
-
-			return OptimizedAsmApp.AsmCode;
-		}
-
-		[PigmeoToDo("Unimplemented"), Obsolete("Use the new compilation process")]
-		private static AssemblyDefinition OptimizeCIL(AssemblyDefinition AssemblyToOptimize) {
-			ShowInfo.InfoDebug("Optimizing CIL for PIC architecture");
-
-			AssemblyDefinition OptimizedAssembly = AssemblyToOptimize;
-			return OptimizedAssembly;
-		}
-
-		[PigmeoToDo("Unimplemented"), Obsolete("Use the new compilation process")]
-		private static AssemblyDefinition AddKernel(AssemblyDefinition assembly) {
-			ShowInfo.InfoDebug("Adding the kernel to the assembly");
-
-			AssemblyDefinition AssemblyWithKernel = assembly;
-			return AssemblyWithKernel;
-		}
-
-		[PigmeoToDo("Unimplemented"), Obsolete("Use the new compilation process")]
-		private static Asm OptimizeAsm(Asm asm) {
-			ShowInfo.InfoDebug("Optimizing the assembly language for the PIC architecture");
-
-			Asm OptimizedAsm = new Asm(asm);
-			return OptimizedAsm;
-		}
-
 		protected static UInt16 TitleLength = 76;
 		protected static char TitleChar = '=';
 
