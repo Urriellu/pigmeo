b29dd368a20b8c90d7d5ed9edaac9851c20da4aa
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-19 15:19:32 +0000

    Old compilation process moved from trunk to old-code

--- trunk/pigmeo-compiler/src/GlobalShares.cs
+++ trunk/pigmeo-compiler/src/GlobalShares.cs
@@ -42,43 +42,6 @@ namespace Pigmeo.Compiler {
 		/// <summary>
 		/// Runs the compilation
 		/// </summary>
-		[Obsolete("Use Compile(string FileToCompile")]
-		public static void Compile() {
-			DateTime StartTime = DateTime.Now;
-			ErrorsAndWarnings.TotalErrors = 0;
-			CompilationProgress = 0;
-			try {
-				CilFrontend.Frontend();
-				CompilationProgress = 40;
-				if(ErrorsAndWarnings.TotalErrors > 0) {
-					ShowInfo.InfoVerbose(i18n.str(136, ErrorsAndWarnings.TotalErrors));
-					return;
-				}
-				Backend.RunBackend(GlobalShares.AssemblyToCompile);
-				if(ErrorsAndWarnings.TotalErrors > 0) {
-					ShowInfo.InfoVerbose(i18n.str(136, ErrorsAndWarnings.TotalErrors));
-					return;
-				}
-				CompilationProgress = 80;
-				//Assembler.RunAssembler();
-			} catch(Exception e) {
-				if(ErrorsAndWarnings.TotalErrors > 0) ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0008", false);
-				else throw e;
-			}
-
-			if(ErrorsAndWarnings.TotalErrors == 0) {
-				ShowInfo.InfoVerbose(i18n.str(11));
-				GlobalShares.CompilationProgress = 100;
-			} else ShowInfo.InfoVerbose(i18n.str("CompEndErrors", ErrorsAndWarnings.TotalErrors));
-			DateTime EndTime = DateTime.Now;
-			TimeSpan CompilationTime = EndTime - StartTime;
-			ShowInfo.InfoVerbose(i18n.str("CompileTime", CompilationTime.Minutes, CompilationTime.Seconds, CompilationTime.Milliseconds));
-			if(ErrorsAndWarnings.TotalErrors > 0) Environment.Exit(1);
-		}
-
-		/// <summary>
-		/// Runs the compilation
-		/// </summary>
 		public static string[] Compile(string CompilingFile) {
 			string[] AssemblyCode = null;
 			DateTime StartTime = DateTime.Now;
