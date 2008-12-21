f7fd31c650b3b205a9c430f09827273324ac8756
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-21 19:44:10 +0000

    DataMemoryBankPIC renamed to DataMemoryBank. Supporting memory bank #3

--- trunk/pigmeo-framework/src/devices/PIC16F887.cs
+++ trunk/pigmeo-framework/src/devices/PIC16F887.cs
@@ -1,5 +1,6 @@
 ﻿using Pigmeo;
 using Pigmeo.Internal;
+using Pigmeo.Internal.PIC;
 
 [assembly: DeviceLibrary(Architecture.PIC, Family.PIC14, Branch.PIC16F887)]
 
@@ -14,7 +15,7 @@ namespace Pigmeo.MCU {
 			device.Architecture = Architecture.PIC;
 			device.Family = Family.PIC14;
 			device.Branch = Branch.PIC16F887;
-			device.DataMemory = new DataMemoryBankPIC[4];
+			device.DataMemory = new DataMemoryBank[4];
 			device.DataMemory[0].FirstSFR = 0x00;
 			device.DataMemory[0].LastSFR = 0x1F;
 			device.DataMemory[0].FirstGPR = 0x20;
@@ -25,6 +26,8 @@ namespace Pigmeo.MCU {
 			device.DataMemory[1].LastGPR = 0x3F;
 			device.DataMemory[2].FirstSFR = 0x00;
 			device.DataMemory[2].LastSFR = 0x0F;
+			device.DataMemory[3].FirstSFR = 0x00;
+			device.DataMemory[3].LastSFR = 0x0D;
 			device.MaxWords = 8192;
 			device.IncludeFile = "p16f887.inc";
 
