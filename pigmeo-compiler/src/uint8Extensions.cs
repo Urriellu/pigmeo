b06889002e0825daf8657f770c8c1d5f4b5a5e97
urriellu <pigmeo@urriellu.net>
2008-12-21 19:41:00 +0000

    ToAsmString() moved to uint16Extensions

--- pigmeo-compiler/src/uint8Extensions.cs
+++ pigmeo-compiler/src/uint8Extensions.cs
@@ -13,33 +13,7 @@ namespace Pigmeo.Compiler {
 		/// Returns an string representing the value in the configured numeral system for the target architecture
 		/// </summary>
 		public static string ToAsmString(this byte num, Architecture TargetArch) {
-			string str = "";
-			switch(TargetArch) {
-				case Architecture.PIC:
-					switch(config.Internal.NumeralSystem) {
-						case NumeralSystems.Binary:
-							str="B'" + Convert.ToString(num, 2) + "'";
-							break;
-						case NumeralSystems.Decimal:
-							str="D'" + Convert.ToString(num, 10) + "'";
-							break;
-						case NumeralSystems.Hexadecimal:
-							str = "0x" + num.ToString("X2");
-							break;
-						case NumeralSystems.Octal:
-							str = "O'" + Convert.ToString(num, 8) + "'";
-							break;
-						default:
-							ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "BE0002", false, config.Internal.NumeralSystem.ToString());
-							break;
-					}
-					break;
-				default:
-					ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "BE0001", false, TargetArch.ToString());
-					break;
-			}
-			ShowInfo.InfoDebug("A Byte variable has been converted to a string. Number: {0}, Architecture: {1}, Numeral System: {2}, Result: {3}", num, TargetArch.ToString(), config.Internal.NumeralSystem, str);
-			return str;
+			return ((UInt16)num).ToAsmString(TargetArch);
 		}
 	}
 }
