3870e490ef2ecf9e2b016d1b54d19b6e52591c32
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-21 19:23:39 +0000

    New PIC asm instructions: BANKSEL and ERRORLEVEL

---
+++ trunk/pigmeo-compiler/src/BackendPIC/instructions/ERRORLEVEL.cs
@@ -0,0 +1,18 @@
+﻿namespace Pigmeo.Compiler.BackendPIC {
+	/// <summary>
+	/// Sets the types of messages that are printed in the listing file and error file
+	/// </summary>
+	public class ERRORLEVEL:AsmInstruction {
+		/// <summary>
+		/// Sets the types of messages that are printed in the listing file and error file
+		/// </summary>
+		public ERRORLEVEL(string FirstValue, string comment) {
+			directive = Directive.ERRORLEVEL;
+			type = InstructionType.Directive_str;
+
+			//this.Prefix = "#";
+			this.FirstValue = FirstValue;
+			this.comment = comment;
+		}
+	}
+}
