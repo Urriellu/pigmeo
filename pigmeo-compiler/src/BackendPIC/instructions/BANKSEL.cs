377234e6e2f173598f5b35973626a8123618d2b5
urriellu <pigmeo@urriellu.net>
2008-12-21 19:23:39 +0000

    New PIC asm instructions: BANKSEL and ERRORLEVEL

---
+++ pigmeo-compiler/src/BackendPIC/instructions/BANKSEL.cs
@@ -0,0 +1,18 @@
+﻿namespace Pigmeo.Compiler.BackendPIC {
+	/// <summary>
+	/// Generates bank selecting code to set the bank to the bank containing the designated label
+	/// </summary>
+	public class BANKSEL:AsmInstruction {
+		/// <summary>
+		/// Generates bank selecting code to set the bank to the bank containing the designated label
+		/// </summary>
+		public BANKSEL(string label, string f, string comment) {
+			OP = OpCode.BANKSEL;
+			type = InstructionType.ByteOriented_f;
+
+			this.file = f;
+			this.label = label;
+			this.comment = comment;
+		}
+	}
+}
