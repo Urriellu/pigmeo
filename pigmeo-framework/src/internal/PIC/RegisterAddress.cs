f920828d1364d1f56015d93785a0cb91ff338e59
urriellu <pigmeo@urriellu.net>
2008-12-21 19:46:38 +0000

    New property: FullAddress

--- pigmeo-framework/src/internal/PIC/RegisterAddress.cs
+++ pigmeo-framework/src/internal/PIC/RegisterAddress.cs
@@ -1,4 +1,5 @@
-﻿namespace Pigmeo.Internal.PIC {
+﻿using System;
+namespace Pigmeo.Internal.PIC {
 	public class RegisterAddress {
 		public bool Undefined;
 		public byte Bank;
@@ -19,6 +20,16 @@
 			this.Bank = Bank;
 			this.Address = Address;
 			this.Bit = Bit;
+		}
+
+		/// <summary>
+		/// The full address of this register. For example if OPTION_REG is at 0x01 on bank 1 (the second bank), its FullAddress is 0x81; if TMR0 is at 0x01 on bank 2 its FullAddress is 0x101
+		/// </summary>
+		public UInt16 FullAddress {
+			get {
+				//return (UInt16)(Address + ((UInt16)Bank) << 7);
+				return (UInt16)(128 * Bank + Address);
+			}
 		}
 	}
 }
