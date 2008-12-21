f5a1a2d1dc70f89eb75c275306aa753a7e1197d9
urriellu <pigmeo@urriellu.net>
2008-12-21 19:53:04 +0000

    More documentation. DataMemoryBankPIC renamed to DataMemoryBank

--- pigmeo-framework/src/internal/InfoPIC.cs
+++ pigmeo-framework/src/internal/InfoPIC.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using Pigmeo.Internal.PIC;
 
 namespace Pigmeo.Internal {
+	/// <summary>
+	/// Contains all the required information about a Microchip PIC
+	/// </summary>
 	public class InfoPIC:InfoDevice {
 		public string IncludeFile;
 
 		/// <summary>
 		/// RAM/data memory banks. Each index in the array represents a bank
 		/// </summary>
-		public DataMemoryBankPIC[] DataMemory;
+		public DataMemoryBank[] DataMemory;
 
 		/// <summary>
 		/// Max amount of words/instructions stored in the program memory
@@ -20,12 +24,12 @@ namespace Pigmeo.Internal {
 		}
 
 		/// <summary>
-		/// Total amount of RAM available for General Purpose Registers
+		/// Total amount of RAM available for General Purpose Registers, in bytes
 		/// </summary>
 		public UInt16 GprSize {
 			get {
 				UInt16 total = 0;
-				foreach(DataMemoryBankPIC bank in DataMemory) {
+				foreach(DataMemoryBank bank in DataMemory) {
 					total += bank.GprSize;
 				}
 				return total;
@@ -38,7 +42,7 @@ namespace Pigmeo.Internal {
 		public UInt16 SfrSize {
 			get {
 				UInt16 total = 0;
-				foreach(DataMemoryBankPIC bank in DataMemory) {
+				foreach(DataMemoryBank bank in DataMemory) {
 					total += bank.SfrSize;
 				}
 				return total;
