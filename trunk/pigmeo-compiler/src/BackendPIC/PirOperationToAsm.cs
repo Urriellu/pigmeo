a2eb99572e63a7d6c3261fd917237b9fb080b3d5
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-21 19:29:34 +0000

    Supporting BANKSEL

--- trunk/pigmeo-compiler/src/BackendPIC/PirOperationToAsm.cs
+++ trunk/pigmeo-compiler/src/BackendPIC/PirOperationToAsm.cs
@@ -14,23 +14,37 @@ namespace Pigmeo.Compiler.BackendPIC {
 				// W:=constant
 				if(O.Arguments[0] is PIR.ConstantInt32Operand && O.Result == GlobalOperands.W) Code.Add(new MOVLW(O.AsmLabel, (byte)((O.Arguments[0] as PIR.ConstantInt32Operand).Value), ""));
 
-				// W:=8bitField
-				if(O.Arguments[0] is PIR.FieldOperand && (O.Arguments[0] as PIR.FieldOperand).TheField.Size == 1 && O.Result == GlobalOperands.W) Code.Add(new MOVF(O.AsmLabel, (O.Arguments[0] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
-
-				// 8bitField:=W
-				if(O.Arguments[0] == GlobalOperands.W && O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.Size == 1) Code.Add(new MOVWF(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField.AsmName, ""));
+				// W:=8bitStaticField
+				if(O.Arguments[0] is PIR.FieldOperand && (O.Arguments[0] as PIR.FieldOperand).TheField.IsStatic && (O.Arguments[0] as PIR.FieldOperand).TheField.Size == 1 && O.Result == GlobalOperands.W) {
+					Code.Add(AsmInstruction.SelectRamBank(O.AsmLabel, (O.Arguments[0] as PIR.FieldOperand).TheField as PIR.PIC.Field));
+					Code.Add(new MOVF("", (O.Arguments[0] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
+				}
+				
+				// 8bitStaticField:=W
+				if(O.Arguments[0] == GlobalOperands.W && O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.IsStatic && (O.Result as PIR.FieldOperand).TheField.Size == 1){
+					Code.Add(AsmInstruction.SelectRamBank(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField as PIR.PIC.Field));
+					Code.Add(new MOVWF("", (O.Result as PIR.FieldOperand).TheField.AsmName, ""));
+				}
 			} else if(O is PIR.Add) {
 				if(O.Arity != 2) ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", false, "Invalid addition arity: " + O.Arity);
 
 				//StaticField++
-				if(O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.IsStatic && O.Result == O.Arguments[0] && O.Arguments[1] is PIR.ConstantInt32Operand && (O.Arguments[1] as PIR.ConstantInt32Operand).Value == 1) Code.Add(new INCF(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField.AsmName, Destination.F, ""));
-
+				if(O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.IsStatic && O.Result == O.Arguments[0] && O.Arguments[1] is PIR.ConstantInt32Operand && (O.Arguments[1] as PIR.ConstantInt32Operand).Value == 1){
+					Code.Add(AsmInstruction.SelectRamBank(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField as PIR.PIC.Field));
+					Code.Add(new INCF("", (O.Result as PIR.FieldOperand).TheField.AsmName, Destination.F, ""));
+				}
+				
 				//StaticField--
-				if(O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.IsStatic && O.Result == O.Arguments[0] && O.Arguments[1] is PIR.ConstantInt32Operand && (O.Arguments[1] as PIR.ConstantInt32Operand).Value == -1) Code.Add(new DECF(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField.AsmName, Destination.F, ""));
-
-
-				// W:=W+8bitField
-				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.FieldOperand && (O.Arguments[1] as PIR.FieldOperand).TheField.Size == 1) Code.Add(new ADDWF(O.AsmLabel, (O.Arguments[1] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
+				if(O.Result is PIR.FieldOperand && (O.Result as PIR.FieldOperand).TheField.IsStatic && O.Result == O.Arguments[0] && O.Arguments[1] is PIR.ConstantInt32Operand && (O.Arguments[1] as PIR.ConstantInt32Operand).Value == -1) {
+					Code.Add(AsmInstruction.SelectRamBank(O.AsmLabel, (O.Result as PIR.FieldOperand).TheField as PIR.PIC.Field));
+					Code.Add(new DECF("", (O.Result as PIR.FieldOperand).TheField.AsmName, Destination.F, ""));
+				}
+				
+				// W:=W+8bitStaticField
+				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.FieldOperand && (O.Arguments[1] as PIR.FieldOperand).TheField.IsStatic && (O.Arguments[1] as PIR.FieldOperand).TheField.Size == 1) {
+					Code.Add(AsmInstruction.SelectRamBank(O.AsmLabel, (O.Arguments[1] as PIR.FieldOperand).TheField as PIR.PIC.Field));
+					Code.Add(new ADDWF("", (O.Arguments[1] as PIR.FieldOperand).TheField.AsmName, Destination.W, ""));
+				}
 
 				// W:=W+constant
 				if(O.Arguments[0] == GlobalOperands.W && O.Result == GlobalOperands.W && O.Arguments[1] is PIR.ConstantInt32Operand) Code.Add(new ADDLW(O.AsmLabel, (byte)((O.Arguments[1] as PIR.ConstantInt32Operand).Value), ""));
