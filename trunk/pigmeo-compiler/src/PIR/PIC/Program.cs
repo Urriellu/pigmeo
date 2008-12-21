5a2f2f46622dca1662f6428325ac35fc2b22a42d
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-21 19:33:22 +0000

    AssignLocations() was implemented. Supporting DataMemory and StaticMemory

--- trunk/pigmeo-compiler/src/PIR/PIC/Program.cs
+++ trunk/pigmeo-compiler/src/PIR/PIC/Program.cs
@@ -9,11 +9,38 @@ namespace Pigmeo.Compiler.PIR.PIC {
 	/// Represents a Program for Microchip PIC Microcontrollers
 	/// </summary>
 	public class Program:PIR.Program {
+		public readonly DataMemory DataMemory;
+
+		/// <summary>
+		/// Instantiates a new PIR.PIC.Program
+		/// </summary>
+		/// <remarks>
+		/// Almost everything is retrieved from the reflected assembly at PIR.Program.GetFromCIL(), not here
+		/// </remarks>
+		public Program(PRefl.Assembly ReflectedAssembly):base(ReflectedAssembly) {
+			DataMemory = new DataMemory(this);
+
+			//now we choose the amount of data memory assigned to each type of memory
+			DataMemory.StaticMemory.AssignAllGpr();
+			//[stack missing]
+			//[heap missing]
+			//...
+			//[more types of memory here]
+		}
+
 		/// <summary>
 		/// Assign the Location() attribute to static fields that don't have it yet
 		/// </summary>
 		public void AssignLocations() {
-			ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", false);
+			foreach(Type T in Types) {
+				foreach(Field F in T.Fields) {
+					if(F.IsStatic && !F.Location.DefinedInHeader && F.Location.Address.Undefined) {
+						if(F.Size > Target.GprSize) ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "PC0008", false, string.Format("Size of Field {0} bigger than General Purpose Registers size on {1}", F.ToStringTypeAndName(), Target.Branch));
+						F.Location = DataMemory.StaticMemory.Allocate((byte)F.Size);
+						ShowInfo.InfoDebug("Assigned location {0} to static field {1}", F.Location, F.ToStringTypeAndName());
+					} else ShowInfo.InfoDebug("{0} doesn't need to get a new Location assigned", F.ToStringTypeAndName());
+				}
+			}
 		}
 
 		/// <summary>
