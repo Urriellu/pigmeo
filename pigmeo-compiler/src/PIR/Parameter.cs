5421caf9863710bae6924eb57d5bd3d739f59188
urriellu <pigmeo@urriellu.net>
2008-12-24 11:04:25 +0000

    Adding support for PIR Method Parameters

---
+++ pigmeo-compiler/src/PIR/Parameter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+using Pigmeo.Internal;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Represents a PIR Method Parameter
+	/// </summary>
+	public class Parameter {
+		protected readonly PRefl.Parameter OriginalParameter;
+
+		/// <summary>
+		/// Instantiates a new Parameter
+		/// </summary>
+		/// <param name="ParentMethod">Method this parameter belongs to</param>
+		/// <param name="ReflectedParameter">The reflected parameter</param>
+		public Parameter(Method ParentMethod, PRefl.Parameter ReflectedParameter) {
+			this.ParentMethod = ParentMethod;
+			this.OriginalParameter = ReflectedParameter;
+			Name = ReflectedParameter.Name;
+			Index = ReflectedParameter.Index;
+			ParamType = ParentProgram.Types[ReflectedParameter.ParamType.FullName];
+		}
+
+		/// <summary>
+		/// Type of this parameters
+		/// </summary>
+		public Type ParamType;
+
+		/// <summary>
+		/// PIR Program this Parameter is contained in
+		/// </summary>
+		public Program ParentProgram {
+			get {
+				return ParentMethod.ParentProgram;
+			}
+		}
+
+		/// <summary>
+		/// The Field's parent Method. That's the Method this Field is contained in
+		/// </summary>
+		public Method ParentMethod;
+
+		/// <summary>
+		/// Name of this Parameter
+		/// </summary>
+		public string Name;
+
+		/// <summary>
+		/// Index of this Parameter in the list of its parent method parameters
+		/// </summary>
+		public UInt16 Index;
+
+		public override string ToString() {
+			return ParamType.Name + " " + Name;
+		}
+	}
+}
---
+++ pigmeo-compiler/src/PIR/ParameterCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Pigmeo.Extensions;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Represents a collection of Method Parameters
+	/// </summary>
+	public class ParameterCollection:Dictionary<UInt16,Parameter> {
+		/// <summary>
+		/// Creates a new collection of parameters with the given default capacity
+		/// </summary>
+		/// <param name="Capacity">Default capacity. This collection will automatically resize itself when needed</param>
+		public ParameterCollection(int Capacity) : base(Capacity) { }
+
+		/// <summary>
+		/// Indicates if a parameter exists in this collection
+		/// </summary>
+		/// <param name="ParameterName">Name of the Parameter being checked</param>
+		public bool Contains(string ParameterName) {
+			for(UInt16 i = 0 ; i < (UInt16)this.Count ; i++) {
+				if(this[i].Name == ParameterName) return true;
+			}
+			return false;
+		}
+
+		public string[] ParamNames {
+			get {
+				string[] names = new string[Count];
+				for(int i = 0 ; i < Count ; i++) names[i] = this[(UInt16)i].Name;
+				return names;
+			}
+		}
+
+		/// <summary>
+		/// Returns a Parameter given its name
+		/// </summary>
+		/// <param name="ParameterName">Name of the local variable being retrieved</param>
+		public Parameter this[string ParameterName] {
+			get {
+				for(UInt16 i = 0 ; i < (UInt16)this.Count ; i++) {
+					if(this[i].Name == ParameterName) return this[i];
+				}
+				throw new ArgumentException("The Parameter does not exist");
+			}
+		}
+
+		public override string ToString() {
+			string[] p = new string[Count];
+			for(int i = 0 ; i < Count ; i++) p[i] = this[(UInt16)i].ToString();
+			return p.CommaSeparatedList();
+		}
+	}
+}
