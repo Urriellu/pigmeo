9df8a699c982ccf893ce3c2275164c50d87f307b
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-27 20:40:45 +0000

    New Operand types: FieldAddrOperand, FieldValueOperand, FieldBitOperand

---
+++ trunk/pigmeo-compiler/src/PIR/FieldAddrOperand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Operand of a PIR Operation which is the address of a Field
+	/// </summary>
+	public class FieldAddrOperand:FieldOperand {
+		public FieldAddrOperand(Field ReferencedField)
+			: base(ReferencedField) {
+		}
+
+		public override string ToString() {
+			return string.Format("[FieldAddress]{0}", TheField.ToStringTypeAndName());
+		}
+	}
+}
