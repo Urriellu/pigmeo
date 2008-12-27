427fd6d0bad1e4fa6acb59323762e588027bb4ff
urriellu <pigmeo@urriellu.net>
2008-12-27 17:30:51 +0000

    Supporting some basic value types

---
+++ pigmeo-compiler/src/PIR/VT_Bool.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class VT_Bool:Struct {
+		protected VT_Bool(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers)
+			: base(ParentProgram, ReflectedType, IncludeMembers) {
+		}
+
+		public new static VT_Bool NewByArch(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers) {
+			return new VT_Bool(ParentProgram, ReflectedType, IncludeMembers);
+		}
+
+		public override uint Size {
+			get {
+				return ParentProgram.Target.PointerSize;
+			}
+		}
+	}
+}
