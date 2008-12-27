794b6dbe847480c97a0764b17fc65424313aa270
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-27 20:41:23 +0000

    FieldOperand is now FieldValueOperand. Supporting FieldBitOperand

--- trunk/pigmeo-compiler/src/PIR/Operand.cs
+++ trunk/pigmeo-compiler/src/PIR/Operand.cs
@@ -11,7 +11,8 @@ namespace Pigmeo.Compiler.PIR {
 
 		public static bool operator ==(Operand First, Operand Second) {
 			if(First is ConstantInt32Operand && Second is ConstantInt32Operand) return (First as ConstantInt32Operand).Value == (Second as ConstantInt32Operand).Value;
-			if(First is FieldOperand && Second is FieldOperand) return (First as FieldOperand).TheField == (Second as FieldOperand).TheField;
+			if(First is FieldValueOperand && Second is FieldValueOperand) return (First as FieldValueOperand).TheField == (Second as FieldValueOperand).TheField;
+			if(First is FieldBitOperand && Second is FieldBitOperand) return ((First as FieldBitOperand).TheField == (Second as FieldBitOperand).TheField && (First as FieldBitOperand).Bit == (Second as FieldBitOperand).Bit);
 			return Object.ReferenceEquals(First, Second);
 		}
 
