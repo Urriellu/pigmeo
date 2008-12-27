978c026dda08723cb07ba974804568a06c95c71f
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-27 20:45:52 +0000

    Supporting more CIL instructions

---
+++ trunk/pigmeo-framework/src/internal/Reflection/Instructions/ldsflda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Pushes the address of a static field on the stack
+		/// </summary>
+		public class ldsflda:FieldOperand {
+			public ldsflda(Method OriginalMethod, MCCil.Instruction OriginalInstruction)
+				: base(OriginalMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.ldsflda;
+			}
+		}
+	}
+}
---
+++ trunk/pigmeo-framework/src/internal/Reflection/Instructions/xor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Computes the bitwise XOR of the two integer values on top of stack and pushes the result on the stack
+		/// </summary>
+		public class xor:Instruction {
+			public xor(Method OriginalMethod, MCCil.Instruction OriginalInstruction)
+				: base(OriginalMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.xor;
+			}
+		}
+	}
+}
