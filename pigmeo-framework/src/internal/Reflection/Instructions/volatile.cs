f6113c40298465a894a7440e3f10072dcba095f2
urriellu <pigmeo@urriellu.net>
2008-12-27 20:45:52 +0000

    Supporting more CIL instructions

---
+++ pigmeo-framework/src/internal/Reflection/Instructions/volatile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Indicates that the following instruction operates on a volatile field
+		/// </summary>
+		public class Volatile:Instruction {
+			public Volatile(Method OriginalMethod, MCCil.Instruction OriginalInstruction)
+				: base(OriginalMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.Volatile;
+			}
+		}
+	}
+}
