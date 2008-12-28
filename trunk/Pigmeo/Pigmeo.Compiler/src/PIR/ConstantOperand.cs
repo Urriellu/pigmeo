6be831bc213dee3089b2e9f8bf561a27212c199e
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-28 18:51:53 +0000

    Reverting to 914

--- trunk/Pigmeo/Pigmeo.Compiler/src/PIR/ConstantOperand.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Pigmeo.Compiler.PIR {
-	/// <summary>
-	/// Operand of a PIR Operation which is a constant value
-	/// </summary>
-	public abstract class ConstantOperand:Operand {
-	}
-}
