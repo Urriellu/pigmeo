e9977f5e5cd5dcdddebf12feb08aa13b999cc720
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-29 21:23:46 +0000

    Reorganizing jumping operations

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/ConditionalJumping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Jumps to other operation based on a condition
+	/// </summary>
+	public abstract class ConditionalJumping:JumpingOperation {
+		protected ConditionalJumping(Method ParentMethod)
+			: base(ParentMethod) {
+		}
+	}
+}
