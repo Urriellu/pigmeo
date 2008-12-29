99db367169a8c309abb83558bc180a7af9411a5c
urriellu <pigmeo@urriellu.net>
2008-12-29 21:24:21 +0000

    New enum: Condition

---
+++ Pigmeo/Pigmeo.Compiler/PIR/Condition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Pigmeo.Compiler.PIR {
+	public enum Condition {
+		[Description("==")]
+		Equal,
+		[Description("!=")]
+		NotEqual,
+		[Description("<")]
+		LessThan,
+		[Description(">")]
+		GreaterThan,
+		[Description("<=")]
+		LessThanOrEqual,
+		[Description(">=")]
+		GreaterThanOrEqual
+	}
+
+	public static class ConditionExtensions {
+		public static string ToSymbolString(this Condition TheCondition) {
+			DescriptionAttribute[] attributes = (DescriptionAttribute[])TheCondition.GetType().GetField(TheCondition.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+			if(attributes.Length == 0) throw new NotImplementedException("Undefined symbol");
+			return attributes[0].Description;
+		}
+	}
+}
