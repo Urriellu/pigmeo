70f77cf3cd2cbd094387914a343d33db0ed074d7
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-30 20:03:19 +0000

    Supporting more PIR Operations

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/OR.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class OR:ArithmeticOperation {
+		public OR(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "OR";
+			Arguments = new Operand[2];
+			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
+		}
+
+		public OR(Method ParentMethod, Operand Result, Operand FirstOperand, Operand SecondOperand)
+			: this(ParentMethod) {
+			this.Result = Result;
+			Arguments[0] = FirstOperand;
+			Arguments[1] = SecondOperand;
+		}
+	}
+}
---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/ShiftLeft.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class ShiftLeft:ArithmeticOperation {
+		public ShiftLeft(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "<<";
+			Arguments = new Operand[2];
+			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
+		}
+
+		public ShiftLeft(Method ParentMethod, Operand Result, Operand FirstOperand, Operand SecondOperand)
+			: this(ParentMethod) {
+			this.Result = Result;
+			Arguments[0] = FirstOperand;
+			Arguments[1] = SecondOperand;
+		}
+	}
+}
