7b372173838ac5f0c6b4ff1be9fa2d90b226fe03
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-30 21:05:04 +0000

    New: ParameterBitOperand

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/ParameterBitOperand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Operand of a PIR Operation which is the bit of a Parameter
+	/// </summary>
+	public class ParameterBitOperand:ParameterOperand {
+		public byte Bit;
+
+		public ParameterBitOperand(Parameter ReferencedParameter, byte Bit)
+			: base(ReferencedParameter) {
+			this.Bit = Bit;
+		}
+
+		public override string ToString() {
+			return string.Format("[ParameterBit]{0}<bit {1}>", TheParameter.ToString(), Bit);
+		}
+	}
+}
