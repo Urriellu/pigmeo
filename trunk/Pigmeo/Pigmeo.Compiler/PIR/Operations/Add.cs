2f02d0bccfd367f5f8d2b772bb641c1505044218
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2008-12-30 20:01:56 +0000

    Reorganizing arithmetic operations

--- trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Add.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Add.cs
@@ -2,7 +2,7 @@
 using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
-	public class Add:Operation {
+	public class Add:ArithmeticOperation {
 		public Add(Method ParentMethod)
 			: base(ParentMethod) {
 			Operator = "+";
@@ -15,10 +15,6 @@ namespace Pigmeo.Compiler.PIR {
 			this.Result = Result;
 			Arguments[0] = FirstSummand;
 			Arguments[1] = SecondSummand;
-		}
-
-		public override string ToString() {
-			return Label + ": " + Result + " " + AssignmentSign + " " + Arguments[0] + " " + Operator + " " + Arguments[1];
 		}
 	}
 }
---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/ArithmeticOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public abstract class ArithmeticOperation:Operation {
+		protected ArithmeticOperation(Method ParentMethod)
+			: base(ParentMethod) {
+		}
+
+		public override string ToString() {
+			string ret = Label + ": " + Result + " " + AssignmentSign + " ";
+			for(int i = 0 ; i < Arguments.Length ; i++) {
+				ret += Arguments[0];
+				if(i != Arguments.Length - 1) ret += " " + Operator + " ";
+			}
+			return ret;
+		}
+	}
+}
