156f4aac59df78546a89b1d08a3d9f624966bd2b
urriellu <pigmeo@urriellu.net>
2009-01-02 16:43:59 +0000

    More Operand types

---
+++ Pigmeo/Pigmeo.Compiler/PIR/ParameterAddrOperand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Operand of a PIR Operation which is the address of a Parameter
+	/// </summary>
+	public class ParameterAddrOperand:ParameterOperand {
+		public ParameterAddrOperand(Parameter ReferencedParam)
+			: base(ReferencedParam) {
+		}
+
+		public override string ToString() {
+			return string.Format("[ParameterAddress]{0}", TheParameter.ToString());
+		}
+	}
+}
