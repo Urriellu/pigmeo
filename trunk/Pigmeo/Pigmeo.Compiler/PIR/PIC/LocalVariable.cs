831e807cc6dceb8f949d51ea1f6a2b1e6cb969a7
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-01-02 16:50:47 +0000

    Instantiating LocalVariables properly

--- trunk/Pigmeo/Pigmeo.Compiler/PIR/PIC/LocalVariable.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/PIC/LocalVariable.cs
@@ -6,5 +6,12 @@ using Pigmeo.Compiler.UI;
 
 namespace Pigmeo.Compiler.PIR.PIC {
 	public class LocalVariable:PIR.LocalVariable {
+		public LocalVariable(PIR.Method ParentMethod, PRefl.LocalVariable RefldLocalVar)
+			: base(ParentMethod, RefldLocalVar) {
+		}
+
+		public LocalVariable(PIR.Method ParentMethod, PIR.Type LocalVarType, string Name)
+			: base(ParentMethod, LocalVarType, Name) {
+		}
 	}
 }
