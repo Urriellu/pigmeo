d5d8a238ff2b8f902e9c85de6e8d04414c199657
urriellu <pigmeo@urriellu.net>
2009-01-02 19:57:27 +0000

    Supporting Jumps not derived from Pigmeo Reflection

--- Pigmeo/Pigmeo.Compiler/PIR/Operations/Jump.cs
+++ Pigmeo/Pigmeo.Compiler/PIR/Operations/Jump.cs
@@ -1,21 +1,27 @@
 ﻿using System;
-using PRefl=Pigmeo.Internal.Reflection;
+using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
 	public class Jump:JumpingOperation {
-		protected Jump(Method ParentMethod):base(ParentMethod) {
+		protected Jump(Method ParentMethod)
+			: base(ParentMethod) {
 			Operator = "Jump";
+			Arguments = new Operand[1];
 		}
 
-		public Jump(Method ParentMethod, PRefl.Instructions.br_s OrigCilInstr):this(ParentMethod) {
-			Arguments = new Operand[1];
+		public Jump(Method ParentMethod, PRefl.Instructions.br_s OrigCilInstr)
+			: this(ParentMethod) {
 			Arguments[0] = new OperationOperand(ParentMethod, OrigCilInstr.RefdInstr.Index);
 		}
 
 		public Jump(Method ParentMethod, PRefl.Instructions.br OrigCilInstr)
 			: this(ParentMethod) {
-			Arguments = new Operand[1];
 			Arguments[0] = new OperationOperand(ParentMethod, OrigCilInstr.RefdInstr.Index);
+		}
+
+		public Jump(Method ParendMethod, int Index)
+			: this(ParendMethod) {
+			Arguments[0] = new OperationOperand(ParendMethod, Index);
 		}
 	}
 }
