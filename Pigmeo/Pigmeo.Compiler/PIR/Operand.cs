20f21d485235f32c144571225ce7b4a1d507c206
urriellu <pigmeo@urriellu.net>
2009-01-04 12:39:18 +0000

    Supporting more types of operands

--- Pigmeo/Pigmeo.Compiler/PIR/Operand.cs
+++ Pigmeo/Pigmeo.Compiler/PIR/Operand.cs
@@ -13,6 +13,13 @@ namespace Pigmeo.Compiler.PIR {
 			if(First is ConstantInt32Operand && Second is ConstantInt32Operand) return (First as ConstantInt32Operand).Value == (Second as ConstantInt32Operand).Value;
 			if(First is FieldValueOperand && Second is FieldValueOperand) return (First as FieldValueOperand).TheField == (Second as FieldValueOperand).TheField;
 			if(First is FieldBitOperand && Second is FieldBitOperand) return ((First as FieldBitOperand).TheField == (Second as FieldBitOperand).TheField && (First as FieldBitOperand).Bit == (Second as FieldBitOperand).Bit);
+			if(First is FieldAddrOperand && Second is FieldAddrOperand) return (First as FieldAddrOperand).TheField == (Second as FieldAddrOperand).TheField;
+			if(First is ParameterValueOperand && Second is ParameterValueOperand) return (First as ParameterValueOperand).TheParameter == (Second as ParameterValueOperand).TheParameter;
+			if(First is ParameterBitOperand && Second is ParameterBitOperand) return ((First as ParameterBitOperand).TheParameter == (Second as ParameterBitOperand).TheParameter && (First as ParameterBitOperand).Bit == (Second as ParameterBitOperand).Bit);
+			if(First is ParameterAddrOperand && Second is ParameterAddrOperand) return (First as ParameterAddrOperand).TheParameter == (Second as ParameterAddrOperand).TheParameter;
+			if(First is LocalVariableValueOperand && Second is LocalVariableValueOperand) return (First as LocalVariableValueOperand).TheLV == (Second as LocalVariableValueOperand).TheLV;
+			if(First is LocalVariableBitOperand && Second is LocalVariableBitOperand) return ((First as LocalVariableBitOperand).TheLV == (Second as LocalVariableBitOperand).TheLV && (First as LocalVariableBitOperand).Bit == (Second as LocalVariableBitOperand).Bit);
+			if(First is LocalVariableAddrOperand && Second is LocalVariableAddrOperand) return (First as LocalVariableAddrOperand).TheLV == (Second as LocalVariableAddrOperand).TheLV;
 			return Object.ReferenceEquals(First, Second);
 		}
 
