27e6a8d057d8b006903c2af74facfc62c76c95fc
urriellu <pigmeo@urriellu.net>
2009-01-04 14:49:26 +0000

    Enums moved to independent files

---
+++ Pigmeo/Pigmeo.Compiler/BackendPIC/OpCode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Compiler.BackendPIC {
+	/// <summary>
+	/// Available instruction names, such as ADDWF, ANDWF, NOP...
+	/// </summary>
+	public enum OpCode:byte {
+		ADDWF,
+		ANDWF,
+		BANKSEL,
+		CLRF,
+		CLRW,
+		COMF,
+		DECF,
+		DECFSZ,
+		INCF,
+		INCFSZ,
+		IORWF,
+		MOVF,
+		MOVWF,
+		NOP,
+		RLF,
+		RRF,
+		SUBWF,
+		SWAPF,
+		XORWF,
+		BCF,
+		BSF,
+		BTFSC,
+		BTFSS,
+		ADDLW,
+		ANDLW,
+		CALL,
+		CLRWDT,
+		GOTO,
+		IORLW,
+		MOVLW,
+		RETFIE,
+		RETLW,
+		RETURN,
+		SLEEP,
+		SUBLW,
+		XORLW
+	}
+}
