c97c6a7a14c4dc32f908a5b93fcdc1046f495efc
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-04-20 17:53:07 +0000

    Avoid compiling using VS

--- trunk/Pigmeo/Pigmeo.Framework/AssemblyInfo.cs
+++ trunk/Pigmeo/Pigmeo.Framework/AssemblyInfo.cs
@@ -1,3 +1,9 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyKeyFile("pigmeo.key")]
+
+#if VisualStudio
+	Pigmeo cannot be compiled using Visual Studio.
+	Use NAnt instead.
+	See README.txt for more information
+#endif
