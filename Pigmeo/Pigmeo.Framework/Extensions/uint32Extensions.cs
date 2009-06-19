db6e62d140f8d697bad349af58fd6ef56dd2b5ac
urriellu <pigmeo@urriellu.net>
2009-06-19 13:55:14 +0000

    New set of extension methods for UInt32

---
+++ Pigmeo/Pigmeo.Framework/Extensions/uint32Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pigmeo.Extensions {
+	public static class uint32Extensions {
+		/// <summary>
+		/// Gets the four bytes that represent this UInt16
+		/// </summary>
+		public static byte[] GetBytes(this UInt32 n) {
+			byte[] bytes = new byte[4];
+			bytes[3] = (byte)(n >> 24);
+			bytes[2] = (byte)(n >> 16);
+			bytes[1] = (byte)(n >> 8);
+			bytes[0] = (byte)n;
+			return bytes;
+		}
+	}
+}
