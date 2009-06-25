3fc0dfdf9b651a4746b758b5bb79322d58baa8b8
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-06-25 18:55:04 +0000

    Supporting one type of internal oscillator on PICs

---
+++ trunk/Pigmeo/Pigmeo.Devices/Shared/PIC/IntOsc_62.5_125_250_500_2_4_8_16.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Pigmeo.MCU {
+	public enum IntOscFreq {
+		/// <summary>
+		/// 62.5kHz. PLL must be disabled
+		/// </summary>
+		_62_5kHz,
+		_125kHz,
+		_250kHz,
+		_500kHz,
+		_2MHz,
+		_4MHz,
+		_8MHz,
+		_16MHz
+	}
+
+	/// <summary>
+	/// Internall Oscillator
+	/// </summary>
+	public static class IntOsc {
+		/// <summary>
+		/// Set the internal oscillator to the specified frequency.
+		/// </summary>
+		/// <param name="Freq"></param>
+		public static void Setup(IntOscFreq Freq) {
+			if (ConfigBits.PLLEnable) {
+				switch (Freq) {
+					case IntOscFreq._16MHz:
+						Registers.OSCCON.IRCF1 = true;
+						Registers.OSCCON.IRCF0 = true;
+						break;
+					case IntOscFreq._8MHz:
+						Registers.OSCCON.IRCF1 = true;
+						Registers.OSCCON.IRCF0 = false;
+						break;
+					case IntOscFreq._4MHz:
+						Registers.OSCCON.IRCF1 = false;
+						Registers.OSCCON.IRCF0 = true;
+						break;
+					case IntOscFreq._2MHz:
+						Registers.OSCCON.IRCF1 = false;
+						Registers.OSCCON.IRCF0 = false;
+						break;
+					default:
+						throw new Exception("PLL must be disabled in the config bits for using the lower frequencies of the internal oscillator");
+				}
+			} else {
+				switch (Freq) {
+					case IntOscFreq._500kHz:
+						Registers.OSCCON.IRCF1 = true;
+						Registers.OSCCON.IRCF0 = true;
+						break;
+					case IntOscFreq._250kHz:
+						Registers.OSCCON.IRCF1 = true;
+						Registers.OSCCON.IRCF0 = false;
+						break;
+					case IntOscFreq._125kHz:
+						Registers.OSCCON.IRCF1 = false;
+						Registers.OSCCON.IRCF0 = true;
+						break;
+					case IntOscFreq._62_5kHz:
+						Registers.OSCCON.IRCF1 = false;
+						Registers.OSCCON.IRCF0 = false;
+						break;
+					default:
+						throw new Exception("PLL must be enabled in the config bits for using the highest frequencies of the internal oscillator");
+				}
+			}
+		}
+	}
+}
