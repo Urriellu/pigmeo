4f731bb76a6e8f2ed3689fc6c3a00e1322807beb
urriellu <pigmeo@urriellu.net>
2009-06-25 18:56:24 +0000

    Partial PIC16F722 support

---
+++ Pigmeo/Pigmeo.Devices/PIC16F722.cs
@@ -0,0 +1,42 @@
+﻿using Pigmeo;
+using Pigmeo.Internal;
+using Pigmeo.Internal.PIC;
+
+[assembly: DeviceLibrary(Architecture.PIC, Family.PIC14, Branch.PIC16F722)]
+
+namespace Pigmeo.MCU {
+	/// <summary>
+	/// Constains all the information about the PIC
+	/// </summary>
+	public static class Info {
+		public static InfoPIC GetInfo() {
+			InfoPIC device = new InfoPIC();
+
+			device.Architecture = Architecture.PIC;
+			device.Family = Family.PIC14;
+			device.Branch = Branch.PIC16F722;
+			/*device.DataMemory = new DataMemoryBank[2];
+			device.DataMemory[0].FirstSFR = 0x00;
+			device.DataMemory[0].LastSFR = 0x1F;
+			device.DataMemory[0].FirstGPR = 0x20;
+			device.DataMemory[0].LastGPR = 0x7F;
+			device.DataMemory[1].FirstSFR = 0x00;
+			device.DataMemory[1].LastSFR = 0x1F;
+			device.DataMemory[1].FirstGPR = 0x20;
+			device.DataMemory[1].LastGPR = 0x3F;
+			device.MaxWords = 2048;
+			device.IncludeFile = "p16f722.inc";*/
+
+			return device;
+		}
+	}
+
+	public static class Ports {
+		public static PORTA A = new PORTA();
+		public static PORTB B = new PORTB();
+	}
+
+	public static class Timers {
+		public static TMR0 Timer0 = new TMR0();
+	}
+}
