571af10ff5f79ef74d7324104cdecbfaedbfc175
urriellu <pigmeo@urriellu.net>
2009-07-02 17:03:19 +0000

    Implementing PIC16F722.dll properly

--- Pigmeo/Pigmeo.Devices/PIC16F722.cs
+++ Pigmeo/Pigmeo.Devices/PIC16F722.cs
@@ -15,7 +15,7 @@ namespace Pigmeo.MCU {
 			device.Architecture = Architecture.PIC;
 			device.Family = Family.PIC14;
 			device.Branch = Branch.PIC16F722;
-			/*device.DataMemory = new DataMemoryBank[2];
+			device.DataMemory = new DataMemoryBank[4];
 			device.DataMemory[0].FirstSFR = 0x00;
 			device.DataMemory[0].LastSFR = 0x1F;
 			device.DataMemory[0].FirstGPR = 0x20;
@@ -24,8 +24,14 @@ namespace Pigmeo.MCU {
 			device.DataMemory[1].LastSFR = 0x1F;
 			device.DataMemory[1].FirstGPR = 0x20;
 			device.DataMemory[1].LastGPR = 0x3F;
+			device.DataMemory[2].FirstSFR = 0x00;
+			device.DataMemory[2].LastSFR = 0x1F;
+			device.DataMemory[3].FirstSFR = 0x00;
+			device.DataMemory[3].LastSFR = 0x1F;
 			device.MaxWords = 2048;
-			device.IncludeFile = "p16f722.inc";*/
+			device.IncludeFile = "p16f722.inc";
+
+			device.ConfigWords = 2;
 
 			return device;
 		}
