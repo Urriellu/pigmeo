08881fc70d4e6529e68e2aba67ebef3f3a150203
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-05 11:19:16 +0000

    New operand: ConstantStringOperand

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/ConstantStringOperand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Operand of a PIR Operation which is a constant string
+	/// </summary>
+	public class ConstantStringOperand:ConstantOperand {
+		public readonly string Value;
+
+		public ConstantStringOperand(string Value) {
+			this.Value = Value;
+		}
+
+		public override string ToString() {
+			return string.Format("[ConstantString]{0}", Value);
+		}
+	}
+}
