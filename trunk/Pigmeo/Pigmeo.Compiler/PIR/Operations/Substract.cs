5bf957bf240df4ce5e84c4f0becbc191a71e4a3f
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-05 11:23:28 +0000

    New operation: Substract

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Substract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Substract:ArithmeticOperation {
+		public Substract(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "-";
+			Arguments = new Operand[2];
+			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
+		}
+
+		public Substract(Method ParentMethod, Operand Result, Operand Minuend, Operand Subtrahend)
+			: this(ParentMethod) {
+			this.Result = Result;
+			Arguments[0] = Minuend;
+			Arguments[1] = Subtrahend;
+		}
+	}
+}
