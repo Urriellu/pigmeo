3c6ec03753a29d18c946aa4371f440fd2072799e
urriellu <pigmeo@urriellu.net>
2009-07-05 11:22:06 +0000

    New operation: ShiftRight

---
+++ Pigmeo/Pigmeo.Compiler/PIR/Operations/ShiftRight.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class ShiftRight:ArithmeticOperation {
+		public ShiftRight(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = ">>";
+			Arguments = new Operand[2];
+			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
+		}
+
+		public ShiftRight(Method ParentMethod, Operand Result, Operand FirstOperand, Operand SecondOperand)
+			: this(ParentMethod) {
+			this.Result = Result;
+			Arguments[0] = FirstOperand;
+			Arguments[1] = SecondOperand;
+		}
+	}
+}
