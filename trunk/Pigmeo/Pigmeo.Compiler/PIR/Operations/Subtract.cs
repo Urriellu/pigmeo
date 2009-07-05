6b832eb1927057753182b699d51d9ff9851f3ff1
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-05 11:27:31 +0000

    Substract renamed to Subtract

--- trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Subtract.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Subtract.cs
@@ -2,15 +2,15 @@
 using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
-	public class Substract:ArithmeticOperation {
-		public Substract(Method ParentMethod)
+	public class Subtract:ArithmeticOperation {
+		public Subtract(Method ParentMethod)
 			: base(ParentMethod) {
 			Operator = "-";
 			Arguments = new Operand[2];
 			Result = Arguments[0] = Arguments[1] = GlobalOperands.TOSS;
 		}
 
-		public Substract(Method ParentMethod, Operand Result, Operand Minuend, Operand Subtrahend)
+		public Subtract(Method ParentMethod, Operand Result, Operand Minuend, Operand Subtrahend)
 			: this(ParentMethod) {
 			this.Result = Result;
 			Arguments[0] = Minuend;
