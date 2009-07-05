33fc639458a561b704604e0293cf98f93cab393b
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-05 11:25:07 +0000

    New operation: Switch

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Switch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// 
+	/// </summary>
+	public class Switch:Operation {
+		public Operand ComparedOperand {
+			get {
+				return Arguments[0];
+			}
+			set {
+				Arguments[0] = value;
+			}
+		}
+
+		public OperationOperand[] OperationJumps;
+
+		protected Switch(Method ParentMethod)
+			: base(ParentMethod) {
+			Arguments = new Operand[1];
+			ComparedOperand = GlobalOperands.TOSS;
+		}
+
+		public Switch(Method ParentMethod, PRefl.Instructions.Switch OrigCilInstr)
+			: this(ParentMethod) {
+			OperationJumps = new OperationOperand[OrigCilInstr.RefdInstrs.Length];
+			for(int i = 0; i < OperationJumps.Length; i++) {
+				OperationJumps[i] = new OperationOperand(ParentMethod, OrigCilInstr.RefdInstrs[i].Index);
+			}
+		}
+
+		public override string ToString() {
+			string ret = Label + ": switch(" + ComparedOperand + ") {\n";
+			for(int i = 0; i < OperationJumps.Length; i++) {
+				ret += "\tcase " + i + ":";
+				ret += "\t\tJump to " + OperationJumps[i];
+			}
+			return ret;
+		}
+	}
+}
