011a22a09902de9b649b1416eded2f132bf53155
urriellu <pigmeo@urriellu.net>
2009-07-05 11:34:19 +0000

    New reflection instruction: bge

---
+++ Pigmeo/Pigmeo.Framework/Internal/Reflection/Instructions/bge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Branch on greater than or equal
+		/// </summary>
+		public class bge:InstructionOperand {
+			public bge(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.bge;
+			}
+		}
+	}
+}
