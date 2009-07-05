682fec93606b9bbc2cdcd1f13718d86f410a2d30
urriellu <pigmeo@urriellu.net>
2009-07-05 11:31:13 +0000

    New reflection instruction: brtrue

---
+++ Pigmeo/Pigmeo.Framework/Internal/Reflection/Instructions/brtrue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Mono.Cecil;
+using MCCil = Mono.Cecil.Cil;
+
+namespace Pigmeo.Internal.Reflection {
+	public static partial class Instructions {
+		/// <summary>
+		/// Branch on true (non-zero)
+		/// </summary>
+		public class brtrue:InstructionOperand {
+			public brtrue(Method ParentMethod, MCCil.Instruction OriginalInstruction)
+				: base(ParentMethod, OriginalInstruction) {
+				this.OpCode = OpCodes.brtrue;
+			}
+		}
+	}
+}
