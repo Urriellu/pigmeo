d08bec331cfa07349fa8d690c050606bd4cc4e8d
urriellu <pigmeo@urriellu.net>
2009-07-05 11:25:38 +0000

    New operation: Throw

---
+++ Pigmeo/Pigmeo.Compiler/PIR/Operations/Throw.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using PRefl = Pigmeo.Internal.Reflection;
+using Pigmeo.Compiler.UI;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Throw:Operation {
+		public Throw(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "Throw";
+			Arguments = new Operand[] { GlobalOperands.TOSS };
+		}
+	}
+}
