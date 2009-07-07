a235b827e033faa0b10f003921dca289bfd63a20
urriellu <pigmeo@urriellu.net>
2009-07-07 17:56:37 +0000

    New PIR Operation: CallVirtual

---
+++ Pigmeo/Pigmeo.Compiler/PIR/Operations/CallVirtual.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using PRefl = Pigmeo.Internal.Reflection;
+using Pigmeo.Compiler.UI;
+
+namespace Pigmeo.Compiler.PIR {
+	/// <summary>
+	/// Calls a method based on the run-time type of an object
+	/// </summary>
+	public class CallVirtual:Call {
+		protected CallVirtual(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "CallVirtual";
+		}
+
+		public CallVirtual(Method ParentMethod, PRefl.Instructions.callvirt OrigCilInstr)
+			: this(ParentMethod) {
+			Arguments = new Operand[OrigCilInstr.ReferencedMethod.Parameters.Count + 1];
+			Arguments[0] = new MethodOperand(ParentProgram.Types[OrigCilInstr.ReferencedMethod.ParentType.FullName].Methods.GetFromPRefl(OrigCilInstr.ReferencedMethod));
+			for(int i = 0 ; i < OrigCilInstr.ReferencedMethod.Parameters.Count ; i++) Arguments[i + 1] = GlobalOperands.TOSS;
+			if(OrigCilInstr.ReferencedMethod.ReturnType.FullName != "System.Void") Result = GlobalOperands.TOSS;
+		}
+
+		/// <summary>
+		/// The constructor called
+		/// </summary>
+		public Method CalledMethod {
+			get {
+				return ((MethodOperand)Arguments[0]).TheMethod;
+			}
+		}
+	}
+}
