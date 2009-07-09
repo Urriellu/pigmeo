6c210e1250d6ba0c32d5542fc598cd2b007fd995
urriellu <pigmeo@urriellu.net>
2009-07-09 21:03:43 +0000

    InLine didn't work. Now almost works :-S

--- Pigmeo/Pigmeo.Compiler/PIR/Operations/Call.cs
+++ Pigmeo/Pigmeo.Compiler/PIR/Operations/Call.cs
@@ -29,8 +29,7 @@ namespace Pigmeo.Compiler.PIR {
 
 
 		public void InLine() {
-			ShowInfo.InfoDebug("Inlining Method Call: " + this);
-			ShowInfo.InfoDebugDecompile("InLine Method before being inlined", CalledMethod);
+			ShowInfo.InfoDebug("Inlining Method Call: " + this + "...");
 
 			//correspondence between local variables and parameters of the called method and the new local variables created in the caller method
 			Dictionary<LocalVariable, LocalVariable> LVRelation = new Dictionary<LocalVariable, LocalVariable>();
@@ -38,6 +37,12 @@ namespace Pigmeo.Compiler.PIR {
 
 			Method Caller = ParentMethod;
 			Call CallOperation = this;
+
+			ShowInfo.InfoDebugDecompile("Method with an inline call before inlining it", Caller);
+			ShowInfo.InfoDebugDecompile("InLine Method before being inlined", CalledMethod);
+
+			int CallOptnOriginalIndex = CallOperation.Index;
+			int ExtraOperations = 0;
 
 			#region creating local variables (in the caller method) for each parameter of the called method
 			foreach(Parameter param in CalledMethod.Parameters.Values) {
@@ -57,23 +62,26 @@ namespace Pigmeo.Compiler.PIR {
 			}
 			#endregion
 
-			#region in the called method, replacing each reference to parameters and local variables by references to the new local variables in the caller method
+			ShowInfo.InfoDebug("There are {0} local variables and {1} parameters moved from the callee to the caller method", LVRelation.Count, ParamRelation.Count);
+
+			/*#region in the called method, replacing each reference to parameters and local variables by references to the new local variables in the caller method
 			foreach(Parameter P in ParamRelation.Keys) {
 				CalledMethod.ReplaceRef(P, ParamRelation[P]);
 			}
 			foreach(LocalVariable LV in LVRelation.Keys) {
 				CalledMethod.ReplaceRef(LV, LVRelation[LV]);
 			}
-			#endregion
+			#endregion*/
 
 			#region operands supposed to be passed as arguments are now copied to the new local variables in the caller method
 			for(int i = 1; i < CallOperation.Arguments.Length; i++) {
 				Caller.Operations.InsertBefore(CallOperation, new Copy(Caller, CallOperation.Arguments[i], new LocalVariableValueOperand(ParamRelation[CalledMethod.Parameters[(ushort)(i - 1)]])));
+				ExtraOperations++;
 			}
 			#endregion
 
-			#region moving operations from the called method to the caller method
-			Operation PreviousOp = CallOperation; //operations must be places AFTER the CallOperation so when CallOperation is removed, the first operation of the inlined method will get the old index the CallOperation had (so jumps to the CallOperation won't break)
+			#region copying operations from the called method to the caller method
+			Operation PreviousOp = CallOperation; //operations must be placed AFTER the CallOperation so when CallOperation is removed, the first operation of the inlined method will get the old index the CallOperation had (so jumps to the CallOperation won't break)
 			for(int i = 0; i < CalledMethod.Operations.Count; i++) {
 				Operation MovingOp;
 
@@ -84,14 +92,16 @@ namespace Pigmeo.Compiler.PIR {
 							MovingOp = new Jump(Caller, CallOperation.Index);
 						} else {
 							//if this is a "return" from a method that returns nothing and it's the last operation, do nothing
-							MovingOp = null;
+							//MovingOp = null;
+							continue;
 						}
 					} else {
 						MovingOp = null;
 						ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", true, "Inlining a method that returns a value");
 					}
 				} else {
-					MovingOp = CalledMethod.Operations[i];
+					MovingOp = CalledMethod.Operations[i].CloneOperation();
+					Console.WriteLine("Same operand?" + object.ReferenceEquals(CalledMethod.Operations[i].Result, MovingOp.Result));
 				}
 
 				MovingOp.ParentMethod = Caller;
@@ -104,7 +114,34 @@ namespace Pigmeo.Compiler.PIR {
 			Caller.Operations.Remove(CallOperation);
 			#endregion
 
+			#region Update references to parameters, local variables and operations
+			foreach(Parameter P in ParamRelation.Keys) {
+				Caller.ReplaceRef(P, ParamRelation[P]);
+			}
+			foreach(LocalVariable LV in LVRelation.Keys) {
+				Caller.ReplaceRef(LV, LVRelation[LV]);
+			}
+
+			//Caller.UpdateIndices(CallOptnOriginalIndex, CalledMethod.Operations.Count, OptnsOffset);
+
+			for(int i = CallOptnOriginalIndex + ExtraOperations; i < CallOptnOriginalIndex + ExtraOperations + CalledMethod.Operations.Count; i++) {
+				Operation CurrOptn = Caller.Operations[i];
+				if(CurrOptn.Arguments != null) {
+					for(int j = 0; j < CurrOptn.Arguments.Length; j++) {
+						Console.WriteLine("a");
+						Console.WriteLine(CurrOptn);
+						Console.WriteLine(CalledMethod.Operations[i - CallOptnOriginalIndex - ExtraOperations]);
+						if(CurrOptn.Arguments[j] is OperationOperand) {
+							(CurrOptn.Arguments[j] as OperationOperand).OperationIndex = (CalledMethod.Operations[i - CallOptnOriginalIndex - ExtraOperations].Arguments[j] as OperationOperand).OperationIndex + CallOptnOriginalIndex + ExtraOperations;
+						}
+					}
+				}
+			}
+			#endregion
+
 			ShowInfo.InfoDebugDecompile("Caller method after inlining the called one", ParentMethod);
+			ShowInfo.InfoDebugDecompile("InLine Method after being inlined (should be the same as before)", CalledMethod);
+			Environment.Exit(1);
 		}
 
 		public override string ToString() {
