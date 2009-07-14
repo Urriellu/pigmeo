0b68c0ae43ff1ff2947b401d854f808dd10b5272
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-14 17:53:46 +0000

    AvoidTOSS() called multiple times while optimizing PIR

--- trunk/Pigmeo/Pigmeo.Compiler/Frontend.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/Frontend.cs
@@ -25,8 +25,10 @@ namespace Pigmeo.Compiler {
 			Program PlainProgram = Program.GetFromCIL(ReflectedAssembly);
 			ShowInfo.InfoVerbose(i18n.str("CompilingApp", PlainProgram.Name, PlainProgram.Target.Architecture, PlainProgram.Target.Family, PlainProgram.Target.Branch));
 			ShowInfo.InfoDebugDecompile("Original assembly converted to PIR", PlainProgram);
+			File.WriteAllText(PlainProgram.Name + "-disassembled-beforeopt.pir", PlainProgram.ToString());
 			Program OptimizedProgram = OptimizeProgram(PlainProgram);
 			ShowInfo.InfoDebugDecompile("PIR optimized", OptimizedProgram);
+			File.WriteAllText(OptimizedProgram.Name + "-disassembled-optimized.pir", OptimizedProgram.ToString());
 
 			ShowInfo.InfoVerbose("The final program contains {0} types ({1} classes, {2} structs), {3} methods", OptimizedProgram.Types.Count, OptimizedProgram.ClassCount, OptimizedProgram.StructCount, OptimizedProgram.MethodCount);
 
@@ -43,13 +45,14 @@ namespace Pigmeo.Compiler {
 			Program OptimizedProg = OriginalProgram;
 
 			#region optimizations that don't have influence on other optimizations and must be executed at the beginning
-			OptimizedProg.AvoidTOSS();
+			//OptimizedProg.AvoidTOSS();
 			#endregion
 
 			#region optimizations that DO have influence on other optimizations
 			bool KeepOptimizing = true;
 			while(KeepOptimizing) {
 				KeepOptimizing = false;
+				if(OptimizedProg.AvoidTOSS()) KeepOptimizing = true;
 				OptimizedProg.FindSingleCallInlinizable();
 				OptimizedProg.FindShortInlinizableMethods();
 				if(OptimizedProg.InLineAll()) KeepOptimizing = true;
