4a86b3e8cdb7dd39f845d4cb88bb72601cb3e184
urriellu <pigmeo@urriellu.net>
2009-07-14 19:01:28 +0000

    PIR.Class.ToString() moved to PIR.Type.ToString()

--- Pigmeo/Pigmeo.Compiler/PIR/Type.cs
+++ Pigmeo/Pigmeo.Compiler/PIR/Type.cs
@@ -36,5 +36,34 @@ namespace Pigmeo.Compiler.PIR {
 		}
 
 		public abstract UInt32 Size { get; }
+
+		public override string ToString() {
+			string Output = "";
+			if(IsPublic) Output += "public ";
+			if(this is Enum) Output += "enum ";
+			else if(this is Struct) Output += "struct ";
+			else if(this is Class) {
+				Output += "class ";
+				if((this as Class).IsAbstract) Output += "abstract ";
+				if((this as Class).IsSealed) Output += "sealed ";
+			} else ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0003", true);
+			Output += Name;
+			if(BaseType == null) Output += ":WithoutBaseType";
+			else Output += ":" + BaseType.Name;
+			Output += " {\n";
+			foreach(Field f in Fields) {
+				foreach(string line in f.ToString().Split('\n')) {
+					Output += "\t" + line + "\n";
+				}
+			}
+			Output += "\n";
+			foreach(Method m in Methods) {
+				foreach(string line in m.ToString().Split('\n')) {
+					Output += "\t" + line + "\n";
+				}
+			}
+			Output += "}\n";
+			return Output;
+		}
 	}
 }
