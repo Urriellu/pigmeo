56d7da1f877bc1cff996a4732647e6ef4ee5311e
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-19 19:03:18 +0000

    New PIR Operation: Conversion

---
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/Conversion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Pigmeo.Internal;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.PIR {
+	public class Conversion:Operation {
+		public BaseType TargetType;
+
+		protected Conversion(Method ParentMethod)
+			: base(ParentMethod) {
+			Operator = "Convert";
+			Arguments = new Operand[1];
+		}
+
+		public Conversion(Method ParentMethod, PRefl.Instructions.conv OrigCilInstr)
+			: this(ParentMethod) {
+			Arguments[0] = GlobalOperands.TOSS;
+			Result = GlobalOperands.TOSS;
+			TargetType = OrigCilInstr.TargetType;
+		}
+
+		public override string ToString() {
+			return Label + ": " + Result + " " + AssignmentSign + " Convert " + Arguments[0] + " to " + TargetType.ToString();
+		}
+
+		public override Type ResultType {
+			get {
+				return ParentProgram.Types[TargetType.GetBclName()];
+			}
+			set {
+				base.ResultType = value;
+			}
+		}
+	}
+}
