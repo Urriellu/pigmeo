3e2adf7e879bdb89f754229714839229c3addac4
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-19 19:26:25 +0000

    .NET native types now derive from VT_BaseType

--- trunk/Pigmeo/Pigmeo.Compiler/PIR/VT_UInt8.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/VT_UInt8.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using PRefl = Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler.PIR {
-	public class VT_UInt8:Struct {
+	public class VT_UInt8:VT_BaseType {
 		protected VT_UInt8(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers)
-			: base(ParentProgram, ReflectedType, IncludeMembers) {
+			: base(ParentProgram, ReflectedType, IncludeMembers, Internal.BaseType.UInt8) {
 		}
 
 		public new static VT_UInt8 NewByArch(Program ParentProgram, PRefl.Type ReflectedType, bool IncludeMembers) {
