30399b175b81165c65b5cd6a9acca70a76fd96b9
urriellu <urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2009-07-19 19:01:55 +0000

    Overriding ResultType for Arithmetic Operations

--- trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/ArithmeticOperation.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/PIR/Operations/ArithmeticOperation.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Pigmeo.Internal;
 using PRefl = Pigmeo.Internal.Reflection;
+using Pigmeo.Compiler.UI;
 
 namespace Pigmeo.Compiler.PIR {
 	public abstract class ArithmeticOperation:Operation {
@@ -9,11 +11,32 @@ namespace Pigmeo.Compiler.PIR {
 
 		public override string ToString() {
 			string ret = Label + ": " + Result + " " + AssignmentSign + " ";
-			for(int i = 0 ; i < Arguments.Length ; i++) {
+			for(int i = 0; i < Arguments.Length; i++) {
 				ret += Arguments[i];
 				if(i != Arguments.Length - 1) ret += " " + Operator + " ";
 			}
 			return ret;
+		}
+
+		public override Type ResultType {
+			get {
+				ShowInfo.InfoDebug("Getting result type of operation " + this.ToString());
+				Type ResType = null;
+				foreach(Operand Opnd in Arguments) {
+					BaseType BT = BaseType.NativeInt;
+					if(Opnd is LocalVariableOperand && ((Opnd as LocalVariableOperand).TheLV.LocalVarType.IsBaseType)) BT = (((Opnd as LocalVariableOperand).TheLV.LocalVarType)).ReprBaseType;
+					else if(Opnd is ParameterOperand && ((Opnd as ParameterOperand).TheParameter.ParamType.IsBaseType)) BT = ((Opnd as ParameterOperand).TheParameter.ParamType).ReprBaseType;
+					else if(Opnd is FieldOperand && ((Opnd as FieldOperand).TheField.FieldType.IsBaseType)) BT = ((Opnd as FieldOperand).TheField.FieldType).ReprBaseType;
+					if(BT.IsStandardSize()) {
+						if(ResType == null || (ResType != null && BT.IsHeavierThan(ResType.ReprBaseType))) ResType = ParentProgram.Types[BT.GetBclName()];
+					}
+				}
+				if(ResType == null) ErrorsAndWarnings.Throw(ErrorsAndWarnings.errType.Error, "INT0001", true, "Unable to find result type");
+				return ResType;
+			}
+			set {
+				base.ResultType = value;
+			}
 		}
 	}
 }
