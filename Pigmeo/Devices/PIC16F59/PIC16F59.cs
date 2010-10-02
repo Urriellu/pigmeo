5c1aad025146b70e23cf0c747647dfba595602f2
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2010-10-02 18:53:20 +0000

    Each microcontroller library has now its own VS project

---
+++ Pigmeo/Devices/PIC16F59/PIC16F59.cs
@@ -0,0 +1,43 @@
+﻿using Pigmeo;
+using Pigmeo.Internal;
+using Pigmeo.Internal.PIC;
+
+[assembly: DeviceLibrary(Architecture.PIC, Family.PIC14, Branch.PIC16F59)]
+
+namespace Pigmeo.MCU {
+	/// <summary>
+	/// Constains all the information about the PIC
+	/// </summary>
+	public static class Info {
+		public static InfoPIC GetInfo() {
+			InfoPIC device = new InfoPIC();
+
+			device.Architecture = Architecture.PIC;
+			device.Family = Family.PIC14;
+			device.Branch = Branch.PIC16F59;
+			device.DataMemory = new DataMemoryBank[8];
+			device.DataMemory[0].FirstSFR = 0x00;
+			device.DataMemory[0].LastSFR = 0x09;
+			device.DataMemory[0].FirstGPR = 0x0A;
+			device.DataMemory[0].LastGPR = 0x1F;
+			device.DataMemory[1].FirstGPR = 0x10;
+			device.DataMemory[1].LastGPR = 0x1F;
+			device.DataMemory[2].FirstGPR = 0x10;
+			device.DataMemory[2].LastGPR = 0x1F;
+			device.DataMemory[3].FirstGPR = 0x10;
+			device.DataMemory[3].LastGPR = 0x1F;
+			device.DataMemory[4].FirstGPR = 0x10;
+			device.DataMemory[4].LastGPR = 0x1F;
+			device.DataMemory[5].FirstGPR = 0x10;
+			device.DataMemory[5].LastGPR = 0x1F;
+			device.DataMemory[6].FirstGPR = 0x10;
+			device.DataMemory[6].LastGPR = 0x1F;
+			device.DataMemory[7].FirstGPR = 0x10;
+			device.DataMemory[7].LastGPR = 0x1F;
+			device.MaxWords = 2048;
+			device.IncludeFile = "p16f59.inc";
+
+			return device;
+		}
+	}
+}
