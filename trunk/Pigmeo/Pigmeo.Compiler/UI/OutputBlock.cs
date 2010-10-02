5c5b69285e99d1b64787f1e4f08004dc4120df3a
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2010-10-02 19:27:21 +0000

    Form for debugging with visual studio

---
+++ trunk/Pigmeo/Pigmeo.Compiler/UI/OutputBlock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Compiler.UI {
+	/// <summary>
+	/// Blocks in which messages are grouped when storing debug/verbose output messages
+	/// </summary>
+	public class OutputBlock {
+		public string Name = "Unnamed";
+	
+		public OutputOrigin Origin = OutputOrigin.Miscellaneous;
+
+		public string Title {
+			get {
+				switch(Origin) {
+					case OutputOrigin.HighLevelCompiler:
+						return "HL: " + Name;
+					case OutputOrigin.Frontend:
+						return "Fe: " + Name;
+					case OutputOrigin.Backend:
+						return "Be: " + Name;
+					case OutputOrigin.Assembler:
+						return "asblr: " + Name;
+					case OutputOrigin.Miscellaneous:
+						return "Miscellaneous";
+					default:
+						throw new NotImplementedException(Origin.ToString());
+				}
+			}
+		}
+
+		/// <summary>
+		/// List of messages.
+		/// First string: title of message (can be empty)
+		/// Second string: the message
+		/// </summary>
+		public Dictionary<string, string> Messages = new Dictionary<string, string>(5);
+	}
+
+	public enum OutputOrigin {
+		Miscellaneous,
+		HighLevelCompiler,
+		Frontend,
+		Backend,
+		Assembler
+	}
+}
---
+++ trunk/Pigmeo/Pigmeo.Compiler/UI/WinForms/FrmDebugVS.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using PRefl = Pigmeo.Internal.Reflection;
+
+namespace Pigmeo.Compiler.UI.WinForms {
+	public partial class FrmDebugVS:Form {
+		readonly List<OutputBlock> Outputs;
+
+		public FrmDebugVS(List<OutputBlock> Outputs) {
+			InitializeComponent();
+			this.Outputs = Outputs;
+		}
+
+		private void FrmDebugVS_Load(object sender, EventArgs e) {
+			lstOutputs.Items.Clear();
+			tabsDebug.SelectTab(tabOutputs);
+			UpdateLstOutputs();
+		}
+
+		public void UpdateLstOutputs() {
+			ListViewItem LastSelectedItem = (lstOutputs.SelectedItems.Count > 0) ? lstOutputs.SelectedItems[0] : null;
+			txtDebugOutput.Clear();
+			lstOutputs.Items.Clear();
+			foreach(var item in Outputs) {
+				lstOutputs.Items.Add(item.Title);
+				lstOutputs.Items[lstOutputs.Items.Count - 1].Tag = item.Name;
+			}
+			if(lstOutputs.Items.Count > 0) lstOutputs.Items[0].Selected = true;
+			Refresh();
+		}
+
+		private void lstOutputs_SelectedIndexChanged(object sender, EventArgs e) {
+			if(lstOutputs.SelectedIndices.Count > 0) {
+				int ind = lstOutputs.SelectedIndices[0];
+				foreach(var msg in Outputs[ind].Messages) {
+					txtDebugOutput.AppendText(msg.Key + ":" + Environment.NewLine);
+					txtDebugOutput.AppendText(msg.Value + ":" + Environment.NewLine);
+				}
+			} else txtDebugOutput.Clear();
+		}
+
+		public void SetReflectedAssembly(PRefl.Assembly ReflectedAssembly) {
+			txtReflectedAss.Clear();
+			txtReflectedAss.Text = ReflectedAssembly.ToString();
+		}
+
+		public void SetAsm(string[] AsmCode) {
+			txtGenAsm.Clear();
+			foreach(string line in AsmCode) {
+				txtGenAsm.AppendText(line + Environment.NewLine);
+			}
+		}
+
+		internal void SetExeInfo(List<string> info) {
+			txtAssInfo.Clear();
+			foreach(string line in info) {
+				txtAssInfo.AppendText(line + Environment.NewLine);
+			}
+		}
+	}
+}
