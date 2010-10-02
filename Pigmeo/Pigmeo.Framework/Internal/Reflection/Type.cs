a5349265841e4da3a761eb67b0c827ce3364a1c7
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2010-10-02 19:48:29 +0000

    Removing unneeded usings

--- Pigmeo/Pigmeo.Framework/Internal/Reflection/Type.cs
+++ Pigmeo/Pigmeo.Framework/Internal/Reflection/Type.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Pigmeo.Extensions;
-
+﻿
 namespace Pigmeo.Internal.Reflection {
 	/// <summary>
 	/// Represents a reflected .NET Type (class, struct, enum...)
