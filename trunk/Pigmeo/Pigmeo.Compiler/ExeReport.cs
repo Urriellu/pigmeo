f86633ef73ec1517fcdd8f0d101b9f13b35afc36
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2010-10-02 19:02:38 +0000

    ExeReport improved. Documentation added

--- trunk/Pigmeo/Pigmeo.Compiler/ExeReport.cs
+++ trunk/Pigmeo/Pigmeo.Compiler/ExeReport.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-using Mono.Cecil;
-using Mono.Cecil.Cil;
 using Pigmeo;
 using Pigmeo.Internal;
 using Pigmeo.Compiler.UI;
+using Pigmeo.Internal.Reflection;
 
 namespace Pigmeo.Compiler {
 	public class ExeReport {
 		protected static List<string> ReportStrings;
 
-		[PigmeoToDo("Must be rewritten using P.I.Reflection and maybe PIR")]
+		/// <summary>
+		/// Builds a report with detailed information about an assembly (.exe file)
+		/// </summary>
+		/// <param name="FilePath">Full path to the assembly</param>
+		/// <returns>Collection of lines with all the information</returns>
 		public static List<string> BuildReport(string FilePath) {
 			ReportStrings = new List<string>();
 			string FileName = System.IO.Path.GetFileName(FilePath);
 			ShowInfo.InfoDebug("Retrieving information about {0}", FilePath);
 
 			ReportStrings.Add(i18n.str("InfoAbout", FileName));
+
+			Assembly ass = Assembly.GetFromFile(FilePath);
+			ReportStrings.Add(ass.FullName);
+			ReportStrings.Add("EntryPoint: " + ass.EntryPoint.FullNameWAssParams);
+			ReportStrings.Add("References: " + ass.References.ToString());
+			ReportStrings.Add("Target Architecture: " + ass.Target.Architecture);
+			ReportStrings.Add("Target Family: " + ass.Target.Family);
+			ReportStrings.Add("Target Branch: " + ass.Target.Branch);
+			ReportStrings.Add("Types: " + ass.Types);
 
 			return ReportStrings;
 		}
