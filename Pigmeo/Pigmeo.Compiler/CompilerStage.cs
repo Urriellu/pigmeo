6aa4c17813757b8b04421a9c2b403f3fb24485c8
Urriellu <Urriellu@666d2513-1790-499f-a5fc-e7c30fe740bf>
2010-10-17 19:13:05 +0000

    New enum: CompilerStage

---
+++ Pigmeo/Pigmeo.Compiler/CompilerStage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pigmeo.Compiler {
+	/// <summary>
+	/// The possible compiler statuses
+	/// </summary>
+	public enum CompilerStage {
+		/// <summary>
+		/// Compiler initialization
+		/// </summary>
+		Initialization,
+
+		/// <summary>
+		/// Intermediate stages between different stages
+		/// </summary>
+		Intermediate,
+
+		/// <summary>
+		/// Running the high level language compiler
+		/// </summary>
+		HighLevelCompiler,
+
+		/// <summary>
+		/// Running the Frontend
+		/// </summary>
+		Frontend,
+
+		/// <summary>
+		/// Running the backend
+		/// </summary>
+		Backend,
+
+		/// <summary>
+		/// Running the assembler
+		/// </summary>
+		Assembler
+	}
+}
